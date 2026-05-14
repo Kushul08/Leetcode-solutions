02a028e9481c943cf6fa186e3f19535d979d55e6
Kushul Aleti <kushulaleti@gmail.com>
2026-05-14 15:58:55 +0530

    refactor: update Check if Array is Good (easy)

--- solutions/easy/2784-check-if-array-is-good.py
+++ solutions/easy/2784-check-if-array-is-good.py
@@ -1,8 +1,8 @@
 # ─────────────────────────────────────────────────
 #  Problem : 2784. Check if Array is Good
 #  Difficulty : Easy
-#  Runtime  : 0 ms
-#  Memory   : 12.4 MB
+#  Runtime  : 4 ms
+#  Memory   : 12.2 MB
 #  Solved   : 2026-05-14
 # ─────────────────────────────────────────────────
 
@@ -13,6 +13,9 @@ class Solution(object):
         :rtype: bool
         """
         max_val=max(nums)
+        for i in range(1,max_val+1):
+            if i not in nums:
+                return False
         total_sum=(max_val*(max_val+1))/2
         total_sum+=max_val
         return sum(nums)==total_sum
