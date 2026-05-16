1d67301352e2ebcf6371541e3821b94b5ab1a33e
Kushul Aleti <kushulaleti@gmail.com>
2026-05-16 14:28:25 +0530

    refactor: update Count Complete Tree Nodes (easy)

--- solutions/easy/0222-count-complete-tree-nodes.py
+++ solutions/easy/0222-count-complete-tree-nodes.py
@@ -1,8 +1,8 @@
 # ─────────────────────────────────────────────────
 #  Problem : 0222. Count Complete Tree Nodes
 #  Difficulty : Easy
-#  Runtime  : 39 ms
-#  Memory   : 28.4 MB
+#  Runtime  : 84 ms
+#  Memory   : 28.1 MB
 #  Solved   : 2026-05-16
 # ─────────────────────────────────────────────────
 
@@ -26,15 +26,14 @@ class Solution(object):
             if not root:
                 return 0
             return 1+right_height(root.right)
-        def dfs(root):
-            if not root:
-                return 0
+        if not root:
+            return 0
             
-            left=left_height(root)
-            right=right_height(root.right)
+        left=left_height(root)
+        right=right_height(root.right)
 
-            if left==right:
-                return pow(2,left)-1
-            else:
-                return 1+dfs(root.left)+dfs(root.right)
-        return dfs(root)
+        if left==right:
+            return pow(2,left)-1
+            
+        return 1+self.countNodes(root.left)+self.countNodes(root.right)
+        
