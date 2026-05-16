6e59b8a306316171dd08c02b780ee3081f096042
Kushul Aleti <kushulaleti@gmail.com>
2026-05-16 11:30:43 +0530

    solve: 0154. Find Minimum in Rotated Sorted Array II [hard] (py)

---
+++ solutions/hard/0154-find-minimum-in-rotated-sorted-array-ii.py
@@ -0,0 +1,26 @@
+# ─────────────────────────────────────────────────
+#  Problem : 0154. Find Minimum in Rotated Sorted Array II
+#  Difficulty : Hard
+#  Runtime  : 0 ms
+#  Memory   : 12.7 MB
+#  Solved   : 2026-05-16
+# ─────────────────────────────────────────────────
+
+class Solution(object):
+    def findMin(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        l=0
+        r=len(nums)-1
+
+        while l<r:
+            mid=(l+r)//2
+            if nums[mid]>nums[r]:
+                l=mid+1
+            elif nums[mid]==nums[r]:
+                r-=1
+            else:
+                r=mid
+        return nums[l]
