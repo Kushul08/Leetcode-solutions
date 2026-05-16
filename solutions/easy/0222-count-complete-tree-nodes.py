a8d110c2bdd6ecd193546c0eb051f786718910a1
Kushul Aleti <kushulaleti@gmail.com>
2026-05-16 13:53:15 +0530

    refactor: update Count Complete Tree Nodes (easy)

--- solutions/easy/0222-count-complete-tree-nodes.py
+++ solutions/easy/0222-count-complete-tree-nodes.py
@@ -1,8 +1,8 @@
 # ─────────────────────────────────────────────────
 #  Problem : 0222. Count Complete Tree Nodes
 #  Difficulty : Easy
-#  Runtime  : 7 ms
-#  Memory   : 28.3 MB
+#  Runtime  : 19 ms
+#  Memory   : 28.4 MB
 #  Solved   : 2026-05-16
 # ─────────────────────────────────────────────────
 
@@ -12,22 +12,18 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-from collections import deque
 class Solution(object):
     def countNodes(self, root):
         """
         :type root: Optional[TreeNode]
         :rtype: int
         """
-        if not root: return 0
-        queue=deque([root])
-        count=0
-
-        while queue:
-            node=queue.popleft()
-            count+=1
-            if node.left:
-                queue.append(node.left)
-            if node.right:
-                queue.append(node.right)
-        return count
+        count=[0]
+        def dfs(root):
+            if not root:
+                return
+            count[0]+=1
+            dfs(root.left)
+            dfs(root.right)
+        dfs(root)
+        return count[0]
