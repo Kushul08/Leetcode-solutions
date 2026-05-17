62d69a9f8c6ce03b8ebbdbc5aa5a9f3298279f1c
Kushul Aleti <kushulaleti@gmail.com>
2026-05-17 17:29:17 +0530

    refactor: update Jump Game II (medium)

--- solutions/medium/0045-jump-game-ii.py
+++ solutions/medium/0045-jump-game-ii.py
@@ -1,8 +1,8 @@
 # ─────────────────────────────────────────────────
 #  Problem : 0045. Jump Game II
 #  Difficulty : Medium
-#  Runtime  : 0 ms
-#  Memory   : 12.4 MB
+#  Runtime  : 6 ms
+#  Memory   : 12.8 MB
 #  Solved   : 2026-05-17
 # ─────────────────────────────────────────────────
 
@@ -12,11 +12,11 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        if len(nums)==1:
+            return 0
         def find_greater(start,end):
-            if end>=len(nums)-1:
-                return len(nums)
-            farthest=0
-            index=0
+            farthest=start
+            index=start
             for i in range(start,end):
                 if farthest<i+nums[i]:
                     farthest=i+nums[i]
@@ -26,6 +26,8 @@ class Solution(object):
         i=0
         count=0
         while i<len(nums):
+            if nums[i]+i>=len(nums)-1:
+                return count+1
             i=find_greater(i+1,i+nums[i]+1)
             count+=1
             if i>=len(nums)-1:
