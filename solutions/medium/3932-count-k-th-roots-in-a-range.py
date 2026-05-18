f639d139770389aaf3abc5ae4a1ecf04c028342c
Kushul Aleti <kushulaleti@gmail.com>
2026-05-18 06:06:06 +0530

    solve: 3932. Count K-th Roots in a Range [medium] (py)

---
+++ solutions/medium/3932-count-k-th-roots-in-a-range.py
@@ -0,0 +1,15 @@
+# ─────────────────────────────────────────────────
+#  Problem : 3932. Count K-th Roots in a Range
+#  Difficulty : Medium
+#  Runtime  : 0 ms
+#  Memory   : 19.3 MB
+#  Solved   : 2026-05-18
+# ─────────────────────────────────────────────────
+
+import math
+class Solution:
+    def countKthRoots(self, l: int, r: int, k: int) -> int:
+        left=ceil(pow(l,1/k))
+        right=floor(pow(r,1/k))
+
+        return right-left+1
