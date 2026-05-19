efc7fe6ffe97e8f2f91b331dbb790409e8188fd5
Kushul Aleti <kushulaleti@gmail.com>
2026-05-19 14:41:43 +0530

    solve: 0700. Search in a Binary Search Tree [easy] (py)

---
+++ solutions/easy/0700-search-in-a-binary-search-tree.py
@@ -0,0 +1,36 @@
+# ─────────────────────────────────────────────────
+#  Problem : 0700. Search in a Binary Search Tree
+#  Difficulty : Easy
+#  Runtime  : 0 ms
+#  Memory   : 16.4 MB
+#  Solved   : 2026-05-19
+# ─────────────────────────────────────────────────
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def searchBST(self, root, val):
+        """
+        :type root: Optional[TreeNode]
+        :type val: int
+        :rtype: Optional[TreeNode]
+        """
+        node=root
+        while node:
+            if node.val==val:
+                return node
+            elif val<node.val:
+                if node.left:
+                    node=node.left
+                    continue
+                return 
+            else:
+                if node.right:
+                    node=node.right
+                    continue
+                return
+        return 
