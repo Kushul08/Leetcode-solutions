e16dc181459fdec7ad3988623ef726f147402446
Kushul Aleti <kushulaleti@gmail.com>
2026-05-19 09:45:18 +0530

    solve: 2540. Minimum Common Value [easy] (py)

---
+++ solutions/easy/2540-minimum-common-value.py
@@ -0,0 +1,25 @@
+# ─────────────────────────────────────────────────
+#  Problem : 2540. Minimum Common Value
+#  Difficulty : Easy
+#  Runtime  : 0 ms
+#  Memory   : 12.5 MB
+#  Solved   : 2026-05-19
+# ─────────────────────────────────────────────────
+
+class Solution(object):
+    def getCommon(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: int
+        """
+        i=0
+        j=0
+        while i<len(nums1) and j<len(nums2):
+            if nums1[i]==nums2[j]:
+                return nums1[i]
+            elif nums1[i]<nums2[j]:
+                i+=1
+            else:
+                j+=1
+        
