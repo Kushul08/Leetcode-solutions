d26bc754cb6597f7441cb42f6bfc5ea8daca08f1
Kushul Aleti <kushulaleti@gmail.com>
2026-05-19 14:10:33 +0530

    solve: 0114. Flatten Binary Tree to Linked List [medium] (py)

---
+++ solutions/medium/0114-flatten-binary-tree-to-linked-list.py
@@ -0,0 +1,31 @@
+# ─────────────────────────────────────────────────
+#  Problem : 0114. Flatten Binary Tree to Linked List
+#  Difficulty : Medium
+#  Runtime  : 4 ms
+#  Memory   : 12.7 MB
+#  Solved   : 2026-05-19
+# ─────────────────────────────────────────────────
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def flatten(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: None Do not return anything, modify root in-place instead.
+        """
+        prev=[None]
+        curr=root
+        def recursive(node):
+            if not node:
+                return
+            recursive(node.right)
+            recursive(node.left)
+            node.right=prev[0]
+            node.left=None
+            prev[0]=node
+        recursive(root)
