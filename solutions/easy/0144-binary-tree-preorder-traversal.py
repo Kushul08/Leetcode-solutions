a8531e746dfacdfffcb879745f3ea27c55c6c9d3
Kushul Aleti <kushulaleti@gmail.com>
2026-05-19 10:18:49 +0530

    solve: 0144. Binary Tree Preorder Traversal [easy] (py)

---
+++ solutions/easy/0144-binary-tree-preorder-traversal.py
@@ -0,0 +1,39 @@
+# ─────────────────────────────────────────────────
+#  Problem : 0144. Binary Tree Preorder Traversal
+#  Difficulty : Easy
+#  Runtime  : 0 ms
+#  Memory   : 12.3 MB
+#  Solved   : 2026-05-19
+# ─────────────────────────────────────────────────
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def preorderTraversal(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: List[int]
+        """
+        inorder=[]
+        curr=root
+
+        while curr:
+            if curr.left==None:
+                inorder.append(curr.val)
+                curr=curr.right
+            else:
+                prev=curr.left
+                while prev.right and prev.right!=curr:
+                    prev=prev.right
+                if  prev.right==None:
+                    prev.right=curr
+                    inorder.append(curr.val)
+                    curr=curr.left
+                else:
+                    prev.right=None
+                    curr=curr.right
+        return inorder
