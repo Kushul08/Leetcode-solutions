a0781b1a4dba050a2827515acf3703aa51ee793f
Kushul Aleti <kushulaleti@gmail.com>
2026-05-21 14:52:22 +0530

    solve: 0098. Validate Binary Search Tree [medium] (py)

---
+++ solutions/medium/0098-validate-binary-search-tree.py
@@ -0,0 +1,38 @@
+# ─────────────────────────────────────────────────
+#  Problem : 0098. Validate Binary Search Tree
+#  Difficulty : Medium
+#  Runtime  : 7 ms
+#  Memory   : 17.4 MB
+#  Solved   : 2026-05-21
+# ─────────────────────────────────────────────────
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isValidBST(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: bool
+        """
+        prev=[None]
+        def inorder(root):
+            if not root:
+                return True
+            
+            if not inorder(root.left): return False
+
+            if prev[0]==None: 
+                prev[0]=root.val-1
+            if prev[0]>root.val: return False
+            elif prev[0]==root.val: return False
+            else: prev[0]=root.val
+            
+            
+            if not inorder(root.right): return False
+            return True
+
+        return inorder(root)
