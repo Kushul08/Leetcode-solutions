3b7f1a2c5371c77cf4026add64fb4e7e8b941729
Kushul Aleti <kushulaleti@gmail.com>
2026-05-28 16:29:00 +0530

    refactor: update Longest Common Suffix Queries (hard)

--- solutions/hard/3093-longest-common-suffix-queries.py
+++ solutions/hard/3093-longest-common-suffix-queries.py
@@ -1,13 +1,16 @@
 # ─────────────────────────────────────────────────
 #  Problem : 3093. Longest Common Suffix Queries
 #  Difficulty : Hard
-#  Runtime  : 0 ms
-#  Memory   : 12.3 MB
+#  Runtime  : 5183 ms
+#  Memory   : 341.1 MB
 #  Solved   : 2026-05-28
 # ─────────────────────────────────────────────────
 
-from heapq import heappush,heappop
-import heapq
+class Trie:
+    def __init__(self):
+        self.children={}
+        self.best=(float('inf'),float('inf'))
+    
 class Solution(object):
     def stringIndices(self, wordsContainer, wordsQuery):
         """
@@ -15,19 +18,30 @@ class Solution(object):
         :type wordsQuery: List[str]
         :rtype: List[int]
         """
-        look={}
-
+        root=Trie()
         for i,word in enumerate(wordsContainer):
-            for j in range(len(word)+1):
-                if word[j:] in look:
-                    heappush(look[word[j:]],(len(word),i))
-                else:
-                    look[word[j:]]=[]
-                    heappush(look[word[j:]],(len(word),i))
+            word=word[::-1]
+            node=root
+            best=(len(word),i)
+
+            if best<node.best:
+                node.best=best
+            for ch in (word):
+                if ch not in node.children:
+                    node.children[ch]=Trie()
+                node=node.children[ch]
+
+                if best<node.best:
+                    node.best=best
+        
         ans=[]
+
         for i,word in enumerate(wordsQuery):
-            for i in range(len(word)+1):
-                if word[i:] in look:
-                    ans.append(look[word[i:]][0][1])
+            node=root
+            for ch in reversed(word):
+
+                if ch not in node.children:
                     break
+                node=node.children[ch]
+            ans.append(node.best[1])
         return ans
