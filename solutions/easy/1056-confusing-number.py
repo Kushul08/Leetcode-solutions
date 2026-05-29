3e47bbe564fdbc4d34d4522b6551c72a85631796
Kushul Aleti <kushulaleti@gmail.com>
2026-05-29 22:08:28 +0530

    solve: 1056. Confusing Number [easy] (py)

---
+++ solutions/easy/1056-confusing-number.py
@@ -0,0 +1,23 @@
+# ─────────────────────────────────────────────────
+#  Problem : 1056. Confusing Number
+#  Difficulty : Easy
+#  Runtime  : 0 ms
+#  Memory   : 12.5 MB
+#  Solved   : 2026-05-29
+# ─────────────────────────────────────────────────
+
+class Solution(object):
+    def confusingNumber(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        rotated ={0:0,1:1,6:9,8:8,9:6}
+        ans=0
+        num=n
+        while num>0:
+            val=num%10
+            if val in rotated:
+                ans=ans*10+rotated[val]
+            num=num//10
+        return ans!=n
