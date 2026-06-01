8c212df14061cf46fe46dcde3c85b43bf1a1e53f
Kushul Aleti <kushulaleti@gmail.com>
2026-06-01 14:09:20 +0530

    solve: 1765. Map of Highest Peak [medium] (py)

---
+++ solutions/medium/1765-map-of-highest-peak.py
@@ -0,0 +1,38 @@
+# ─────────────────────────────────────────────────
+#  Problem : 1765. Map of Highest Peak
+#  Difficulty : Medium
+#  Runtime  : 0 ms
+#  Memory   : 12.5 MB
+#  Solved   : 2026-06-01
+# ─────────────────────────────────────────────────
+
+from collections import deque
+class Solution(object):
+    def highestPeak(self, isWater):
+        """
+        :type isWater: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        ans=isWater
+        m,n=len(isWater),len(isWater[0])
+        visited=[[0 for _ in range(m)] for _ in range(n)]
+        queue=deque()
+        for i in range(len(isWater)):
+            for j in range(len(isWater[0])):
+                if isWater[i][j]==1:
+                    queue.append((i,j,0))
+        while queue:
+            x,y,dis=queue.popleft()
+            visited[x][y]=1
+            if isWater[x][y]==0:
+                ans[x][y]=dis
+            else:
+                ans[x][y]=0
+            directions=[(-1,0),(1,0),(0,1),(0,-1)]
+            for dx,dy in directions:
+                nx,ny=x+dx,y+dy
+                if 0<=nx and nx<n and 0<=ny and ny<m:
+                    if visited[nx][ny]==0:
+                        visited[nx][ny]=1
+                        queue.append((nx,ny,dis+1))
+        return ans
