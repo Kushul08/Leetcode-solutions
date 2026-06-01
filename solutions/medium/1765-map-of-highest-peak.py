f91b6123479d56f1db6020529deef0d0c9ddc27c
Kushul Aleti <kushulaleti@gmail.com>
2026-06-01 14:13:43 +0530

    refactor: update Map of Highest Peak (medium)

--- solutions/medium/1765-map-of-highest-peak.py
+++ solutions/medium/1765-map-of-highest-peak.py
@@ -1,8 +1,8 @@
 # ─────────────────────────────────────────────────
 #  Problem : 1765. Map of Highest Peak
 #  Difficulty : Medium
-#  Runtime  : 0 ms
-#  Memory   : 12.5 MB
+#  Runtime  : 1182 ms
+#  Memory   : 121.2 MB
 #  Solved   : 2026-06-01
 # ─────────────────────────────────────────────────
 
@@ -14,20 +14,20 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         ans=isWater
-        m,n=len(isWater),len(isWater[0])
+        n,m=len(isWater),len(isWater[0])
         visited=[[0 for _ in range(m)] for _ in range(n)]
         queue=deque()
         for i in range(len(isWater)):
             for j in range(len(isWater[0])):
                 if isWater[i][j]==1:
                     queue.append((i,j,0))
+                    ans[i][j]=0
+                    visited[i][j]=1
         while queue:
             x,y,dis=queue.popleft()
             visited[x][y]=1
             if isWater[x][y]==0:
                 ans[x][y]=dis
-            else:
-                ans[x][y]=0
             directions=[(-1,0),(1,0),(0,1),(0,-1)]
             for dx,dy in directions:
                 nx,ny=x+dx,y+dy
