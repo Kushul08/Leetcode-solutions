42bb10ef87c103b0feab7174ea0bc3af5ad20343
Kushul Aleti <kushulaleti@gmail.com>
2026-06-03 17:06:58 +0530

    refactor: update Earliest Finish Time for Land and Water Rides II (medium)

--- solutions/medium/3635-earliest-finish-time-for-land-and-water-rides-ii.py
+++ solutions/medium/3635-earliest-finish-time-for-land-and-water-rides-ii.py
@@ -1,11 +1,12 @@
 # ─────────────────────────────────────────────────
 #  Problem : 3635. Earliest Finish Time for Land and Water Rides II
 #  Difficulty : Medium
-#  Runtime  : 0 ms
-#  Memory   : 12.2 MB
+#  Runtime  : 1244 ms
+#  Memory   : 31.5 MB
 #  Solved   : 2026-06-03
 # ─────────────────────────────────────────────────
 
+from bisect import bisect_right
 class Solution(object):
     def earliestFinishTime(self, landStartTime, landDuration, waterStartTime, waterDuration):
         """
@@ -15,14 +16,33 @@ class Solution(object):
         :type waterDuration: List[int]
         :rtype: int
         """
-        ans=float('inf')
-        for i in range(len(landStartTime)):
-            for j in range(len(waterStartTime)):
-                land=landStartTime[i]+landDuration[i]
-                finish1=max(land,waterStartTime[j])+waterDuration[j]
+        def solve(firststart,firstdur,secondstart,seconddur):
 
-                water=waterStartTime[j]+waterDuration[j]
-                finish2=max(water,landStartTime[i])+landDuration[i]
+            rides=sorted(zip(secondstart,seconddur))
 
-                ans=min(ans,finish1,finish2)
-        return ans
+            start=[s for s,d in rides]
+            n=len(start)
+
+            prefix=[0]*n
+            prefix[0]=rides[0][-1]
+            for i in range(1,len(rides)):
+                prefix[i]=min(prefix[i-1],rides[i][-1])
+            
+            suffix=[0]*n
+            suffix[-1]=rides[-1][0]+rides[-1][-1]
+
+            for i in range(len(rides)-2,-1,-1):
+                suffix[i]=min(suffix[i+1],rides[i][0]+rides[i][1])
+            ans=float('inf')
+            for s,d in zip(firststart,firstdur):
+                val=s+d
+                indx=bisect_right(start,val)
+
+                if indx>0:
+                    ans=min(ans,val+prefix[indx-1])
+                if indx<n:
+                    ans=min(ans,suffix[indx])
+            return ans
+        
+        return min(solve(landStartTime, landDuration, waterStartTime, waterDuration),
+                    solve( waterStartTime, waterDuration,landStartTime, landDuration))
