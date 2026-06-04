78f112da1405785a698b4a127a110df9b6376dfe
Kushul Aleti <kushulaleti@gmail.com>
2026-06-04 13:58:19 +0530

    refactor: update Word Ladder (hard)

--- solutions/hard/0127-word-ladder.py
+++ solutions/hard/0127-word-ladder.py
@@ -1,12 +1,12 @@
 # ─────────────────────────────────────────────────
 #  Problem : 0127. Word Ladder
 #  Difficulty : Hard
-#  Runtime  : 0 ms
-#  Memory   : 12.4 MB
+#  Runtime  : 548 ms
+#  Memory   : 13 MB
 #  Solved   : 2026-06-04
 # ─────────────────────────────────────────────────
 
-from collections import deque, Counter
+from collections import deque
 class Solution(object):
     def ladderLength(self, beginWord, endWord, wordList):
         """
@@ -19,8 +19,10 @@ class Solution(object):
             return 0
         queue=deque()
         queue.append((beginWord,1))
-        visited=set(beginWord)
-
+        words=set(wordList)
+        print(words)
+        if beginWord in words:
+            words.remove(beginWord)
         def check(s1,s2):
             count=0
             for a,b in zip(s1,s2):
@@ -30,19 +32,18 @@ class Solution(object):
             if count==1:
                 return True
             return False
-        ans=float('inf')
         while queue:
-            # print(queue)
             node,step=queue.popleft()
             if node==endWord:
-                ans=min(ans,step)
-                continue
-            for char in wordList:
-                if char in visited:
-                    continue
-                if check(node,char):
-                    queue.append((char,step+1))
-                    # print(queue)
-                    visited.add(char)
-        if ans==float('inf'): return 0
-        return ans
+                return step
+            word=list(node)
+            for i in range(len(word)):
+                original=word[i]
+                for j in range(97,123):
+                    word[i]=chr(j)
+                    new_word=''.join(word)
+                    if new_word in words:
+                        queue.append((new_word,step+1))
+                        words.remove(new_word)
+                word[i]=original
+        return 0
