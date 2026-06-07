9da14dae9e2f60059686def16748c57f01ba5039
Kushul Aleti <kushulaleti@gmail.com>
2026-06-07 21:29:51 +0530

    refactor: update Number of Islands (medium)

--- solutions/medium/0200-number-of-islands.py
+++ solutions/medium/0200-number-of-islands.py
@@ -1,8 +1,8 @@
 # ─────────────────────────────────────────────────
 #  Problem : 0200. Number of Islands
 #  Difficulty : Medium
-#  Runtime  : 273 ms
-#  Memory   : 31.3 MB
+#  Runtime  : 244 ms
+#  Memory   : 27.7 MB
 #  Solved   : 2026-06-07
 # ─────────────────────────────────────────────────
 
@@ -13,7 +13,7 @@ class Solution(object):
         :type grid: List[List[str]]
         :rtype: int
         """
-        seen=set()
+        # seen=set()
         queue=deque()
         directions=[(-1,0),(1,0),(0,1),(0,-1)]
         for i in range(len(grid)):
@@ -23,10 +23,13 @@ class Solution(object):
         islands=0
         while queue:
             x,y=queue.popleft()
-            if (x,y) in seen:
+            # if (x,y) in seen:
+            #     continue
+            if grid[x][y]=='0':
                 continue
             islands+=1
-            seen.add((x,y))
+            grid[x][y]='0'
+            # seen.add((x,y))
 
             temp=deque([(x,y)])
 
@@ -36,7 +39,8 @@ class Solution(object):
                     nx,ny=i+dx,j+dy
                     if nx<0 or nx>=len(grid) or ny<0 or ny>=len(grid[0]):
                         continue
-                    if grid[nx][ny]=='1' and (nx,ny) not in seen:
-                        seen.add((nx,ny))
+                    if grid[nx][ny]=='1': #and (nx,ny) not in seen
+                        grid[nx][ny]='0'
+                        # seen.add((nx,ny))
                         temp.append((nx,ny))
         return islands
