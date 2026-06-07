0e2e938f75a8fe74b8ad1e20da20a08cfe7b7292
Kushul Aleti <kushulaleti@gmail.com>
2026-06-07 21:30:34 +0530

    refactor: update Number of Islands (medium)

--- solutions/medium/0200-number-of-islands.py
+++ solutions/medium/0200-number-of-islands.py
@@ -1,8 +1,8 @@
 # ─────────────────────────────────────────────────
 #  Problem : 0200. Number of Islands
 #  Difficulty : Medium
-#  Runtime  : 244 ms
-#  Memory   : 27.7 MB
+#  Runtime  : 247 ms
+#  Memory   : 27.9 MB
 #  Solved   : 2026-06-07
 # ─────────────────────────────────────────────────
 
@@ -13,7 +13,6 @@ class Solution(object):
         :type grid: List[List[str]]
         :rtype: int
         """
-        # seen=set()
         queue=deque()
         directions=[(-1,0),(1,0),(0,1),(0,-1)]
         for i in range(len(grid)):
@@ -23,13 +22,10 @@ class Solution(object):
         islands=0
         while queue:
             x,y=queue.popleft()
-            # if (x,y) in seen:
-            #     continue
             if grid[x][y]=='0':
                 continue
             islands+=1
             grid[x][y]='0'
-            # seen.add((x,y))
 
             temp=deque([(x,y)])
 
@@ -39,8 +35,7 @@ class Solution(object):
                     nx,ny=i+dx,j+dy
                     if nx<0 or nx>=len(grid) or ny<0 or ny>=len(grid[0]):
                         continue
-                    if grid[nx][ny]=='1': #and (nx,ny) not in seen
+                    if grid[nx][ny]=='1': 
                         grid[nx][ny]='0'
-                        # seen.add((nx,ny))
                         temp.append((nx,ny))
         return islands
