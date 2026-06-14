0e24fca5c395f3482f076dd72b2d9178fd0512cf
Kushul Aleti <kushulaleti@gmail.com>
2026-06-14 20:43:08 +0530

    refactor: update Course Schedule II (medium)

--- solutions/medium/0210-course-schedule-ii.py
+++ solutions/medium/0210-course-schedule-ii.py
@@ -1,8 +1,8 @@
 # ─────────────────────────────────────────────────
 #  Problem : 0210. Course Schedule II
 #  Difficulty : Medium
-#  Runtime  : 0 ms
-#  Memory   : 12.4 MB
+#  Runtime  : 11 ms
+#  Memory   : 15.5 MB
 #  Solved   : 2026-06-14
 # ─────────────────────────────────────────────────
 
@@ -16,7 +16,25 @@ class Solution(object):
         adj_list=[[] for _ in range(numCourses)]
         for a,b in prerequisites:
             adj_list[b].append(a)
-
+        visited=[0]*numCourses 
+        path=[0]*numCourses 
+        def acyclic(root):
+            if visited[root]==0:
+                visited[root]=1
+                for node in adj_list[root]:
+                    if path[node]==1:
+                        return False
+                    path[node]=1
+                    if acyclic(node)==False:
+                        return False
+                    path[node]=0
+            return True
+        for node in range(numCourses):
+            if visited[node]==0:
+                path[node]=1
+                if acyclic(node)==False:
+                    return []
+                path[node]=0
         stack=[]
         visited=[0]*numCourses 
         def dfs(node):
