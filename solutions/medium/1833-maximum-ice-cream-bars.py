87e7b9d2da2ac1a02986d49bd4d54d9446954164
Kushul Aleti <kushulaleti@gmail.com>
2026-06-21 15:23:33 +0530

    refactor: update Maximum Ice Cream Bars (medium)

--- solutions/medium/1833-maximum-ice-cream-bars.py
+++ solutions/medium/1833-maximum-ice-cream-bars.py
@@ -1,8 +1,8 @@
 # ─────────────────────────────────────────────────
 #  Problem : 1833. Maximum Ice Cream Bars
 #  Difficulty : Medium
-#  Runtime  : 104 ms
-#  Memory   : 21.2 MB
+#  Runtime  : 109 ms
+#  Memory   : 21.3 MB
 #  Solved   : 2026-06-21
 # ─────────────────────────────────────────────────
 
@@ -17,12 +17,9 @@ class Solution(object):
         arr=[0]*(max(costs)+1)
         for cost in costs:
             arr[cost]+=1
-        # print(arr)
         max_ice=0
 
         for i in range(1,len(arr)):
-            
-            # print(coins,i,max_ice)
             if coins==0 or coins<i: break
             need=min(coins//i,arr[i])
             coins-=need*i
