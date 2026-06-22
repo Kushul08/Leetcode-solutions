e233bdf440a8f23be05a9c4fff8920173ef8587e
Kushul Aleti <kushulaleti@gmail.com>
2026-06-22 08:41:25 +0530

    solve: 1189. Maximum Number of Balloons [easy] (py)

---
+++ solutions/easy/1189-maximum-number-of-balloons.py
@@ -0,0 +1,24 @@
+# ─────────────────────────────────────────────────
+#  Problem : 1189. Maximum Number of Balloons
+#  Difficulty : Easy
+#  Runtime  : 0 ms
+#  Memory   : 12.4 MB
+#  Solved   : 2026-06-22
+# ─────────────────────────────────────────────────
+
+from collections import Counter
+class Solution(object):
+    def maxNumberOfBalloons(self, text):
+        """
+        :type text: str
+        :rtype: int
+        """
+        text_freq=Counter(text)
+        ballon_freq=Counter('ballon')
+
+        instances=float('inf')
+        for char in 'ballon':
+            instances=min(instances,text_freq[char]/ballon_freq[char])
+        if instances==float('inf'):
+            return 0
+        return instances
