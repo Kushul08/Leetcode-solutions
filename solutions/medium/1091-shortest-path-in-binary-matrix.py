28b018910150a70331ac3ece15e858bfb2d1dc0a
Kushul Aleti <kushulaleti@gmail.com>
2026-06-25 17:52:27 +0530

    solve: 1091. Shortest Path in Binary Matrix [medium] (py)

---
+++ solutions/medium/1091-shortest-path-in-binary-matrix.py
@@ -0,0 +1,38 @@
+# ─────────────────────────────────────────────────
+#  Problem : 1091. Shortest Path in Binary Matrix
+#  Difficulty : Medium
+#  Runtime  : 310 ms
+#  Memory   : 12.7 MB
+#  Solved   : 2026-06-25
+# ─────────────────────────────────────────────────
+
+from heapq import heappush,heappop
+class Solution(object):
+    def shortestPathBinaryMatrix(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        n=len(grid)
+        if grid[0][0]==1: return -1
+        dis=[[float('inf')]*len(grid) for _ in range(len(grid))]
+        dis[0][0]=1
+        
+        queue=[]
+
+        heappush(queue,(dis[0][0],0,0))
+        directions=[(-1,-1),(-1,0),(-1,1),(0,-1),(0,1),(1,-1),(1,0),(1,1)]
+        while queue:
+            d,x,y=heappop(queue)
+            if d>dis[x][y]:
+                continue
+            for dx,dy in directions:
+                nx,ny=x+dx,y+dy
+                if 0<=nx<n and 0<=ny<n:
+                    if grid[nx][ny]==0 and dis[x][y]+1<dis[nx][ny]:
+                        dis[nx][ny]=dis[x][y]+1
+                        heappush(queue,(dis[nx][ny],nx,ny))
+        if dis[-1][-1]==float('inf'):
+            return -1
+        return dis[-1][-1]
+            
