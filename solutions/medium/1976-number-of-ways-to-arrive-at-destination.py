761b73e25d1f6b5fde2533f6ab69efd013db2a17
Kushul Aleti <kushulaleti@gmail.com>
2026-06-26 17:25:34 +0530

    solve: 1976. Number of Ways to Arrive at Destination [medium] (py)

---
+++ solutions/medium/1976-number-of-ways-to-arrive-at-destination.py
@@ -0,0 +1,38 @@
+# ─────────────────────────────────────────────────
+#  Problem : 1976. Number of Ways to Arrive at Destination
+#  Difficulty : Medium
+#  Runtime  : 0 ms
+#  Memory   : 12.5 MB
+#  Solved   : 2026-06-26
+# ─────────────────────────────────────────────────
+
+from heapq import heappush, heappop
+class Solution(object):
+    def countPaths(self, n, roads):
+        """
+        :type n: int
+        :type roads: List[List[int]]
+        :rtype: int
+        """
+        MOD=int(1e9+7)
+        adj_list=[[] for _ in range(n)]
+        for u,v,w in roads:
+            adj_list[u].append((v,w))
+            adj_list[v].append((u,w))
+        dis=[float('inf')]*n
+        queue=[]
+        heappush(queue,(0,0))
+        count=0
+        while queue:
+            cost,node=heappop(queue)
+            if cost>dis[node]:
+                continue
+            for v,w in adj_list[node]:
+                if cost+w==dis[v]:
+                    count+=1
+                if cost+w<dis[v]:
+                    count=1
+                    dis[v]=cost+w
+                    heappush(queue,(dis[v],v))
+        count=count%MOD
+        return count
