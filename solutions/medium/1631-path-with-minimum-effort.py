9e59c224c27f296b43c87c448c5398cb23c9e75a
Kushul Aleti <kushulaleti@gmail.com>
2026-06-26 08:50:52 +0530

    solve: 1631. Path With Minimum Effort [medium] (py)

---
+++ solutions/medium/1631-path-with-minimum-effort.py
@@ -0,0 +1,35 @@
+# ─────────────────────────────────────────────────
+#  Problem : 1631. Path With Minimum Effort
+#  Difficulty : Medium
+#  Runtime  : 451 ms
+#  Memory   : 13.3 MB
+#  Solved   : 2026-06-26
+# ─────────────────────────────────────────────────
+
+from heapq import heappop, heappush
+class Solution(object):
+    def minimumEffortPath(self, heights):
+        """
+        :type heights: List[List[int]]
+        :rtype: int
+        """
+        n,m=len(heights),len(heights[0])
+        max_abs=[[float('inf')]*m for _ in range(n)]
+
+        max_abs[0][0]=0
+        queue=[]
+        heappush(queue,(0,0,0))
+        directions=[(-1,0),(0,-1),(0,1),(1,0)]
+        while queue:
+            diff,x,y=heappop(queue)
+            if diff>max_abs[x][y]:
+                continue
+            for dx,dy in directions:
+                nx,ny=x+dx,y+dy
+                if 0<=nx<n and 0<=ny<m:
+                    curr_abs=abs(heights[x][y]-heights[nx][ny])
+                    max_so_far=max(max_abs[x][y],curr_abs)
+                    if max_so_far<max_abs[nx][ny]:
+                        max_abs[nx][ny]=max_so_far
+                        heappush(queue,(max_so_far,nx,ny))
+        return max_abs[-1][-1]
