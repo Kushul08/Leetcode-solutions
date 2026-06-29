b1ee037f44e0edce3928ac437bff368b27cccbcb
Kushul Aleti <kushulaleti@gmail.com>
2026-06-29 21:45:10 +0530

    refactor: update Find the City With the Smallest Number of Neighbors at a Threshold Distance (medium)

--- solutions/medium/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
+++ solutions/medium/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
@@ -1,11 +1,12 @@
 # ─────────────────────────────────────────────────
 #  Problem : 1334. Find the City With the Smallest Number of Neighbors at a Threshold Distance
 #  Difficulty : Medium
-#  Runtime  : 1202 ms
-#  Memory   : 12.8 MB
+#  Runtime  : 292 ms
+#  Memory   : 13.4 MB
 #  Solved   : 2026-06-29
 # ─────────────────────────────────────────────────
 
+from heapq import heappush, heappop
 class Solution(object):
     def findTheCity(self, n, edges, distanceThreshold):
         """
@@ -14,28 +15,29 @@ class Solution(object):
         :type distanceThreshold: int
         :rtype: int
         """
-        dist=[[float('inf')]*n for _ in range(n)]
+        adj_list=[[] for _ in range(n)]
         for u,v,w in edges:
-            dist[u][v]=w
-            dist[v][u]=w
-        for k in range(n):
-            for i in range(n):
-                for j in range(n):
-                    if i==j:
-                        dist[i][j]=0
-                        continue
-                    if dist[i][k]!=float('inf') and dist[k][j]!=float('inf'):
-
-                        dist[i][j]=min(dist[i][j],
-                                    dist[i][k]+dist[k][j])
-        city_count=n
+            adj_list[u].append((v,w))
+            adj_list[v].append((u,w))
+        count_cities=n
         city_no=0
-        for i in range(n):
+        for src in range(n):
+            queue=[]
+            dist=[float('inf')]*n
+            dist[src]=0
+            heappush(queue,(0,src))
+            
+            while queue:
+                step,node=heappop(queue)
+                for v,w in adj_list[node]:
+                    if step+w<dist[v]:
+                        dist[v]=step+w
+                        heappush(queue,(dist[v],v))
+            dist[src]=1e9
             count=0
-            for j in range(n):
-                if dist[i][j]<=distanceThreshold :
-                    count+=1
-            if count<=city_count:
-                city_count=count
-                city_no=i
-        return city_no
+            for d in dist:
+                if d<=distanceThreshold: count+=1
+            if count<=count_cities:
+                count_cities=count
+                city_no=src
+        return city_no 
