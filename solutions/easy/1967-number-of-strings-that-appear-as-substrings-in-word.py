415ef51e3a4d608620793570de9c3b42ef3d57b0
Kushul Aleti <kushulaleti@gmail.com>
2026-06-29 09:09:25 +0530

    refactor: update Number of Strings That Appear as Substrings in Word (easy)

--- solutions/easy/1967-number-of-strings-that-appear-as-substrings-in-word.py
+++ solutions/easy/1967-number-of-strings-that-appear-as-substrings-in-word.py
@@ -1,8 +1,8 @@
 # ─────────────────────────────────────────────────
 #  Problem : 1967. Number of Strings That Appear as Substrings in Word
 #  Difficulty : Easy
-#  Runtime  : 0 ms
-#  Memory   : 12.5 MB
+#  Runtime  : 19 ms
+#  Memory   : 12.8 MB
 #  Solved   : 2026-06-29
 # ─────────────────────────────────────────────────
 
@@ -13,8 +13,14 @@ class Solution(object):
         :type word: str
         :rtype: int
         """
+        sub_string=set()
+        for i in range(len(word)):
+            string=''
+            for j in range(i,len(word)):
+                string+=word[j]
+                sub_string.add(string)
         count=0
         for pattern in patterns:
-            if pattern in word:
+            if pattern in sub_string:
                 count+=1
         return count
