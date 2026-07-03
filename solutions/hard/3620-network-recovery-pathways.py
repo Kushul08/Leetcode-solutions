b68ea6cbcf9fc39a536a9a4035aed857fc7e4a71
Kushul Aleti <kushulaleti@gmail.com>
2026-07-03 20:09:30 +0530

    refactor: update Network Recovery Pathways (hard)

--- solutions/hard/3620-network-recovery-pathways.py
+++ solutions/hard/3620-network-recovery-pathways.py
@@ -1,8 +1,8 @@
 # ─────────────────────────────────────────────────
 #  Problem : 3620. Network Recovery Pathways
 #  Difficulty : Hard
-#  Runtime  : 0 ms
-#  Memory   : 12.5 MB
+#  Runtime  : 52 ms
+#  Memory   : 26.2 MB
 #  Solved   : 2026-07-03
 # ─────────────────────────────────────────────────
 
@@ -19,23 +19,22 @@ class Solution(object):
         adj_list=[[] for _ in range(n)]
         for u,v,w in edges:
             adj_list[u].append((v,w))
-        queue=deque()
-        queue.append((0,0,float('inf')))
-        costs=[float('inf')]*n
-        costs[0]=0
-        ans=float('-inf')
+        
 
         def check(num):
             queue=[]
             heappush(queue,(0,0))
-
+            dis=[float('inf')]*n
+            dis[0]=0
             while queue:
                 cost,node=heappop(queue)
-                if node==n-1 and cost<=k:
+                if node==n-1:
                     return True
                 for v,w in adj_list[node]:
                     if w>=num and online[v]==True and cost+w<=k:
-                        heappush(queue,(cost+w,v))
+                        if cost+w<dis[v]:
+                            dis[v]=cost+w
+                            heappush(queue,(cost+w,v))
             return False
         low=0
         high=int(1e9)
