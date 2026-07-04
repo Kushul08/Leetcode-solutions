65a6e432f94eb1e1554ed6256ef234044a174177
Kushul Aleti <kushulaleti@gmail.com>
2026-07-04 13:24:43 +0530

    refactor: update Minimum Lights to Illuminate a Road (medium)

--- solutions/medium/3964-minimum-lights-to-illuminate-a-road.py
+++ solutions/medium/3964-minimum-lights-to-illuminate-a-road.py
@@ -1,37 +1,37 @@
 # ─────────────────────────────────────────────────
 #  Problem : 3964. Minimum Lights to Illuminate a Road
 #  Difficulty : Medium
-#  Runtime  : 506 ms
-#  Memory   : 35.6 MB
+#  Runtime  : 798 ms
+#  Memory   : 32.1 MB
 #  Solved   : 2026-07-04
 # ─────────────────────────────────────────────────
 
-import math
 class Solution:
     def minLights(self, lights: list[int]) -> int:
         n=len(lights)
-        nums=set(i for i in range(len(lights)))
-        for i in range(len(lights)):
-            # print('hello',lights[i])
-            if lights[i]!=0:
-                start=max(0,i-lights[i])
-                end=min(n-1,i+lights[i])
-                if start  not in nums and end not in nums: continue
-                for val in range(start,end+1):
-                    if val in nums:
-                        nums.remove(val)
-        count=0
-        nums=list(nums)
-        if not nums: return 0
-        nums.append(int(1e9))
-        min_lights=0
+        diff=[0]*(n+1)
+        for i,light in enumerate(lights):
+            if light:
+                l=max(0,i-light)
+                r=min(n-1,i+light)
+                diff[l]+=1
+                diff[r+1]-=1
+        prefix=[diff[0]]
+        for i in range(1,len(diff)):
+            prefix.append(diff[i]+prefix[-1])
+        print(diff,prefix)
+        
         
 
-
-        for i in range(1,len(nums)):
-            if nums[i-1]+1==nums[i]:
+        ans=count=0
+        for i,num in enumerate(prefix):
+            if i==len(prefix)-1: break
+            if num==0:
                 count+=1
             else:
-                min_lights+=(1 if count+1<=2 else math.ceil((count+1)/3))
+                print(count)
+                ans+=math.ceil(count/3)
                 count=0
-        return min_lights
+        if count:
+            ans+=math.ceil(count/3)
+        return ans
