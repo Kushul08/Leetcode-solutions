e6c39fc1e7632fc3151807ce59b58be4dbdd9d0e
Kushul Aleti <kushulaleti@gmail.com>
2026-07-06 15:45:13 +0530

    refactor: update Remove Covered Intervals (medium)

--- solutions/medium/1288-remove-covered-intervals.py
+++ solutions/medium/1288-remove-covered-intervals.py
@@ -1,8 +1,8 @@
 # ─────────────────────────────────────────────────
 #  Problem : 1288. Remove Covered Intervals
 #  Difficulty : Medium
-#  Runtime  : 0 ms
-#  Memory   : 12.3 MB
+#  Runtime  : 5 ms
+#  Memory   : 12.6 MB
 #  Solved   : 2026-07-06
 # ─────────────────────────────────────────────────
 
@@ -17,6 +17,10 @@ class Solution(object):
         for interval in intervals:
             if stack and stack[-1][0]<=interval[0] and stack[-1][1]>=interval[1]:
                 continue
+            elif stack and stack[-1][0]==interval[0] and stack[-1][1]<interval[1]:
+                while stack and stack[-1][0]==interval[0] and stack[-1][1]<interval[1]:
+                    stack.pop()
+                stack.append(interval)
             else:
                 stack.append(interval)
         return len(stack)
