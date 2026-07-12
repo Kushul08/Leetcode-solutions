c305a83bef54f1ffa9637616c12e7d752c38cfd4
Kushul Aleti <kushulaleti@gmail.com>
2026-07-12 11:45:58 +0530

    refactor: update Minimum Total Cost to Process All Elements (medium)

--- solutions/medium/3987-minimum-total-cost-to-process-all-elements.py
+++ solutions/medium/3987-minimum-total-cost-to-process-all-elements.py
@@ -1,11 +1,12 @@
 # ─────────────────────────────────────────────────
 #  Problem : 3987. Minimum Total Cost to Process All Elements
 #  Difficulty : Medium
-#  Runtime  : 0 ms
-#  Memory   : 19.3 MB
+#  Runtime  : 321 ms
+#  Memory   : 32.9 MB
 #  Solved   : 2026-07-12
 # ─────────────────────────────────────────────────
 
+import math
 class Solution:
     def minimumCost(self, nums: list[int], k: int) -> int:
         available=k
@@ -17,10 +18,8 @@ class Solution:
                 diff=num-available
                 required=math.ceil(diff/k)
                 available+=(k*required)
-                # print(required,ans,diff,available)
-                for _ in range(required):
-                    ans+=step
-                    step+=1
-                    ans%=MOD
+                ans+=(((required+step-1)*(required+step))//2-((step*(step-1))//2))%MOD
+                step+=required
             available-=num    
+        # ans=int(ans)
         return ans%MOD
