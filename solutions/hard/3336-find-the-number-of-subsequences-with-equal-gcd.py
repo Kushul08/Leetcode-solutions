acaad6d2fd46b9e9b97c966e06f7e1fa8881cc95
Kushul Aleti <kushulaleti@gmail.com>
2026-07-14 20:52:05 +0530

    solve: 3336. Find the Number of Subsequences With Equal GCD [hard] (py)

---
+++ solutions/hard/3336-find-the-number-of-subsequences-with-equal-gcd.py
@@ -0,0 +1,24 @@
+# ─────────────────────────────────────────────────
+#  Problem : 3336. Find the Number of Subsequences With Equal GCD
+#  Difficulty : Hard
+#  Runtime  : 3 ms
+#  Memory   : 19.9 MB
+#  Solved   : 2026-07-14
+# ─────────────────────────────────────────────────
+
+from functools import cache
+class Solution:
+    def subsequencePairCount(self, nums: List[int]) -> int:
+
+        @cache
+        def subsequence(i,g1,g2):
+            if i==len(nums):
+                if g1==g2 and g1!=0:
+                    return 1
+                return 0
+
+            seq1=subsequence(i+1,math.gcd(g1,nums[i]),g2)
+            seq2=subsequence(i+1,g1,math.gcd(g2,nums[i]))
+            ignore=subsequence(i+1,g1,g2)
+            return seq1+seq2+ignore
+        return subsequence(0,0,0)
