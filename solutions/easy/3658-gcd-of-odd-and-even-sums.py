1cf0239b00f2227e5f53c2955080f0e73aabb721
Kushul Aleti <kushulaleti@gmail.com>
2026-07-15 07:51:44 +0530

    refactor: update GCD of Odd and Even Sums (easy)

--- solutions/easy/3658-gcd-of-odd-and-even-sums.py
+++ solutions/easy/3658-gcd-of-odd-and-even-sums.py
@@ -1,19 +1,14 @@
 # ─────────────────────────────────────────────────
 #  Problem : 3658. GCD of Odd and Even Sums
 #  Difficulty : Easy
-#  Runtime  : 34 ms
-#  Memory   : 19.5 MB
+#  Runtime  : 0 ms
+#  Memory   : 19.2 MB
 #  Solved   : 2026-07-15
 # ─────────────────────────────────────────────────
 
-from math import gcd
 class Solution:
     def gcdOfOddEvenSums(self, n: int) -> int:
-        even=0
-        odd=0
-        for i in range(1,2*n+1):
-            if i&1==0:
-                even+=i
-            else:
-                odd+=i
-        return math.gcd(odd,even)
+        # even= sum of even numbers upto n is n*(n+1)
+        # odd=sum of odd numbers upto n is n*n
+        # and there gcd is n
+        return n
