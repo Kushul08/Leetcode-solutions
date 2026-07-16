f2ebc59007da60c8f209021487d7861f6389a923
Kushul Aleti <kushulaleti@gmail.com>
2026-07-16 17:29:45 +0530

    refactor: update Number of Islands II (hard)

--- solutions/hard/0305-number-of-islands-ii.py
+++ solutions/hard/0305-number-of-islands-ii.py
@@ -1,8 +1,8 @@
 # ─────────────────────────────────────────────────
 #  Problem : 0305. Number of Islands II
 #  Difficulty : Hard
-#  Runtime  : 257 ms
-#  Memory   : 19.5 MB
+#  Runtime  : 156 ms
+#  Memory   : 18.7 MB
 #  Solved   : 2026-07-16
 # ─────────────────────────────────────────────────
 
@@ -10,20 +10,18 @@ class DSU:
     def __init__(self,m,n):
         self.rank={}
         self.parent={}
-        for i in range(m):
-            for j in range(n):
-                self.parent[(i,j)]=(i,j)
-                self.rank[(i,j)]=0
     def find_par(self,node):
         if self.parent[node]==node:
             return node
         self.parent[node]=self.find_par(self.parent[node])
         return self.parent[node]
     def union(self,n1,n2):
+
         ulp_u=self.find_par(n1)
         ulp_v=self.find_par(n2)
+        
         if ulp_u==ulp_v:
-            return
+            return False
         if self.rank[ulp_u]<self.rank[ulp_v]:
             self.parent[ulp_u]=ulp_v
         elif self.rank[ulp_u]>self.rank[ulp_v]:
@@ -31,6 +29,7 @@ class DSU:
         else:
             self.parent[ulp_u]=ulp_v
             self.rank[ulp_v]+=1
+        return True
 class Solution(object):
     def numIslands2(self, m, n, positions):
         """
@@ -43,18 +42,18 @@ class Solution(object):
         dsu=DSU(m,n)
         count=0
         ans=[]
-        grid=[[0]*n for _ in range(m)]
         for x,y in positions:
-            if grid[x][y]==1:
+            if (x,y) in dsu.parent:
                 ans.append(count)
                 continue
+            dsu.parent[(x,y)]=(x,y)
+            dsu.rank[(x,y)]=0
             count+=1
-            grid[x][y]=1
+            # grid[x][y]=1
             for dx,dy in directions:
                 nx,ny=x+dx,y+dy
-                if 0<=nx<m and 0<=ny<n:
-                    if grid[nx][ny]==1 and dsu.find_par((x,y))!=dsu.find_par((nx,ny)):
+                if 0<=nx<m and 0<=ny<n and (nx,ny) in dsu.parent:
+                    if dsu.union((nx,ny),(x,y)):
                         count-=1
-                        dsu.union((nx,ny),(x,y))
             ans.append(count)
         return ans
