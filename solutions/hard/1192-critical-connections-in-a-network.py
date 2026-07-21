c2ee425be7c3027922928e66d814f7e6ff875b5a
Kushul Aleti <kushulaleti@gmail.com>
2026-07-21 17:02:28 +0530

    solve: 1192. Critical Connections in a Network [hard] (py)

---
+++ solutions/hard/1192-critical-connections-in-a-network.py
@@ -0,0 +1,38 @@
+# ─────────────────────────────────────────────────
+#  Problem : 1192. Critical Connections in a Network
+#  Difficulty : Hard
+#  Runtime  : 0 ms
+#  Memory   : 19.2 MB
+#  Solved   : 2026-07-21
+# ─────────────────────────────────────────────────
+
+class Solution:
+    def criticalConnections(self, n: int, connections: List[List[int]]) -> List[List[int]]:
+        adj_list=[[] for _ in range(n)]
+        for u,v in connections:
+            adj_list[u].append(v)
+            adj_list[v].append(u)
+        # print(adj_list)
+        # return [[1,0],[0,1]]
+        visited=[0]*n
+        bridges=[]
+        timer=1
+        low=[0]*n
+        tin=[0]*n   
+        def dfs(node,parent,low,tin):
+            nonlocal timer
+            visited[node]=1
+            low[node]=timer
+            tin[node]=timer
+            timer+=1
+            for it in adj_list[node]:
+                if it==parent: continue
+                if visited[it]==0:
+                    dfs(it,node,low,tin)
+                    low[node]=min(low[node],low[it])
+                    if low[it]>low[node]:
+                        bridges.append([node,it])
+                else:
+                    low[node]=min(low[node],low[it])
+        dfs(0,-1,low,tin)
+        return bridges
