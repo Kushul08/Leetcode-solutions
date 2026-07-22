be82e7392fd25cbcdb238e4e8d8614bd499a0612
Kushul Aleti <kushulaleti@gmail.com>
2026-07-22 18:49:43 +0530

    refactor: update Maximum Number of Non-Overlapping Substrings (hard)

--- solutions/hard/0001-maximum-number-of-non-overlapping-substrings.py
+++ solutions/hard/0001-maximum-number-of-non-overlapping-substrings.py
@@ -1,8 +1,8 @@
 # ─────────────────────────────────────────────────
 #  Problem : 0001. Maximum Number of Non-Overlapping Substrings
 #  Difficulty : Hard
-#  Runtime  : 299 ms
-#  Memory   : 13.3 MB
+#  Runtime  : 295 ms
+#  Memory   : 13.2 MB
 #  Solved   : 2026-07-22
 # ─────────────────────────────────────────────────
 
@@ -21,36 +21,27 @@ class Solution(object):
         def getInterval(ch):
             L = hashmap[ch][0]
             R = hashmap[ch][1]
-
             i = L
-
             while i <= R:
                 c = s[i]
-
                 if hashmap[c][0]< L:
-                    return None      # invalid interval
-
+                    return None     
                 R = max(R, hashmap[c][1])
-
                 i += 1
-
             return (L, R)
-        intervals={}
+        intervals=[]
         for ch in hashmap:
             interval=getInterval(ch)
             if interval is not None:
-                intervals[ch]=interval
+                intervals.append(interval)
         # Now the problem simplifies to max no of intervals which are non overlapping
-        # print(intervals)
-        intervals=sorted(intervals.values(), key=lambda item:item[1])
-        # print(intervals)
+        intervals=sorted(intervals, key=lambda item:item[1])
         max_intervals=[]
         current=-1
         for a,b in intervals:
             if a>current:
                 max_intervals.append([a,b])
                 current=b
-        # print(max_intervals)
         substrings=[]
         for a,b in max_intervals:
             substrings.append(s[a:b+1])
