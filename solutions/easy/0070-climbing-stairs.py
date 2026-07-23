4d2bf35dbb5d59dd903a40d9d05b4f4f4e41822d
Kushul Aleti <kushulaleti@gmail.com>
2026-07-23 16:26:41 +0530

    refactor: update Climbing Stairs (easy)

--- solutions/easy/0070-climbing-stairs.py
+++ solutions/easy/0070-climbing-stairs.py
@@ -2,12 +2,14 @@
 #  Problem : 0070. Climbing Stairs
 #  Difficulty : Easy
 #  Runtime  : 0 ms
-#  Memory   : 19.2 MB
+#  Memory   : 19.3 MB
 #  Solved   : 2026-07-23
 # ─────────────────────────────────────────────────
 
+from functools import lru_cache
 class Solution:
     def climbStairs(self, n: int) -> int:
+        @lru_cache
         def climb(curr):
             if curr>n:
                 return 0
