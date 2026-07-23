3a3522b71a6d99a43a22c9f710dbe62a1b35395f
Kushul Aleti <kushulaleti@gmail.com>
2026-07-23 16:50:14 +0530

    refactor: update Climbing Stairs (easy)

--- solutions/easy/0070-climbing-stairs.py
+++ solutions/easy/0070-climbing-stairs.py
@@ -7,10 +7,10 @@
 # ─────────────────────────────────────────────────
 
 class Solution:
+    dp=[0]*46
     def climbStairs(self, n: int) -> int:
-        dp=[0]*46
-        dp[1]=1
-        dp[2]=2
+        self.dp[1]=1
+        self.dp[2]=2
         for i in range(3,n+1):
-            dp[i]=dp[i-1]+dp[i-2]
-        return dp[n]
+            self.dp[i]=self.dp[i-1]+self.dp[i-2]
+        return self.dp[n]
