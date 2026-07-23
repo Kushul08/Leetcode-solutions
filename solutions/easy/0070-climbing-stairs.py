35c81e9f4a47a33b4999d85c1b69b4fecb5fb068
Kushul Aleti <kushulaleti@gmail.com>
2026-07-23 16:52:05 +0530

    refactor: update Climbing Stairs (easy)

--- solutions/easy/0070-climbing-stairs.py
+++ solutions/easy/0070-climbing-stairs.py
@@ -2,15 +2,15 @@
 #  Problem : 0070. Climbing Stairs
 #  Difficulty : Easy
 #  Runtime  : 0 ms
-#  Memory   : 19.2 MB
+#  Memory   : 19.4 MB
 #  Solved   : 2026-07-23
 # ─────────────────────────────────────────────────
 
 class Solution:
-    dp=[0]*46
     def climbStairs(self, n: int) -> int:
-        self.dp[1]=1
-        self.dp[2]=2
+        a=1
+        b=2
         for i in range(3,n+1):
-            self.dp[i]=self.dp[i-1]+self.dp[i-2]
-        return self.dp[n]
+            c=a+b
+            a,b=b,c
+        return b
