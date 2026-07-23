e60c472c4f3033aba4a08c273e467dc6b28aa7f3
Kushul Aleti <kushulaleti@gmail.com>
2026-07-23 16:52:35 +0530

    refactor: update Climbing Stairs (easy)

--- solutions/easy/0070-climbing-stairs.py
+++ solutions/easy/0070-climbing-stairs.py
@@ -2,7 +2,7 @@
 #  Problem : 0070. Climbing Stairs
 #  Difficulty : Easy
 #  Runtime  : 0 ms
-#  Memory   : 19.4 MB
+#  Memory   : 19.2 MB
 #  Solved   : 2026-07-23
 # ─────────────────────────────────────────────────
 
@@ -10,6 +10,8 @@ class Solution:
     def climbStairs(self, n: int) -> int:
         a=1
         b=2
+        if n<=2:
+            return n
         for i in range(3,n+1):
             c=a+b
             a,b=b,c
