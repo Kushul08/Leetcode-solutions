d43daafffb22a1b503836ca20bb1306cfc3f9fe1
Kushul Aleti <kushulaleti@gmail.com>
2026-07-25 12:10:44 +0530

    refactor: update Maximum Value of an Alternating Sequence (medium)

--- solutions/medium/3993-maximum-value-of-an-alternating-sequence.cpp
+++ solutions/medium/3993-maximum-value-of-an-alternating-sequence.cpp
@@ -2,13 +2,16 @@
 //  Problem : 3993. Maximum Value of an Alternating Sequence
 //  Difficulty : Medium
 //  Runtime  : 0 ms
-//  Memory   : 7.9 MB
+//  Memory   : 9 MB
 //  Solved   : 2026-07-25
 // ─────────────────────────────────────────────────
 
 class Solution {
 public:
     long long maximumValue(int n, int s, int m) {
+        if (n==1){
+            return s;
+        }
         int m_count=n/2;
         int one_count=(n-1)/2;
         if (n%2==0){ return static_cast<long>(s)+static_cast<long>(m)*m_count-one_count;}
