afafc4d9c9e7f4685b2b06f8c28ba820f4290ce5
Kushul Aleti <kushulaleti@gmail.com>
2026-07-26 11:31:15 +0530

    refactor: update Maximum Product of Three Numbers (easy)

--- solutions/easy/0628-maximum-product-of-three-numbers.py
+++ solutions/easy/0628-maximum-product-of-three-numbers.py
@@ -1,8 +1,8 @@
 # ─────────────────────────────────────────────────
 #  Problem : 0628. Maximum Product of Three Numbers
 #  Difficulty : Easy
-#  Runtime  : 0 ms
-#  Memory   : 12.3 MB
+#  Runtime  : 39 ms
+#  Memory   : 13.3 MB
 #  Solved   : 2026-07-26
 # ─────────────────────────────────────────────────
 
@@ -15,4 +15,4 @@ class Solution(object):
         # 1) Just sort in ascending order and pick the last 3 
         # 2) Use the heapfy method from heapq, and pop three times, but we need to use -ve sign to get convert it to max heap from min heap
         nums.sort()
-        return nums[-1]*nums[-2]*nums[-3]
+        return max(nums[-1]*nums[-2]*nums[-3],nums[0]*nums[1]*nums[-1])
