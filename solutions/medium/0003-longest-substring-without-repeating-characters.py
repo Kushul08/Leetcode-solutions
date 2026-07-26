888ed1521b8c49889f27414324884fb3bc1a840e
Kushul Aleti <kushulaleti@gmail.com>
2026-07-26 12:18:38 +0530

    refactor: update Longest Substring Without Repeating Characters (medium)

--- solutions/medium/0003-longest-substring-without-repeating-characters.py
+++ solutions/medium/0003-longest-substring-without-repeating-characters.py
@@ -1,8 +1,8 @@
 # ─────────────────────────────────────────────────
 #  Problem : 0003. Longest Substring Without Repeating Characters
 #  Difficulty : Medium
-#  Runtime  : 0 ms
-#  Memory   : 12.4 MB
+#  Runtime  : 38 ms
+#  Memory   : 12.5 MB
 #  Solved   : 2026-07-26
 # ─────────────────────────────────────────────────
 
@@ -13,20 +13,20 @@ class Solution(object):
         :rtype: int
         """
         l=0
-        r=1
+        r=0
         n=len(s)
         if n==0:
             return 0
         max_=1
-        hashmap={s[0]:1}
+        hashmap={}
         while r<n:
             while l<=r and (r-l)!=len(hashmap):
-                # print(hashmap,l,r)
                 hashmap[s[l]]-=1
                 if hashmap[s[l]]==0:
                     del hashmap[s[l]]
                 l+=1
             hashmap[s[r]]=hashmap.get(s[r],0)+1
-            max_=max(max_,r-l)
+            if r-l+1==len(hashmap):
+                max_=max(max_,r-l+1)
             r+=1
         return max_
