6349f02e85aabdc33a7f86f737fc90f7928d5277
Kushul Aleti <kushulaleti@gmail.com>
2026-07-27 13:13:00 +0530

    refactor: update House Robber (medium)

--- solutions/medium/0198-house-robber.py
+++ solutions/medium/0198-house-robber.py
@@ -2,14 +2,17 @@
 #  Problem : 0198. House Robber
 #  Difficulty : Medium
 #  Runtime  : 0 ms
-#  Memory   : 19.2 MB
+#  Memory   : 19.3 MB
 #  Solved   : 2026-07-27
 # ─────────────────────────────────────────────────
 
+from functools import lru_cache
 class Solution:
     def rob(self, nums: List[int]) -> int:
         
         n=len(nums)
+
+        @lru_cache
         def recur(i):
             if i>=n:
                 return 0
