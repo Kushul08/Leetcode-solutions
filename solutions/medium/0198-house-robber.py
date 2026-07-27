fd78e82f2bedc221d584218103e6f87a63397e5e
Kushul Aleti <kushulaleti@gmail.com>
2026-07-27 13:56:54 +0530

    refactor: update House Robber (medium)

--- solutions/medium/0198-house-robber.py
+++ solutions/medium/0198-house-robber.py
@@ -2,7 +2,7 @@
 #  Problem : 0198. House Robber
 #  Difficulty : Medium
 #  Runtime  : 0 ms
-#  Memory   : 19.4 MB
+#  Memory   : 19.3 MB
 #  Solved   : 2026-07-27
 # ─────────────────────────────────────────────────
 
@@ -14,8 +14,6 @@ class Solution:
         dp=[0]*n
         dp[0]=nums[0]
         dp[1]=nums[1]
-        max_val=max(dp[0],dp[1])
         for i in range(2,n):
             dp[i]=max(dp[i-2],dp[max(0,i-3)])+nums[i]
-            max_val=max(max_val,dp[i])
-        return max_val
+        return max(dp[-1],dp[-2])
