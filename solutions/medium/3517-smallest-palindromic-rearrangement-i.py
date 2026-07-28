a36490e31de4339f06365f4db48f254adb9556db
Kushul Aleti <kushulaleti@gmail.com>
2026-07-28 14:01:24 +0530

    refactor: update Smallest Palindromic Rearrangement I (medium)

--- solutions/medium/3517-smallest-palindromic-rearrangement-i.py
+++ solutions/medium/3517-smallest-palindromic-rearrangement-i.py
@@ -1,24 +1,25 @@
 # ─────────────────────────────────────────────────
 #  Problem : 3517. Smallest Palindromic Rearrangement I
 #  Difficulty : Medium
-#  Runtime  : 796 ms
-#  Memory   : 13.7 MB
+#  Runtime  : 347 ms
+#  Memory   : 13.8 MB
 #  Solved   : 2026-07-28
 # ─────────────────────────────────────────────────
 
-from collections import Counter
 class Solution(object):
     def smallestPalindrome(self, s):
         """
         :type s: str
         :rtype: str
         """
-        counter=Counter(s)
-        left=middle=right=''
+        counter=defaultdict(int)
+        for ch in s:
+            counter[ch]+=1
+        double=''
+        single=''
         new_s=sorted(set(s))
         for ch in new_s:
-            left+=ch*(counter[ch]/2)
+            double+=ch*(counter[ch]/2)
             if counter[ch]%2!=0:
-                middle+=ch
-            right+=ch*(counter[ch]/2)
-        return left+middle+right[::-1]
+                single+=ch
+        return double+single+double[::-1]
