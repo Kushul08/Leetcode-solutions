3868df703ba753b982b8db6a6a10dd7af02d3917
Kushul Aleti <kushulaleti@gmail.com>
2026-07-28 12:28:19 +0530

    refactor: update House Robber II (medium)

--- solutions/medium/0213-house-robber-ii.py
+++ solutions/medium/0213-house-robber-ii.py
@@ -2,7 +2,7 @@
 #  Problem : 0213. House Robber II
 #  Difficulty : Medium
 #  Runtime  : 0 ms
-#  Memory   : 12.5 MB
+#  Memory   : 12.3 MB
 #  Solved   : 2026-07-28
 # ─────────────────────────────────────────────────
 
@@ -14,13 +14,13 @@ class Solution(object):
         """
         if len(nums)<=3:
             return max(nums)
-        def house_rob(nums):
-            n=len(nums)
-            first=nums[0]
-            second=nums[1]
-            third=max(second,nums[0]+nums[2])
-            for i in range(3,n):
-                curr=max(second,first)+nums[i]
+        def house_rob(start,end):
+            n=end
+            first=nums[start]
+            second=nums[start+1]
+            third=max(second,nums[start]+nums[start+2])
+            for i in range(3,n-start):
+                curr=max(second,first)+nums[start+i]
                 first,second,third=second,third,curr
             return max(second,third)
-        return max(house_rob(nums[:-1]),house_rob(nums[1:]))
+        return max(house_rob(0,len(nums)-1),house_rob(1,len(nums)))
