2840c78a7b63a4f87481f17a031d3e66c2e66d8d
Kushul Aleti <kushulaleti@gmail.com>
2026-07-29 21:08:58 +0530

    solve: 0063. Unique Paths II [medium] (py)

---
+++ solutions/medium/0063-unique-paths-ii.py
@@ -0,0 +1,23 @@
+# ─────────────────────────────────────────────────
+#  Problem : 0063. Unique Paths II
+#  Difficulty : Medium
+#  Runtime  : 0 ms
+#  Memory   : 12.5 MB
+#  Solved   : 2026-07-29
+# ─────────────────────────────────────────────────
+
+class Solution(object):
+    def uniquePathsWithObstacles(self, obstacleGrid):
+        """
+        :type obstacleGrid: List[List[int]]
+        :rtype: int
+        """
+        n,m=len(obstacleGrid),len(obstacleGrid[0])
+        dp=[[1]*m for _ in range(n)]
+        for i in range(1,n):
+            for j in range(1,m):
+                if obstacleGrid[i][j]==1: continue
+                up=dp[i-1][j] if obstacleGrid[i-1][j]==0 else 0
+                left=dp[i][j-1] if obstacleGrid[i][j-1]==0 else 0
+                dp[i][j]=up+left
+        return dp[n-1][m-1] if obstacleGrid[n-1][m-1]==0 else 0
