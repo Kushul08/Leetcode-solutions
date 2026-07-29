9b7e264e4379e7fa0aea188805c09ba14e0ee75a
Kushul Aleti <kushulaleti@gmail.com>
2026-07-29 13:36:01 +0530

    refactor: update Smallest Palindromic Rearrangement II (hard)

--- solutions/hard/3518-smallest-palindromic-rearrangement-ii.py
+++ solutions/hard/3518-smallest-palindromic-rearrangement-ii.py
@@ -1,8 +1,8 @@
 # ─────────────────────────────────────────────────
 #  Problem : 3518. Smallest Palindromic Rearrangement II
 #  Difficulty : Hard
-#  Runtime  : 0 ms
-#  Memory   : 19.7 MB
+#  Runtime  : 31 ms
+#  Memory   : 61.7 MB
 #  Solved   : 2026-07-29
 # ─────────────────────────────────────────────────
 
@@ -10,6 +10,9 @@ import math
 class Solution:
     def smallestPalindrome(self, s: str, k: int) -> str:
         n=len(s)
+        fact=[1]*(n+1)
+        for i in range(1,n+1):
+            fact[i]=fact[i-1]*i
         counter=defaultdict(int)
         for ch in s:
             counter[ch]+=1
@@ -27,27 +30,33 @@ class Solution:
 
         denom=1
         for ch in lc:
-            denom*=math.factorial(lc[ch])
-        unique_palindromes=(math.factorial(L))//denom
+            denom*=fact[lc[ch]]
+        unique_palindromes=fact[L]//denom
 
         if k>unique_palindromes:
             return ""
 
         
+        denominator=1
+        for char in lc:
+            denominator*=fact[lc[char]]
+        
         ans=''
         while L>0:
             for ch in string.ascii_lowercase:
                 if ch not in lc: continue
-                denominator=1
-                for char in lc:
-                    if char==ch:
-                        denominator*=math.factorial(lc[char]-1)
-                    else:
-                        denominator*=math.factorial(lc[char])
-                count=(math.factorial(L-1))//denominator
+                # denominator=1
+                # for char in lc:
+                #     if char==ch:
+                #         denominator*=math.factorial(lc[char]-1)
+                #     else:
+                #         denominator*=math.factorial(lc[char])
+                new=denominator//lc[ch]
+                count=fact[L-1]//new
                 if count<k: # if k==count we not take because we can't build it if we pick so
                     k-=count  
                 else:
+                    denominator=new
                     ans+=ch
                     lc[ch]-=1
                     if lc[ch]==0:
