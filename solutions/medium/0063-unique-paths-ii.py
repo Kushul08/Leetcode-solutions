ac1c034d82f2901bea3a7a3b3747c028e8f1ea6c
Kushul Aleti <kushulaleti@gmail.com>
2026-07-29 21:09:03 +0530

    refactor: update Unique Paths II (medium)

--- solutions/medium/0063-unique-paths-ii.py
+++ solutions/medium/0063-unique-paths-ii.py
@@ -13,7 +13,17 @@ class Solution(object):
         :rtype: int
         """
         n,m=len(obstacleGrid),len(obstacleGrid[0])
-        dp=[[1]*m for _ in range(n)]
+        if obstacleGrid[0][0]==1:
+            return 0
+        dp=[[0]*m for _ in range(n)]
+        for i in range(m):
+            if obstacleGrid[0][i]==1:
+                break
+            dp[0][i]=1
+        for i in range(n):
+            if obstacleGrid[i][0]==1:
+                break
+            dp[i][0]=1
         for i in range(1,n):
             for j in range(1,m):
                 if obstacleGrid[i][j]==1: continue
