59058dc34e981ca97656ed0b3e61d36da45e4297
Kushul Aleti <kushulaleti@gmail.com>
2026-07-30 12:42:01 +0530

    refactor: update Unique Paths II (medium)

--- solutions/medium/0063-unique-paths-ii.py
+++ solutions/medium/0063-unique-paths-ii.py
@@ -1,9 +1,9 @@
 # ─────────────────────────────────────────────────
 #  Problem : 0063. Unique Paths II
 #  Difficulty : Medium
-#  Runtime  : 2 ms
-#  Memory   : 12.4 MB
-#  Solved   : 2026-07-29
+#  Runtime  : 0 ms
+#  Memory   : 12.2 MB
+#  Solved   : 2026-07-30
 # ─────────────────────────────────────────────────
 
 class Solution(object):
@@ -15,19 +15,21 @@ class Solution(object):
         n,m=len(obstacleGrid),len(obstacleGrid[0])
         if obstacleGrid[0][0]==1 or obstacleGrid[n-1][m-1]==1:
             return 0
-        dp=[[0]*m for _ in range(n)]
+        dp=[0]*m 
         for i in range(m):
             if obstacleGrid[0][i]==1:
                 break
-            dp[0][i]=1
+            dp[i]=1
+        row=0
         for i in range(n):
             if obstacleGrid[i][0]==1:
+                row=i
                 break
-            dp[i][0]=1
         for i in range(1,n):
             for j in range(1,m):
                 if obstacleGrid[i][j]==1: continue
-                up=dp[i-1][j] if obstacleGrid[i-1][j]==0 else 0
-                left=dp[i][j-1] if obstacleGrid[i][j-1]==0 else 0
-                dp[i][j]=up+left
-        return dp[n-1][m-1] 
+                up=dp[j] if obstacleGrid[i-1][j]==0 else 0
+                left=dp[j-1] if obstacleGrid[i][j-1]==0 else 0
+                dp[j]=up+left
+                left=dp[j]
+        return dp[m-1]
