bd2c108f06b43a726db737eac53595f7c38e85a8
Kushul Aleti <kushulaleti@gmail.com>
2026-07-30 15:34:52 +0530

    refactor: update Triangle (medium)

--- solutions/medium/0120-triangle.py
+++ solutions/medium/0120-triangle.py
@@ -1,8 +1,8 @@
 # ─────────────────────────────────────────────────
 #  Problem : 0120. Triangle
 #  Difficulty : Medium
-#  Runtime  : 7 ms
-#  Memory   : 13.5 MB
+#  Runtime  : 4 ms
+#  Memory   : 13.4 MB
 #  Solved   : 2026-07-30
 # ─────────────────────────────────────────────────
 
@@ -14,19 +14,11 @@ class Solution(object):
         """
         n=len(triangle)
         dp=[[0]*i for i in range(1,n+1)]
-        # print(dp)
         dp[0][0]=triangle[0][0]
 
         for i in range(1,n):
             for j in range(len(triangle[i])):
-                if 0<=j-1:
-                    left=dp[i-1][j-1]
-                else:
-                    left=1e9
-                if j<len(triangle[i-1]):
-                    up=dp[i-1][j]
-                else:
-                    up=1e9
+                left=dp[i-1][j-1] if 0<=j-1 else 1e9
+                up=dp[i-1][j] if j<len(triangle[i-1]) else 1e9
                 dp[i][j]=min(left,up)+triangle[i][j]
-        # print(dp)
         return min(dp[n-1])
