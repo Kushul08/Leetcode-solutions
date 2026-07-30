63252fced766a94839e246787aa196c3d8eafd4c
Kushul Aleti <kushulaleti@gmail.com>
2026-07-30 15:33:50 +0530

    refactor: update Triangle (medium)

--- solutions/medium/0120-triangle.py
+++ solutions/medium/0120-triangle.py
@@ -1,22 +1,32 @@
 # ─────────────────────────────────────────────────
 #  Problem : 0120. Triangle
 #  Difficulty : Medium
-#  Runtime  : 33 ms
-#  Memory   : 20.4 MB
+#  Runtime  : 7 ms
+#  Memory   : 13.5 MB
 #  Solved   : 2026-07-30
 # ─────────────────────────────────────────────────
 
-from functools import lru_cache
-class Solution:
-    def minimumTotal(self, triangle: List[List[int]]) -> int:
-
+class Solution(object):
+    def minimumTotal(self, triangle):
+        """
+        :type triangle: List[List[int]]
+        :rtype: int
+        """
         n=len(triangle)
-        
-        @lru_cache
-        def recur(i,j):
-            if i==n-1:
-                return triangle[i][j]
-            down=triangle[i][j]+recur(i+1,j)
-            right=triangle[i][j]+recur(i+1,j+1)
-            return min(down,right)
-        return recur(0,0)
+        dp=[[0]*i for i in range(1,n+1)]
+        # print(dp)
+        dp[0][0]=triangle[0][0]
+
+        for i in range(1,n):
+            for j in range(len(triangle[i])):
+                if 0<=j-1:
+                    left=dp[i-1][j-1]
+                else:
+                    left=1e9
+                if j<len(triangle[i-1]):
+                    up=dp[i-1][j]
+                else:
+                    up=1e9
+                dp[i][j]=min(left,up)+triangle[i][j]
+        # print(dp)
+        return min(dp[n-1])
