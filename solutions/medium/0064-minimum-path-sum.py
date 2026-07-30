edc3acd00503926c7300d3476e0f0036ec21b9cf
Kushul Aleti <kushulaleti@gmail.com>
2026-07-30 14:34:29 +0530

    refactor: update Minimum Path Sum (medium)

--- solutions/medium/0064-minimum-path-sum.py
+++ solutions/medium/0064-minimum-path-sum.py
@@ -1,21 +1,26 @@
 # ─────────────────────────────────────────────────
 #  Problem : 0064. Minimum Path Sum
 #  Difficulty : Medium
-#  Runtime  : 36 ms
-#  Memory   : 32.9 MB
+#  Runtime  : 11 ms
+#  Memory   : 14.1 MB
 #  Solved   : 2026-07-30
 # ─────────────────────────────────────────────────
 
-from functools import lru_cache
-class Solution:
-    def minPathSum(self, grid: List[List[int]]) -> int:
+class Solution(object):
+    def minPathSum(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
         m,n=len(grid),len(grid[0])
-
-        @lru_cache(None)
-        def recur(x,y):
-            if x<0 or y<0:
-                return float('inf')
-            if x==0 and y==0:
-                return grid[x][y]
-            return min(recur(x-1,y),recur(x,y-1))+grid[x][y]
-        return recur(m-1,n-1)
+        dp=[[0]*n for _ in range(m)]
+        dp[0][0]=grid[0][0]
+        for i in range(1,n):
+            dp[0][i]=dp[0][i-1]+grid[0][i]
+        for i in range(1,m):
+            dp[i][0]=dp[i-1][0]+grid[i][0]
+        
+        for i in range(1,m):
+            for j in range(1,n):
+                dp[i][j]=min(dp[i-1][j],dp[i][j-1]) + grid[i][j]
+        return dp[m-1][n-1]
