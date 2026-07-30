7ba13240307f5d651368a32959217f10680dfafe
Kushul Aleti <kushulaleti@gmail.com>
2026-07-30 14:39:32 +0530

    refactor: update Minimum Path Sum (medium)

--- solutions/medium/0064-minimum-path-sum.py
+++ solutions/medium/0064-minimum-path-sum.py
@@ -1,8 +1,8 @@
 # ─────────────────────────────────────────────────
 #  Problem : 0064. Minimum Path Sum
 #  Difficulty : Medium
-#  Runtime  : 11 ms
-#  Memory   : 14.1 MB
+#  Runtime  : 15 ms
+#  Memory   : 13.8 MB
 #  Solved   : 2026-07-30
 # ─────────────────────────────────────────────────
 
@@ -13,14 +13,13 @@ class Solution(object):
         :rtype: int
         """
         m,n=len(grid),len(grid[0])
-        dp=[[0]*n for _ in range(m)]
-        dp[0][0]=grid[0][0]
-        for i in range(1,n):
-            dp[0][i]=dp[0][i-1]+grid[0][i]
-        for i in range(1,m):
-            dp[i][0]=dp[i-1][0]+grid[i][0]
-        
-        for i in range(1,m):
-            for j in range(1,n):
-                dp[i][j]=min(dp[i-1][j],dp[i][j-1]) + grid[i][j]
-        return dp[m-1][n-1]
+        for i in range(m):
+            for j in range(n):
+                if i==0:
+                    if j!=0:  
+                        grid[i][j]+=grid[i][j-1]
+                elif j==0:
+                    grid[i][j]+=grid[i-1][j]
+                else:
+                    grid[i][j]=min(grid[i-1][j],grid[i][j-1]) + grid[i][j]
+        return grid[m-1][n-1]
