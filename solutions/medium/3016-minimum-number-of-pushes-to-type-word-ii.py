04f8e76e97b6dd33066a8da8af46a785a5f7e75f
Kushul Aleti <kushulaleti@gmail.com>
2026-07-31 10:42:00 +0530

    solve: 3016. Minimum Number of Pushes to Type Word II [medium] (py)

---
+++ solutions/medium/3016-minimum-number-of-pushes-to-type-word-ii.py
@@ -0,0 +1,27 @@
+# ─────────────────────────────────────────────────
+#  Problem : 3016. Minimum Number of Pushes to Type Word II
+#  Difficulty : Medium
+#  Runtime  : 110 ms
+#  Memory   : 20.2 MB
+#  Solved   : 2026-07-31
+# ─────────────────────────────────────────────────
+
+from collections import Counter
+class Solution:
+    def minimumPushes(self, word: str) -> int:
+        hashmap=Counter(word)
+        # sort the hashmap based on frequency in decreasing order
+        hashmap=dict(sorted(hashmap.items(), key=lambda item: item[1], reverse=True))
+        count=0
+        ans=0
+        for key,val in hashmap.items():
+            count+=1
+            if count<=8:
+                ans+=val
+            elif count<=16:
+                ans+=(val*2)
+            elif count<=24:
+                ans+=(val*3)
+            else:
+                ans+=(val*4)
+        return ans
