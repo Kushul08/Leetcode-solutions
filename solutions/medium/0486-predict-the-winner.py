10cc9f2d7c95c59d7a6fb018f7d880cf6ed243c5
Kushul Aleti <kushulaleti@gmail.com>
2026-08-01 15:15:53 +0530

    refactor: update Predict the Winner (medium)

--- solutions/medium/0486-predict-the-winner.py
+++ solutions/medium/0486-predict-the-winner.py
@@ -1,8 +1,8 @@
 # ─────────────────────────────────────────────────
 #  Problem : 0486. Predict the Winner
 #  Difficulty : Medium
-#  Runtime  : 865 ms
-#  Memory   : 464.8 MB
+#  Runtime  : 0 ms
+#  Memory   : 20.2 MB
 #  Solved   : 2026-08-01
 # ─────────────────────────────────────────────────
 
@@ -12,17 +12,16 @@ class Solution:
         n=len(nums)
 
         @lru_cache(None)
-        def recur(x,y,diff,flag):
+        def recur(x,y,turn):
             if y==x:
-                if flag: 
-                    diff+=nums[x]
+                if turn: 
+                    return nums[x]
                 else:
-                    diff-=nums[x]
-                return diff
-            if flag:
-                return max(recur(x+1,y,diff+nums[x],False),
-                recur(x,y-1,diff+nums[y],False))
+                    return -nums[x]
+            if turn:
+                return max(nums[x]+recur(x+1,y,False),
+                nums[y]+recur(x,y-1,False))
             else:
-                return  min(recur(x+1,y,diff-nums[x],True),
-                recur(x,y-1,diff-nums[y],True)) # he wants the difference as small as possible
-        return recur(0,n-1,0,True)>=0       
+                return  min(recur(x+1,y,True)-nums[x],
+                recur(x,y-1,True)-nums[y]) # he wants the difference as small as possible
+        return recur(0,n-1,True)>=0       
