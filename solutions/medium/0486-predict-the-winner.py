aa91124f08f9f11d116eb9d6b18314291a880687
Kushul Aleti <kushulaleti@gmail.com>
2026-08-01 15:00:31 +0530

    solve: 0486. Predict the Winner [medium] (py)

---
+++ solutions/medium/0486-predict-the-winner.py
@@ -0,0 +1,26 @@
+# ─────────────────────────────────────────────────
+#  Problem : 0486. Predict the Winner
+#  Difficulty : Medium
+#  Runtime  : 2080 ms
+#  Memory   : 19.2 MB
+#  Solved   : 2026-08-01
+# ─────────────────────────────────────────────────
+
+class Solution:
+    def predictTheWinner(self, nums: List[int]) -> bool:
+        ans=[False]
+        n=len(nums)
+        def recur(x,y,diff,flag):
+            if y==x:
+                if flag: 
+                    diff+=nums[x]
+                else:
+                    diff-=nums[x]
+                return diff
+            if flag:
+                return max(recur(x+1,y,diff+nums[x],False),
+                recur(x,y-1,diff+nums[y],False))
+            else:
+                return  min(recur(x+1,y,diff-nums[x],True),
+                recur(x,y-1,diff-nums[y],True)) # he wants the difference as small as possible
+        return recur(0,n-1,0,True)>=0       
