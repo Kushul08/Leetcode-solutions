4bae3cf68b210ee3a97dd16d94960ef39c4ac78a
Kushul Aleti <kushulaleti@gmail.com>
2026-08-02 11:30:43 +0530

    solve: 0877. Stone Game [medium] (py)

---
+++ solutions/medium/0877-stone-game.py
@@ -0,0 +1,28 @@
+# ─────────────────────────────────────────────────
+#  Problem : 0877. Stone Game
+#  Difficulty : Medium
+#  Runtime  : 0 ms
+#  Memory   : 12.6 MB
+#  Solved   : 2026-08-02
+# ─────────────────────────────────────────────────
+
+class Solution(object):
+    def stoneGame(self, piles):
+        """
+        :type piles: List[int]
+        :rtype: bool
+        """
+        def recur(i,j,diff,turn):
+            if i==j:
+                if turn:
+                    return diff+piles[i]
+                else:
+                    return diff-piles[j]
+            if turn:
+                return max( recur(i+1,j,diff+piles[i],False),
+                recur(i,j-1,diff+piles[j],False))
+            else:
+                return min(recur(i+1,j,diff-piles[i],True),
+                recur(i,j-1,diff-piles[j],True))
+        return recur(0,0,0,True)>=0
+            
