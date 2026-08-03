fe29fa49ad08a7757da8827afe46f5660b7894c4
Kushul Aleti <kushulaleti@gmail.com>
2026-08-03 12:29:15 +0530

    refactor: update Partition Array Into Two Arrays to Minimize Sum Difference (hard)

--- solutions/hard/2035-partition-array-into-two-arrays-to-minimize-sum-difference.py
+++ solutions/hard/2035-partition-array-into-two-arrays-to-minimize-sum-difference.py
@@ -2,15 +2,18 @@
 #  Problem : 2035. Partition Array Into Two Arrays to Minimize Sum Difference
 #  Difficulty : Hard
 #  Runtime  : 0 ms
-#  Memory   : 19.3 MB
+#  Memory   : 19.4 MB
 #  Solved   : 2026-08-03
 # ─────────────────────────────────────────────────
 
+from functools import lru_cache
 class Solution:
     def minimumDifference(self, nums: List[int]) -> int:
         n=len(nums)
         total_sum=sum(nums)
         min_abs=float('inf')
+
+        @lru_cache(None)
         def recur(i,sums,take):
             nonlocal min_abs
             if i==n:
