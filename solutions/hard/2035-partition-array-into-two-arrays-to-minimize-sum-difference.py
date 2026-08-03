7f99dd355126624e16aab50d7fe20c496dd8992a
Kushul Aleti <kushulaleti@gmail.com>
2026-08-03 12:24:53 +0530

    solve: 2035. Partition Array Into Two Arrays to Minimize Sum Difference [hard] (py)

---
+++ solutions/hard/2035-partition-array-into-two-arrays-to-minimize-sum-difference.py
@@ -0,0 +1,29 @@
+# ─────────────────────────────────────────────────
+#  Problem : 2035. Partition Array Into Two Arrays to Minimize Sum Difference
+#  Difficulty : Hard
+#  Runtime  : 0 ms
+#  Memory   : 19.3 MB
+#  Solved   : 2026-08-03
+# ─────────────────────────────────────────────────
+
+class Solution:
+    def minimumDifference(self, nums: List[int]) -> int:
+        n=len(nums)
+        total_sum=sum(nums)
+        min_abs=float('inf')
+        def recur(i,sums,take):
+            nonlocal min_abs
+            if i==n:
+                if take==n//2:
+                    rem_sum=total_sum-sums
+                    min_abs=min(min_abs,abs(rem_sum-sums))
+                return
+            if take==n//2:
+                rem_sum=total_sum-sums
+                min_abs=min(min_abs,abs(rem_sum-sums))
+                return
+            recur(i+1,sums+nums[i],take+1)
+            recur(i+1,sums,take)
+            return
+        recur(0,0,0)
+        return min_abs
