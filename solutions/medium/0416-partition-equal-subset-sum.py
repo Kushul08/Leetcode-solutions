3936f6c74965cd829b3de6f67d02095ff128727b
Kushul Aleti <kushulaleti@gmail.com>
2026-08-03 10:37:27 +0530

    refactor: update Partition Equal Subset Sum (medium)

--- solutions/medium/0416-partition-equal-subset-sum.py
+++ solutions/medium/0416-partition-equal-subset-sum.py
@@ -1,12 +1,11 @@
 # ─────────────────────────────────────────────────
 #  Problem : 0416. Partition Equal Subset Sum
 #  Difficulty : Medium
-#  Runtime  : 11 ms
-#  Memory   : 34.7 MB
+#  Runtime  : 2544 ms
+#  Memory   : 169.8 MB
 #  Solved   : 2026-08-03
 # ─────────────────────────────────────────────────
 
-from functools import lru_cache
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
         if (sum(nums)&1)==1:
@@ -14,16 +13,21 @@ class Solution:
         n=len(nums)
         k=sum(nums)//2
 
-        @lru_cache(None)
+        dp=[[-1]*(k+1) for _ in range(n)]
+        
         def recur(i,target):
             if i==n:
                 return target==0
             if target==0:
                 return True
+            if dp[i][target]!=-1:
+                return dp[i][target]
             if nums[i]<=target:
                 pick=recur(i+1,target-nums[i])
             else:
                 pick=recur(i+1,target)
             unpick=recur(i+1,target)
-            return pick or unpick
+            dp[i][target]=pick or unpick
+
+            return dp[i][target]
         return recur(0,k)
