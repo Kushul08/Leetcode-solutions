c450e75dcd2be008b13bd1fdde23dccb96ed30a5
Kushul Aleti <kushulaleti@gmail.com>
2026-08-03 22:10:47 +0530

    refactor: update Stone Game III (hard)

--- solutions/hard/1406-stone-game-iii.py
+++ solutions/hard/1406-stone-game-iii.py
@@ -1,8 +1,8 @@
 # ─────────────────────────────────────────────────
 #  Problem : 1406. Stone Game III
 #  Difficulty : Hard
-#  Runtime  : 261 ms
-#  Memory   : 19.2 MB
+#  Runtime  : 2327 ms
+#  Memory   : 497.8 MB
 #  Solved   : 2026-08-03
 # ─────────────────────────────────────────────────
 
@@ -13,20 +13,20 @@ class Solution:
         n=len(stoneValue)
 
         @lru_cache(None)
-        def recur(i,diff,turn):
+        def recur(i,turn):
             if i>=n:
-                return diff
+                return 0
             if turn:
-                one=recur(i+1,diff+stoneValue[i],False)
-                two=recur(i+2,diff+stoneValue[i]+stoneValue[i+1],False) if i+1<n else float('-inf')
-                three=recur(i+3,diff+stoneValue[i]+stoneValue[i+1]+stoneValue[i+2],False) if i+2<n else float('-inf')
+                one=recur(i+1,False)+stoneValue[i]
+                two=recur(i+2,False)+stoneValue[i]+stoneValue[i+1] if i+1<n else float('-inf')
+                three=recur(i+3,False)+stoneValue[i]+stoneValue[i+1]+stoneValue[i+2] if i+2<n else float('-inf')
                 return max(one,two,three)
             else:
-                one=recur(i+1,diff-stoneValue[i],True)
-                two=recur(i+2,diff-stoneValue[i]-stoneValue[i+1],True) if i+1<n else float('inf')
-                three=recur(i+3,diff-stoneValue[i]-stoneValue[i+1]-stoneValue[i+2],True) if i+2<n else float('inf')
+                one=recur(i+1,True)-stoneValue[i]
+                two=recur(i+2,True)-stoneValue[i]-stoneValue[i+1] if i+1<n else float('inf')
+                three=recur(i+3,True)-stoneValue[i]-stoneValue[i+1]-stoneValue[i+2] if i+2<n else float('inf')
                 return min(one,two,three)
-        value=recur(0,0,True)
+        value=recur(0,True)
         if value<0:
             return 'Bob'
         elif value==0:
