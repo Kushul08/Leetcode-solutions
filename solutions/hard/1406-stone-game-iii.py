8819bcfcfbf2e2e440c0f25942f7a38e4077558b
Kushul Aleti <kushulaleti@gmail.com>
2026-08-03 22:01:16 +0530

    refactor: update Stone Game III (hard)

--- solutions/hard/1406-stone-game-iii.py
+++ solutions/hard/1406-stone-game-iii.py
@@ -6,38 +6,27 @@
 #  Solved   : 2026-08-03
 # ─────────────────────────────────────────────────
 
-class Solution(object):
-    def stoneGameIII(self, stoneValue):
-        """
-        :type stoneValue: List[int]
-        :rtype: str
-        """
+class Solution:
+    def stoneGameIII(self, stoneValue: List[int]) -> str:
+        
         n=len(stoneValue)
-        alice=0
-        bob=0
-        i=0
-        flag=True
-        while i<n:
-            points=stoneValue[i]
-            step=1
-            if i+1<n:
-                if points<=stoneValue[i+1]:
-                    points+=stoneValue[i+1]
-                    step=2
-            if i+2<n:
-                if points<=stoneValue[i+2]:
-                    points+=stoneValue[i+2]
-                    step=3
-            if flag==True:
-                alice+=points
-                flag=False
+        def recur(i,diff,turn):
+            if i>=n:
+                return diff
+            if turn:
+                one=recur(i+1,diff+stoneValue[i],False)
+                two=recur(i+2,diff+stoneValue[i]+stoneValue[i+1],False) if i+1<n else float('-inf')
+                three=recur(i+3,diff+stoneValue[i]+stoneValue[i+1]+stoneValue[i+2],False) if i+2<n else float('-inf')
+                return max(one,two,three)
             else:
-                bob+=points
-                flag=True
-            i+=step
-        if alice==bob:
-            return 'Tie'
-        elif alice<bob:
+                one=recur(i+1,diff-stoneValue[i],True)
+                two=recur(i+2,diff-stoneValue[i]-stoneValue[i+1],True) if i+1<n else float('inf')
+                three=recur(i+3,diff-stoneValue[i]-stoneValue[i+1]-stoneValue[i+2],True) if i+2<n else float('inf')
+                return min(one,two,three)
+        value=recur(0,0,True)
+        if value<0:
             return 'Bob'
+        elif value==0:
+            return 'Tie'
         else:
             return 'Alice'
