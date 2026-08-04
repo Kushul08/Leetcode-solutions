41f59433da736e91c679f87edd450ec6473207fc
Kushul Aleti <kushulaleti@gmail.com>
2026-08-04 17:32:03 +0530

    refactor: update Find Missing Elements (easy)

--- solutions/easy/3731-find-missing-elements.py
+++ solutions/easy/3731-find-missing-elements.py
@@ -1,7 +1,7 @@
 # ─────────────────────────────────────────────────
 #  Problem : 3731. Find Missing Elements
 #  Difficulty : Easy
-#  Runtime  : 7 ms
+#  Runtime  : 1 ms
 #  Memory   : 19.3 MB
 #  Solved   : 2026-08-04
 # ─────────────────────────────────────────────────
@@ -10,8 +10,9 @@ class Solution:
     def findMissingElements(self, nums: List[int]) -> List[int]:
         mini=min(nums)
         maxi=max(nums)
+        seen=set(nums)
         ans=[]
         for i in range(mini,maxi+1):
-            if i not in nums:
+            if i not in seen:
                 ans.append(i)
         return ans
