0423e5c4f99e6cf73f59d98bb30f8d54f52c2dd9
Kushul Aleti <kushulaleti@gmail.com>
2026-08-07 16:20:52 +0530

    refactor: update Partition Array Into Two Arrays to Minimize Sum Difference (hard)

--- solutions/hard/2035-partition-array-into-two-arrays-to-minimize-sum-difference.py
+++ solutions/hard/2035-partition-array-into-two-arrays-to-minimize-sum-difference.py
@@ -1,32 +1,41 @@
 # ─────────────────────────────────────────────────
 #  Problem : 2035. Partition Array Into Two Arrays to Minimize Sum Difference
 #  Difficulty : Hard
-#  Runtime  : 0 ms
-#  Memory   : 19.4 MB
-#  Solved   : 2026-08-03
+#  Runtime  : 1999 ms
+#  Memory   : 22.2 MB
+#  Solved   : 2026-08-07
 # ─────────────────────────────────────────────────
 
-from functools import lru_cache
+from bisect import bisect_left, bisect_right
 class Solution:
     def minimumDifference(self, nums: List[int]) -> int:
-        n=len(nums)
-        total_sum=sum(nums)
-        min_abs=float('inf')
-
-        @lru_cache(None)
-        def recur(i,sums,take):
-            nonlocal min_abs
+        n=len(nums)//2
+        left=[[] for _ in range(n+1)]
+        right=[[] for _ in range(n+1)]
+        
+        total=sum(nums)
+        def recur(i,take,sums,subsets,arr):
             if i==n:
-                if take==n//2:
-                    rem_sum=total_sum-sums
-                    min_abs=min(min_abs,abs(rem_sum-sums))
+                subsets[take].append(sums)
                 return
-            if take==n//2:
-                rem_sum=total_sum-sums
-                min_abs=min(min_abs,abs(rem_sum-sums))
-                return
-            recur(i+1,sums+nums[i],take+1)
-            recur(i+1,sums,take)
+            recur(i+1,take+1,sums+arr[i],subsets,arr)
+            recur(i+1,take,sums,subsets,arr)
             return
-        recur(0,0,0)
-        return min_abs
+        recur(0,0,0,left,nums[:n])
+        recur(0,0,0,right,nums[n:])
+
+        min_diff=float('inf')
+        for k in range(n+1):
+            arr=right[n-k]
+            arr.sort()
+            for i in range(len(left[k])):
+                l_sum=left[k][i]
+                rem=total/2-l_sum
+                indx=bisect_left(arr,rem)
+                if indx<len(arr):
+                    val=arr[indx]
+                    min_diff=min(min_diff,abs(total-2*(l_sum+val)))
+                if indx>0:
+                    val=arr[indx-1]
+                    min_diff=min(min_diff,abs(total-2*(l_sum+val)))
+        return min_diff
