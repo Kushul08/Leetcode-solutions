476ce3bb4087e92a57c9e85651428bde4ae192ff
Kushul Aleti <kushulaleti@gmail.com>
2026-08-11 17:19:18 +0530

    refactor: update Coin Change (medium)

--- solutions/medium/0322-coin-change.py
+++ solutions/medium/0322-coin-change.py
@@ -1,23 +1,23 @@
 # ─────────────────────────────────────────────────
 #  Problem : 0322. Coin Change
 #  Difficulty : Medium
-#  Runtime  : 2458 ms
-#  Memory   : 671.2 MB
+#  Runtime  : 1893 ms
+#  Memory   : 133.7 MB
 #  Solved   : 2026-08-11
 # ─────────────────────────────────────────────────
 
-from functools import lru_cache
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
 
         n=len(coins)
-        
-        @lru_cache(None)
+        dp=[[-1]*(amount+1) for _ in range(n)]
         def recur(i,target):
             if i==-1:
                 if target==0:
                     return 0
                 return float('inf')
+            if dp[i][target]!=-1:
+                return dp[i][target]
             pick=float('inf')
             pick_stay=float('inf')
             if coins[i]<=target:
@@ -25,7 +25,9 @@ class Solution:
                 pick_stay=recur(i,target-coins[i])+1
             unpick=recur(i-1,target)
 
-            return min(pick,unpick,pick_stay)
+            dp[i][target]=min(pick,unpick,pick_stay)
+            return dp[i][target]
+
         coin_change=recur(n-1,amount)
         if coin_change!=float('inf'):
             return coin_change
