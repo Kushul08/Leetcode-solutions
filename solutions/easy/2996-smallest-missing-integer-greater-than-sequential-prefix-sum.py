3d2888575bd565b645fcfff3316938e055988ef2
Kushul Aleti <kushulaleti@gmail.com>
2026-08-11 11:23:52 +0530

    refactor: update Smallest Missing Integer Greater Than Sequential Prefix Sum (easy)

--- solutions/easy/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.py
+++ solutions/easy/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.py
@@ -2,10 +2,11 @@
 #  Problem : 2996. Smallest Missing Integer Greater Than Sequential Prefix Sum
 #  Difficulty : Easy
 #  Runtime  : 0 ms
-#  Memory   : 12.4 MB
+#  Memory   : 12.3 MB
 #  Solved   : 2026-08-11
 # ─────────────────────────────────────────────────
 
+from bisect import bisect_left
 class Solution(object):
     def missingInteger(self, nums):
         """
@@ -19,6 +20,12 @@ class Solution(object):
             else:
                 break
         nums_set=set(nums)
-        for x in range(sums,1276):
-            if x not in nums_set:
-                return x 
+        low=sums
+        high=1276
+        while low<=high:
+            mid=(low+high)/2
+            if mid in nums_set:
+                low=mid+1
+            else:
+                high=mid-1
+        return low
