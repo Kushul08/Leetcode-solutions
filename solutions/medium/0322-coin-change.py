6cd4842106bc691bb102056ddc9108fc8f0364a3
Kushul Aleti <kushulaleti@gmail.com>
2026-08-12 14:21:50 +0530

    refactor: update Coin Change (medium)

--- solutions/medium/0322-coin-change.py
+++ solutions/medium/0322-coin-change.py
@@ -1,15 +1,15 @@
 # ─────────────────────────────────────────────────
 #  Problem : 0322. Coin Change
 #  Difficulty : Medium
-#  Runtime  : 0 ms
-#  Memory   : 19.4 MB
+#  Runtime  : 988 ms
+#  Memory   : 23.2 MB
 #  Solved   : 2026-08-12
 # ─────────────────────────────────────────────────
 
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         n=len(coins)
-        dp=[[-1]*(amount+1) for _ in range(n)]
+        dp=[[float('inf')]*(amount+1) for _ in range(n)]
 
         for i in range(n):
             dp[i][0]=0
@@ -19,9 +19,9 @@ class Solution:
         
         for i in range(1,n):
             for j in range(1,amount+1):
-                pick=1e9
+                pick=float('inf')
                 if coins[i]<=j:
                     pick=dp[i][j-coins[i]]+1
                 unpick=dp[i-1][j]
                 dp[i][j]=min(pick,unpick)
-        return dp[n-1][amount]
+        return dp[n-1][amount] if dp[n-1][amount]!=float('inf') else -1
