285683e6b527b4f6adeecf9f467ac983570484a1
Kushul Aleti <kushulaleti@gmail.com>
2026-08-12 14:21:48 +0530

    refactor: update Coin Change (medium)

--- solutions/medium/0322-coin-change.py
+++ solutions/medium/0322-coin-change.py
@@ -1,32 +1,27 @@
 # ─────────────────────────────────────────────────
 #  Problem : 0322. Coin Change
 #  Difficulty : Medium
-#  Runtime  : 1171 ms
-#  Memory   : 93.6 MB
-#  Solved   : 2026-08-11
+#  Runtime  : 0 ms
+#  Memory   : 19.4 MB
+#  Solved   : 2026-08-12
 # ─────────────────────────────────────────────────
 
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-
         n=len(coins)
         dp=[[-1]*(amount+1) for _ in range(n)]
-        def recur(i,target):
-            if i==-1:
-                if target==0:
-                    return 0
-                return float('inf')
-            if dp[i][target]!=-1:
-                return dp[i][target]
-            pick_stay=float('inf')
-            if coins[i]<=target:
-                pick_stay=recur(i,target-coins[i])+1
-            unpick=recur(i-1,target)
 
-            dp[i][target]=min(unpick,pick_stay)
-            return dp[i][target]
-
-        coin_change=recur(n-1,amount)
-        if coin_change!=float('inf'):
-            return coin_change
-        return -1
+        for i in range(n):
+            dp[i][0]=0
+        if coins[0]<=amount:
+            for j in range(coins[0],amount+1,coins[0]):
+                dp[0][j]=j//coins[0]
+        
+        for i in range(1,n):
+            for j in range(1,amount+1):
+                pick=1e9
+                if coins[i]<=j:
+                    pick=dp[i][j-coins[i]]+1
+                unpick=dp[i-1][j]
+                dp[i][j]=min(pick,unpick)
+        return dp[n-1][amount]
