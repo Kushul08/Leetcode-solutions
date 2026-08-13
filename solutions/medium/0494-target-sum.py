b87461554f95af8300cc63096368cee1bc9f54e6
Kushul Aleti <kushulaleti@gmail.com>
2026-08-13 12:23:42 +0530

    refactor: update Target Sum (medium)

--- solutions/medium/0494-target-sum.py
+++ solutions/medium/0494-target-sum.py
@@ -1,21 +1,19 @@
 # ─────────────────────────────────────────────────
 #  Problem : 0494. Target Sum
 #  Difficulty : Medium
-#  Runtime  : 0 ms
-#  Memory   : 19.5 MB
+#  Runtime  : 95 ms
+#  Memory   : 53.5 MB
 #  Solved   : 2026-08-13
 # ─────────────────────────────────────────────────
 
-class Solution(object):
-    def findTargetSumWays(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
+from functools import lru_cache
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
         n=len(nums)
         if (target+sum(nums))%2!=0: return 0
         required=(target+sum(nums))/2
+
+        @lru_cache(None)
         def recur(i,exp):
             if i==-1:
                 if exp==0:
