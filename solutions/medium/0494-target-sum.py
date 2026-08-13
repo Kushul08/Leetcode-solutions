d6526e11b6f6de508eae445b7cbcbc290159cca7
Kushul Aleti <kushulaleti@gmail.com>
2026-08-13 14:05:54 +0530

    refactor: update Target Sum (medium)

--- solutions/medium/0494-target-sum.py
+++ solutions/medium/0494-target-sum.py
@@ -1,8 +1,8 @@
 # ─────────────────────────────────────────────────
 #  Problem : 0494. Target Sum
 #  Difficulty : Medium
-#  Runtime  : 43 ms
-#  Memory   : 19.4 MB
+#  Runtime  : 27 ms
+#  Memory   : 19.5 MB
 #  Solved   : 2026-08-13
 # ─────────────────────────────────────────────────
 
@@ -11,21 +11,21 @@ class Solution:
         n=len(nums)
         if (target+sum(nums))%2!=0 or (target+sum(nums))<0 : return 0
         required=(target+sum(nums))//2
-        dp=[[0]*(required+1) for _ in range(n)]
-        for i in range(n):
-            dp[i][0]=0
+        dp=[0]*(required+1)
         if nums[0]==0:
-            dp[0][0]=2
+            dp[0]=2
         else:
-            dp[0][0]=1
+            dp[0]=1
             if nums[0]<=required:
-                dp[0][nums[0]]=1
+                dp[nums[0]]=1
         for i in range(1,n):
+            temp=[0]*(required+1)
             for j in range(required+1):
                 pick=0
                 if nums[i]<=j:
-                    pick=dp[i-1][j-nums[i]]
-                unpick=dp[i-1][j]
-                dp[i][j]=pick+unpick
-        return dp[n-1][required]
+                    pick=dp[j-nums[i]]
+                unpick=dp[j]
+                temp[j]=pick+unpick
+            dp=temp
+        return dp[required]
         
