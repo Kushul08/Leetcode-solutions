5086a516c9256f3ea9af0011ee9ff8f93c7bc52c
Kushul Aleti <kushulaleti@gmail.com>
2026-08-13 14:03:37 +0530

    refactor: update Target Sum (medium)

--- solutions/medium/0494-target-sum.py
+++ solutions/medium/0494-target-sum.py
@@ -1,8 +1,8 @@
 # ─────────────────────────────────────────────────
 #  Problem : 0494. Target Sum
 #  Difficulty : Medium
-#  Runtime  : 1 ms
-#  Memory   : 19.2 MB
+#  Runtime  : 43 ms
+#  Memory   : 19.4 MB
 #  Solved   : 2026-08-13
 # ─────────────────────────────────────────────────
 
@@ -14,8 +14,12 @@ class Solution:
         dp=[[0]*(required+1) for _ in range(n)]
         for i in range(n):
             dp[i][0]=0
-        if nums[0]<=required: dp[0][nums[0]]=1
-        dp[0][0]=1
+        if nums[0]==0:
+            dp[0][0]=2
+        else:
+            dp[0][0]=1
+            if nums[0]<=required:
+                dp[0][nums[0]]=1
         for i in range(1,n):
             for j in range(required+1):
                 pick=0
