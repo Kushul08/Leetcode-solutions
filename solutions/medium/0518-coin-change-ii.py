340e9fc0b62fa6a7bc92008754de9a7a1a6c1357
Kushul Aleti <kushulaleti@gmail.com>
2026-08-13 14:28:14 +0530

    refactor: update Coin Change II (medium)

--- solutions/medium/0518-coin-change-ii.py
+++ solutions/medium/0518-coin-change-ii.py
@@ -1,26 +1,32 @@
 # ─────────────────────────────────────────────────
 #  Problem : 0518. Coin Change II
 #  Difficulty : Medium
-#  Runtime  : 735 ms
-#  Memory   : 204.3 MB
+#  Runtime  : 487 ms
+#  Memory   : 39 MB
 #  Solved   : 2026-08-13
 # ─────────────────────────────────────────────────
 
-from functools import lru_cache
-class Solution:
-    def change(self, amount: int, coins: List[int]) -> int:
+class Solution(object):
+    def change(self, amount, coins):
+        """
+        :type amount: int
+        :type coins: List[int]
+        :rtype: int
+        """
         n=len(coins)
         
-        @lru_cache(None)
+        dp=[[-1]*(amount+1) for _ in range(n)]
         def recur(i,money):
             if i==-1:
                 if money==0:
                     return 1
                 return 0
-
+            if dp[i][money]!=-1:
+                return dp[i][money]
             pick=0
             if coins[i]<=money:
                 pick=recur(i,money-coins[i])
             unpick=recur(i-1,money)
-            return pick+unpick
+            dp[i][money]=pick+unpick
+            return dp[i][money]
         return recur(n-1,amount)
