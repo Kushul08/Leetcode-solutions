461732a7161633a98992fe214872922d073825a6
Kushul Aleti <kushulaleti@gmail.com>
2026-08-15 18:13:08 +0530

    refactor: update Longest Subsequence With Non-Zero Bitwise XOR (medium)

--- solutions/medium/3702-longest-subsequence-with-non-zero-bitwise-xor.py
+++ solutions/medium/3702-longest-subsequence-with-non-zero-bitwise-xor.py
@@ -1,27 +1,25 @@
 # ─────────────────────────────────────────────────
 #  Problem : 3702. Longest Subsequence With Non-Zero Bitwise XOR
 #  Difficulty : Medium
-#  Runtime  : 0 ms
-#  Memory   : 12.4 MB
+#  Runtime  : 63 ms
+#  Memory   : 22.7 MB
 #  Solved   : 2026-08-15
 # ─────────────────────────────────────────────────
 
-from functools import lru_cache
-class Solution:
-    def longestSubsequence(self, nums: List[int]) -> int:
-
+class Solution(object):
+    def longestSubsequence(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        xor=0
         n=len(nums)
-        dp={}
-        def recur(i,xor):
-            if i==n:
-                if xor!=0:
-                    return 0
-                return float('-inf')
-            if (i,xor) in dp:
-                return dp[(i,xor)]
-            pick=recur(i+1,xor^nums[i])+1
-            unpick=recur(i+1,xor)
-
-            dp[(i,xor)]=max(pick,unpick)
-            return dp[(i,xor)]
-        return recur(0,0)
+        non_zero=False
+        for num in nums:
+            xor^=num
+            if num!=0: non_zero=True
+        if xor!=0:
+            return n
+        if non_zero:
+            return n-1
+        return 0
