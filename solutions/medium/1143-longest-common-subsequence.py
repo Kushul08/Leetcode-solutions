9da6a012eb53974948fc4c0e827e7e37f8be1f2d
Kushul Aleti <kushulaleti@gmail.com>
2026-08-17 12:19:31 +0530

    refactor: update Longest Common Subsequence (medium)

--- solutions/medium/1143-longest-common-subsequence.py
+++ solutions/medium/1143-longest-common-subsequence.py
@@ -1,8 +1,8 @@
 # ─────────────────────────────────────────────────
 #  Problem : 1143. Longest Common Subsequence
 #  Difficulty : Medium
-#  Runtime  : 2 ms
-#  Memory   : 12.3 MB
+#  Runtime  : 0 ms
+#  Memory   : 12.4 MB
 #  Solved   : 2026-08-17
 # ─────────────────────────────────────────────────
 
@@ -20,7 +20,7 @@ class Solution(object):
             if text2[0]==text1[i]:
                 dp[0][i]=dp[0][i-1]+1
             else: dp[0][i]=dp[0][i-1]
-        for j in range(m):
+        for j in range(1,m):
             if text1[0]==text2[j]:
                 dp[j][0]=dp[j-1][0]+1
             else: 
@@ -28,8 +28,10 @@ class Solution(object):
 
         for i in range(1,m):
             for j in range(1,n):
-                dp[i][j]=max(dp[i-1][j],dp[i][j-1])
+                
                 if text2[i]==text1[j]:
-                    dp[i][j]+=1
-        
+                    dp[i][j]=dp[i-1][j-1]+1
+                else:
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1])
+        print(dp)
         return dp[m-1][n-1]
