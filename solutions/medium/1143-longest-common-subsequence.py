e54d85797414e02e8f5d33a2e2be97928e6d2c44
Kushul Aleti <kushulaleti@gmail.com>
2026-08-17 12:10:56 +0530

    refactor: update Longest Common Subsequence (medium)

--- solutions/medium/1143-longest-common-subsequence.py
+++ solutions/medium/1143-longest-common-subsequence.py
@@ -1,24 +1,35 @@
 # ─────────────────────────────────────────────────
 #  Problem : 1143. Longest Common Subsequence
 #  Difficulty : Medium
-#  Runtime  : 601 ms
-#  Memory   : 44.3 MB
+#  Runtime  : 2 ms
+#  Memory   : 12.3 MB
 #  Solved   : 2026-08-17
 # ─────────────────────────────────────────────────
 
-class Solution:
-    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        
+class Solution(object):
+    def longestCommonSubsequence(self, text1, text2):
+        """
+        :type text1: str
+        :type text2: str
+        :rtype: int
+        """
         n,m=len(text1),len(text2)
+        dp=[[0]*n for _ in range(m)]
+        if text1[0]==text2[0]: dp[0][0]=1
+        for i in range(1,n):
+            if text2[0]==text1[i]:
+                dp[0][i]=dp[0][i-1]+1
+            else: dp[0][i]=dp[0][i-1]
+        for j in range(m):
+            if text1[0]==text2[j]:
+                dp[j][0]=dp[j-1][0]+1
+            else: 
+                dp[j][0]=dp[j-1][0]
 
-        dp=[[-1]*m for _ in range(n)]
-        def recur(i,j):
-            if i<0 or j<0: return 0
-            if dp[i][j]!=-1:
-                return dp[i][j]
-            if text1[i]==text2[j]:
-                return recur(i-1,j-1)+1
-            else:
-                dp[i][j]=max(recur(i-1,j),recur(i,j-1))
-                return dp[i][j]
-        return recur(n-1,m-1)
+        for i in range(1,m):
+            for j in range(1,n):
+                dp[i][j]=max(dp[i-1][j],dp[i][j-1])
+                if text2[i]==text1[j]:
+                    dp[i][j]+=1
+        
+        return dp[m-1][n-1]
