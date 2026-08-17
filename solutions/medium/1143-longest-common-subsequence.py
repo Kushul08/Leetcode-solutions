c1d6c626a4a31f61b2f34694beecf2c70d406044
Kushul Aleti <kushulaleti@gmail.com>
2026-08-17 13:41:05 +0530

    refactor: update Longest Common Subsequence (medium)

--- solutions/medium/1143-longest-common-subsequence.py
+++ solutions/medium/1143-longest-common-subsequence.py
@@ -1,8 +1,8 @@
 # ─────────────────────────────────────────────────
 #  Problem : 1143. Longest Common Subsequence
 #  Difficulty : Medium
-#  Runtime  : 506 ms
-#  Memory   : 33.6 MB
+#  Runtime  : 409 ms
+#  Memory   : 12.4 MB
 #  Solved   : 2026-08-17
 # ─────────────────────────────────────────────────
 
@@ -14,22 +14,22 @@ class Solution(object):
         :rtype: int
         """
         n,m=len(text1),len(text2)
-        dp=[[0]*n for _ in range(m)]
-        if text1[0]==text2[0]: dp[0][0]=1
+        dp=[0]*n 
+        if text1[0]==text2[0]: dp[0]=1
         for i in range(1,n):
             if text2[0]==text1[i]:
-                dp[0][i]=1
-            else: dp[0][i]=dp[0][i-1]
-        for j in range(1,m):
-            if text1[0]==text2[j]:
-                dp[j][0]=1
-            else: 
-                dp[j][0]=dp[j-1][0]
+                dp[i]=1
+            elif i>0: 
+                dp[i]=dp[i-1]
+       
 
         for i in range(1,m):
+            temp=[0]*n
+            temp[0]= 1 if text2[i]==text1[0] else dp[0]
             for j in range(1,n):
                 if text2[i]==text1[j]:
-                    dp[i][j]=dp[i-1][j-1]+1
+                    temp[j]=dp[j-1]+1
                 else:
-                    dp[i][j]=max(dp[i-1][j],dp[i][j-1])
-        return dp[m-1][n-1]
+                    temp[j]=max(temp[j-1],dp[j])
+            dp=temp
+        return dp[n-1]
