71642c40a16fbd5fcfb862c5d86b5db08e173686
Kushul Aleti <kushulaleti@gmail.com>
2026-08-17 11:25:35 +0530

    refactor: update Longest Common Subsequence (medium)

--- solutions/medium/1143-longest-common-subsequence.py
+++ solutions/medium/1143-longest-common-subsequence.py
@@ -1,22 +1,24 @@
 # ─────────────────────────────────────────────────
 #  Problem : 1143. Longest Common Subsequence
 #  Difficulty : Medium
-#  Runtime  : 1054 ms
-#  Memory   : 318.6 MB
+#  Runtime  : 601 ms
+#  Memory   : 44.3 MB
 #  Solved   : 2026-08-17
 # ─────────────────────────────────────────────────
 
-from functools import lru_cache
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         
         n,m=len(text1),len(text2)
 
-        @lru_cache(None)
+        dp=[[-1]*m for _ in range(n)]
         def recur(i,j):
             if i<0 or j<0: return 0
+            if dp[i][j]!=-1:
+                return dp[i][j]
             if text1[i]==text2[j]:
                 return recur(i-1,j-1)+1
             else:
-                return max(recur(i-1,j),recur(i,j-1))
+                dp[i][j]=max(recur(i-1,j),recur(i,j-1))
+                return dp[i][j]
         return recur(n-1,m-1)
