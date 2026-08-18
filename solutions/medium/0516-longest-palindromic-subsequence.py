9e34abe35f74560c5f662ba16d0d4cc504a8418c
Kushul Aleti <kushulaleti@gmail.com>
2026-08-18 15:46:56 +0530

    refactor: update Longest Palindromic Subsequence (medium)

--- solutions/medium/0516-longest-palindromic-subsequence.py
+++ solutions/medium/0516-longest-palindromic-subsequence.py
@@ -1,21 +1,20 @@
 # ─────────────────────────────────────────────────
 #  Problem : 0516. Longest Palindromic Subsequence
 #  Difficulty : Medium
-#  Runtime  : 0 ms
-#  Memory   : 19.2 MB
+#  Runtime  : 1510 ms
+#  Memory   : 537.2 MB
 #  Solved   : 2026-08-18
 # ─────────────────────────────────────────────────
 
-class Solution(object):
-    def longestPalindromeSubseq(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
+from functools import lru_cache
+class Solution:
+    def longestPalindromeSubseq(self, s: str) -> int:
+        
         n=len(s)
         s1=s
         s2=s[::-1]
 
+        @lru_cache(None)
         def recur(i,j):
             if i<0 or j<0:
                 return 0
