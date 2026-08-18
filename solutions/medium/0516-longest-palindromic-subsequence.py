8f3a41606c22ceadb00f4f0707a47248e047fb6a
Kushul Aleti <kushulaleti@gmail.com>
2026-08-18 16:42:33 +0530

    refactor: update Longest Palindromic Subsequence (medium)

--- solutions/medium/0516-longest-palindromic-subsequence.py
+++ solutions/medium/0516-longest-palindromic-subsequence.py
@@ -1,8 +1,8 @@
 # ─────────────────────────────────────────────────
 #  Problem : 0516. Longest Palindromic Subsequence
 #  Difficulty : Medium
-#  Runtime  : 1625 ms
-#  Memory   : 33.7 MB
+#  Runtime  : 1271 ms
+#  Memory   : 12.5 MB
 #  Solved   : 2026-08-18
 # ─────────────────────────────────────────────────
 
@@ -15,22 +15,20 @@ class Solution(object):
         n=len(s)
         s1=s
         s2=s[::-1]
-        dp=[[-1]*n for _ in range(n)]
-        dp[0][0]=1 if s1[0]==s2[0] else 0
+        dp=[0]*n 
+        dp[0]=1 if s1[0]==s2[0] else 0
         for i in range(1,n):
             if s1[0]==s2[i]:
-                dp[0][i]=1
+                dp[i]=1
             else:
-                dp[0][i]=dp[0][i-1]
+                dp[i]=dp[i-1]
         for i in range(1,n):
-            if s1[i]==s2[0]:
-                dp[i][0]=1
-            else:
-                dp[i][0]=dp[i-1][0]
-        for i in range(1,n):
+            temp=[0]*n
+            temp[0]= 1 if s1[i]==s2[0] else dp[0]
             for j in range(1,n):
                 if s1[i]==s2[j]:
-                    dp[i][j]=dp[i-1][j-1]+1
+                    temp[j]=dp[j-1]+1
                 else:
-                    dp[i][j]=max(dp[i][j-1],dp[i-1][j])
-        return dp[n-1][n-1]
+                    temp[j]=max(temp[j-1],dp[j])
+            dp=temp
+        return dp[n-1]
