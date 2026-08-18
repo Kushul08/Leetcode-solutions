69f62b9677d36d0c8dc42301d2f6b2c2f415ace6
Kushul Aleti <kushulaleti@gmail.com>
2026-08-18 15:49:36 +0530

    refactor: update Longest Palindromic Subsequence (medium)

--- solutions/medium/0516-longest-palindromic-subsequence.py
+++ solutions/medium/0516-longest-palindromic-subsequence.py
@@ -1,25 +1,30 @@
 # ─────────────────────────────────────────────────
 #  Problem : 0516. Longest Palindromic Subsequence
 #  Difficulty : Medium
-#  Runtime  : 1510 ms
-#  Memory   : 537.2 MB
+#  Runtime  : 1392 ms
+#  Memory   : 67.4 MB
 #  Solved   : 2026-08-18
 # ─────────────────────────────────────────────────
 
-from functools import lru_cache
-class Solution:
-    def longestPalindromeSubseq(self, s: str) -> int:
-        
+class Solution(object):
+    def longestPalindromeSubseq(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
         n=len(s)
         s1=s
         s2=s[::-1]
-
-        @lru_cache(None)
+        dp=[[-1]*n for _ in range(n)]
         def recur(i,j):
             if i<0 or j<0:
                 return 0
+            if dp[i][j]!=-1:
+                return dp[i][j]
             if s1[i]==s2[j]:
-                return recur(i-1,j-1)+1
+                dp[i][j]=recur(i-1,j-1)+1
+                return dp[i][j]
             else:
-                return max(recur(i,j-1),recur(i-1,j))
+                dp[i][j]=max(recur(i,j-1),recur(i-1,j))
+                return dp[i][j]
         return recur(n-1,n-1)
