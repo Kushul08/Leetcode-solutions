3ac4833196e50398339cef738d8ed6ea6a47d5c6
Kushul Aleti <kushulaleti@gmail.com>
2026-08-19 13:02:10 +0530

    refactor: update Cinema Seat Allocation (medium)

--- solutions/medium/1386-cinema-seat-allocation.py
+++ solutions/medium/1386-cinema-seat-allocation.py
@@ -1,8 +1,8 @@
 # ─────────────────────────────────────────────────
 #  Problem : 1386. Cinema Seat Allocation
 #  Difficulty : Medium
-#  Runtime  : 110 ms
-#  Memory   : 15.1 MB
+#  Runtime  : 83 ms
+#  Memory   : 16.5 MB
 #  Solved   : 2026-08-19
 # ─────────────────────────────────────────────────
 
@@ -13,26 +13,16 @@ class Solution(object):
         :type reservedSeats: List[List[int]]
         :rtype: int
         """
-        reservedSeats.sort()
-        row=reservedSeats[0][0]
-        ans=0
-        row_count=0
-        seats=[True,True,True]
+
+        hashmap={}
         for r,c in reservedSeats:
-            if row!=r:
-                row=r
-                row_count+=1
-                if sum(seats)==3:
-                    ans+=2
-                elif sum(seats)==2:
-                    if seats[0]==True and seats[2]==True:
-                        ans+=2
-                    else:
-                        ans+=1
-                elif sum(seats)==1:
-                    ans+=1 
-                seats=[True,True,True]
-            if row==r:
+            if r not in hashmap:
+                hashmap[r]=[]
+            hashmap[r].append(c)
+        ans=0
+        for key,arr in hashmap.items():
+            seats=[True,True,True]
+            for c in arr:
                 if c==2 or c==3:
                     seats[0]=False
                 elif c==4 or c==5:
@@ -43,16 +33,14 @@ class Solution(object):
                     seats[2]=False
                 elif c==8 or c==9:
                     seats[2]=False
-        if sum(seats)==3:
+            if sum(seats)==3:
                 ans+=2
-        elif sum(seats)==2:
-            if seats[0]==True and seats[2]==True:
-                ans+=2
-            else:
-                ans+=1
-        elif sum(seats)==1:
-            ans+=1 
-        row_count+=1
-        ans+=(n-row_count)*2
+            elif sum(seats)==2:
+                if seats[0]==True and seats[2]==True:
+                    ans+=2
+                else:
+                    ans+=1
+            elif sum(seats)==1:
+                ans+=1 
+        ans+=(n-len(hashmap))*2
         return ans
-
