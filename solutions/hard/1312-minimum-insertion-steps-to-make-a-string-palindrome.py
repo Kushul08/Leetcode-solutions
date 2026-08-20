7883c00f1e10c9add8183f9cde6470b82dde673f
Kushul Aleti <kushulaleti@gmail.com>
2026-08-20 13:43:27 +0530

    refactor: update Minimum Insertion Steps to Make a String Palindrome (hard)

--- solutions/hard/1312-minimum-insertion-steps-to-make-a-string-palindrome.py
+++ solutions/hard/1312-minimum-insertion-steps-to-make-a-string-palindrome.py
@@ -1,32 +1,27 @@
 # ─────────────────────────────────────────────────
 #  Problem : 1312. Minimum Insertion Steps to Make a String Palindrome
 #  Difficulty : Hard
-#  Runtime  : 1274 ms
-#  Memory   : 27 MB
-#  Solved   : 2026-08-19
+#  Runtime  : 795 ms
+#  Memory   : 14.6 MB
+#  Solved   : 2026-08-20
 # ─────────────────────────────────────────────────
 
 class Solution(object):
     def minInsertions(self, s):
         """
         :type s: str
-        :rtype: int
+        :rtype: int`
         """
         s1=s
         s2=s[::-1]
 
         n=len(s)
-        dp=[[-1]*n for _ in range(n)]
-        def recur(i,j):
-            if i<0 or j<0:
-                return 0
-            if dp[i][j]!=-1:
-                return dp[i][j]
-            if s1[i]==s2[j]:
-                dp[i][j]=recur(i-1,j-1)+1
-                return dp[i][j]
-            else:
-                dp[i][j]=max(recur(i,j-1),recur(i-1,j))
-                return dp[i][j]
-
-        return n-recur(n-1,n-1)
+        dp=[[0]*(n+1) for _ in range(n+1)]
+        
+        for i in range(1,n+1):
+            for j in range(1,n+1):
+                if s1[i-1]==s2[j-1]:
+                    dp[i][j]=dp[i-1][j-1]+1
+                else:
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1])
+        return n-dp[n][n]
