467dfa82da78f6b8ec2309498ba1035bcf34e573
Kushul Aleti <kushulaleti@gmail.com>
2026-08-20 11:42:09 +0530

    solve: 3069. Distribute Elements Into Two Arrays I [easy] (py)

---
+++ solutions/easy/3069-distribute-elements-into-two-arrays-i.py
@@ -0,0 +1,23 @@
+# ─────────────────────────────────────────────────
+#  Problem : 3069. Distribute Elements Into Two Arrays I
+#  Difficulty : Easy
+#  Runtime  : 10 ms
+#  Memory   : 12.2 MB
+#  Solved   : 2026-08-20
+# ─────────────────────────────────────────────────
+
+class Solution(object):
+    def resultArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        nums1=[nums[0]]
+        nums2=[nums[1]]
+
+        for i in range(2,len(nums)):
+            if nums1[-1]>nums2[-1]:
+                nums1.append(nums[i])
+            else:
+                nums2.append(nums[i])
+        return nums1+nums2
