a66a1ae71a0a13b96bfa4b32bfdf4adb886c0698
Kushul Aleti <kushulaleti@gmail.com>
2026-08-24 07:18:29 +0530

    refactor: update Delete Operation for Two Strings (medium)

--- solutions/medium/0583-delete-operation-for-two-strings.py
+++ solutions/medium/0583-delete-operation-for-two-strings.py
@@ -1,25 +1,33 @@
 # ─────────────────────────────────────────────────
 #  Problem : 0583. Delete Operation for Two Strings
 #  Difficulty : Medium
-#  Runtime  : 143 ms
-#  Memory   : 61 MB
+#  Runtime  : 164 ms
+#  Memory   : 15.4 MB
 #  Solved   : 2026-08-24
 # ─────────────────────────────────────────────────
 
-from functools import lru_cache
-class Solution:
-    def minDistance(self, word1: str, word2: str) -> int:
-
+class Solution(object):
+    def minDistance(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: int
+        """
         n,m=len(word1),len(word2)
 
-        @lru_cache(None)
+        dp=[[-1]*m for _ in range(n)]
+
         def recur(i,j):
             if i<0 or j<0:
                 return 0
+            if dp[i][j]!=-1:
+                return dp[i][j]
             if word1[i]==word2[j]:
-                return recur(i-1,j-1)+1
+                dp[i][j]=recur(i-1,j-1)+1
+                return dp[i][j]
             else:
-                return max(recur(i-1,j),
+                dp[i][j]=max(recur(i-1,j),
                             recur(i,j-1))
+                return dp[i][j]
         lcs=recur(n-1,m-1)
         return n-lcs+m-lcs
