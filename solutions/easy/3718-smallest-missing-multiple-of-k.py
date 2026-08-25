fb68f7e1766659369637487b2fbb9aa647ffbfd1
Kushul Aleti <kushulaleti@gmail.com>
2026-08-25 07:27:37 +0530

    refactor: update Smallest Missing Multiple of K (easy)

--- solutions/easy/3718-smallest-missing-multiple-of-k.py
+++ solutions/easy/3718-smallest-missing-multiple-of-k.py
@@ -1,7 +1,7 @@
 # ─────────────────────────────────────────────────
 #  Problem : 3718. Smallest Missing Multiple of K
 #  Difficulty : Easy
-#  Runtime  : 0 ms
+#  Runtime  : 4 ms
 #  Memory   : 12.3 MB
 #  Solved   : 2026-08-25
 # ─────────────────────────────────────────────────
@@ -14,7 +14,7 @@ class Solution(object):
         :rtype: int
         """
         nums_set=set(nums)
-        for i in range(k,201,k):
-            if i not in nums_set:
-                return i
-        
+        ans=k
+        while ans in nums_set:
+            ans+=k
+        return ans
