18c8bbe7271d87cce31860cfdffbb90c7b073a72
Kushul Aleti <kushulaleti@gmail.com>
2026-08-25 11:06:47 +0530

    solve: 1092. Shortest Common Supersequence [hard] (py)

---
+++ solutions/hard/1092-shortest-common-supersequence.py
@@ -0,0 +1,41 @@
+# ─────────────────────────────────────────────────
+#  Problem : 1092. Shortest Common Supersequence
+#  Difficulty : Hard
+#  Runtime  : 337 ms
+#  Memory   : 44.4 MB
+#  Solved   : 2026-08-25
+# ─────────────────────────────────────────────────
+
+class Solution:
+    def shortestCommonSupersequence(self, str1: str, str2: str) -> str:
+        n,m=len(str1),len(str2)
+
+        dp=[[0]*(m+1) for _ in range(n+1)]
+
+        for i in range(1,n+1):
+            for j in range(1,m+1):
+                if str1[i-1]==str2[j-1]:
+                    dp[i][j]=dp[i-1][j-1]+1
+                else:
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1])
+        
+        x,y=n,m
+        ans=[]
+        while x>0 and y>0:
+            if str1[x-1]==str2[y-1]:
+                ans.append(str1[x-1])
+                x-=1
+                y-=1
+            elif dp[x-1][y]>dp[x][y-1]:
+                ans.append(str1[x-1])
+                x-=1
+            else:
+                ans.append(str2[y-1])
+                y-=1
+        while x>0:
+            ans.extend(str1[x-1])
+            x-=1
+        while y>0:
+            ans.extend(str2[y-1])
+            y-=1
+        return ''.join(ans[::-1])
