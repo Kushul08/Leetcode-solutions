9caf5b4e7a92ee0e7d05f578b39599614ac18d97
Kushul Aleti <kushulaleti@gmail.com>
2026-08-25 21:41:17 +0530

    refactor: update Distinct Subsequences (hard)

--- solutions/hard/0115-distinct-subsequences.py
+++ solutions/hard/0115-distinct-subsequences.py
@@ -1,26 +1,23 @@
 # ─────────────────────────────────────────────────
 #  Problem : 0115. Distinct Subsequences
 #  Difficulty : Hard
-#  Runtime  : 410 ms
-#  Memory   : 43.7 MB
+#  Runtime  : 416 ms
+#  Memory   : 75.7 MB
 #  Solved   : 2026-08-25
 # ─────────────────────────────────────────────────
 
-from functools import lru_cache
 class Solution:
     def numDistinct(self, s: str, t: str) -> int:
         n,m=len(s),len(t)
 
-        dp=[[-1]*(m) for _ in range(n)]
-        def recur(i,j):
-            if i<0 or j<0:
-                return 1 if j==-1 else 0
-            if dp[i][j]!=-1:
-                return dp[i][j]
-            if s[i]==t[j]:
-                dp[i][j]=recur(i-1,j-1)+recur(i-1,j)
-                return dp[i][j]
-            else:
-                dp[i][j]=recur(i-1,j)
-                return dp[i][j]
-        return recur(n-1,m-1)
+        dp=[[0]*(m+1) for _ in range(n+1)]
+        dp[0][0]=1 if s[0]==t[0] else 0
+        for j in range(n):
+            dp[j][0]=1
+        for i in range(1,n+1):
+            for j in range(1,m+1):
+                if s[i-1]==t[j-1]:
+                    dp[i][j]=dp[i-1][j-1]+dp[i-1][j]
+                else:
+                    dp[i][j]=dp[i-1][j]
+        return dp[n][m]
