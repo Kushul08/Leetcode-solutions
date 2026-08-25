f35208e169fc580563c3165a103e98aea86c07b9
Kushul Aleti <kushulaleti@gmail.com>
2026-08-25 21:11:08 +0530

    refactor: update Distinct Subsequences (hard)

--- solutions/hard/0115-distinct-subsequences.py
+++ solutions/hard/0115-distinct-subsequences.py
@@ -1,24 +1,24 @@
 # ─────────────────────────────────────────────────
 #  Problem : 0115. Distinct Subsequences
 #  Difficulty : Hard
-#  Runtime  : 0 ms
-#  Memory   : 19.2 MB
+#  Runtime  : 686 ms
+#  Memory   : 240.8 MB
 #  Solved   : 2026-08-25
 # ─────────────────────────────────────────────────
 
-class Solution(object):
-    def numDistinct(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: int
-        """
-        n=len(s)
-        def recur(i,string):
-            if i==n:
-                return 1 if string==t else 0
-            if string and string[0]!=t[0]:
-                return 0
-            return recur(i+1,string+s[i])+recur(i+1,string)
+from functools import lru_cache
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        n,m=len(s),len(t)
 
-        return (recur(0,''))
+        @lru_cache(None)
+        def recur(i,j,match):
+            if i<0 or j<0:
+                if match==m:
+                    return 1
+                return 0
+            if s[i]==t[j]:
+                return recur(i-1,j-1,match+1)+recur(i-1,j,match)
+            else:
+                return recur(i-1,j,match)
+        return recur(n-1,m-1,0)
