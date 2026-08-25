218bd329b9a0a5878fc08350e5b8b81f2536ad54
Kushul Aleti <kushulaleti@gmail.com>
2026-08-25 07:20:04 +0530

    solve: 3718. Smallest Missing Multiple of K [easy] (py)

---
+++ solutions/easy/3718-smallest-missing-multiple-of-k.py
@@ -0,0 +1,20 @@
+# ─────────────────────────────────────────────────
+#  Problem : 3718. Smallest Missing Multiple of K
+#  Difficulty : Easy
+#  Runtime  : 0 ms
+#  Memory   : 12.3 MB
+#  Solved   : 2026-08-25
+# ─────────────────────────────────────────────────
+
+class Solution(object):
+    def missingMultiple(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        nums_set=set(nums)
+        for i in range(k,101,k):
+            if i not in nums_set:
+                return i
+        
