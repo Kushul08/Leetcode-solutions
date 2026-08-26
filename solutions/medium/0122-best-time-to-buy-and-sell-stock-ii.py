ea1c942dc1ba26c80bcd87d9e28ca44046ab3c6d
Kushul Aleti <kushulaleti@gmail.com>
2026-08-26 18:19:22 +0530

    refactor: update Best Time to Buy and Sell Stock II (medium)

--- solutions/medium/0122-best-time-to-buy-and-sell-stock-ii.py
+++ solutions/medium/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,8 +1,8 @@
 # ─────────────────────────────────────────────────
 #  Problem : 0122. Best Time to Buy and Sell Stock II
 #  Difficulty : Medium
-#  Runtime  : 23 ms
-#  Memory   : 22.3 MB
+#  Runtime  : 25 ms
+#  Memory   : 23.3 MB
 #  Solved   : 2026-08-26
 # ─────────────────────────────────────────────────
 
@@ -16,11 +16,13 @@ class Solution:
         dp[-1][1]=prices[-1]
         flag=0
         for i in range(n-2,-1,-1):
-            skip=dp[i+1][1]
-            sell=dp[i+1][0]+prices[i]
-            dp[i][1]=max(skip,sell)
-            
-            skip=dp[i+1][0]
-            buy=dp[i+1][1]-prices[i]
-            dp[i][0]=max(skip,buy)
+            for buy in range(2):
+                if buy==1:
+                    skip=dp[i+1][1]
+                    sell=dp[i+1][0]+prices[i]
+                    dp[i][1]=max(skip,sell)
+                else:
+                    skip=dp[i+1][0]
+                    buy=dp[i+1][1]-prices[i]
+                    dp[i][0]=max(skip,buy)
         return dp[0][0]
