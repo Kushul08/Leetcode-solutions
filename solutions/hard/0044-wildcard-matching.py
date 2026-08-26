5852e03432d040ed2d9efbb9a9b918295b98b764
Kushul Aleti <kushulaleti@gmail.com>
2026-08-26 12:20:53 +0530

    refactor: update Wildcard Matching (hard)

--- solutions/hard/0044-wildcard-matching.py
+++ solutions/hard/0044-wildcard-matching.py
@@ -1,7 +1,7 @@
 # ─────────────────────────────────────────────────
 #  Problem : 0044. Wildcard Matching
 #  Difficulty : Hard
-#  Runtime  : 1215 ms
+#  Runtime  : 1248 ms
 #  Memory   : 43.5 MB
 #  Solved   : 2026-08-26
 # ─────────────────────────────────────────────────
@@ -23,16 +23,6 @@ class Solution(object):
         if n!=0 and m==0:
             return False
         dp=[[False]*(m+1) for _ in range(n+1)]
-        # def recur(i,j):
-        #     if i<0 or j<0:
-        #         if i<0 and j<0:
-        #             return True
-        #         elif i<0 and j>=0:
-        #             while j>=0 and p[j]=='*':
-        #                 j-=1
-        #             if j<0:
-        #                 return True
-        #         return False
         for i in range(n):
             dp[i][0]=False
         for j in range(1,m+1):
@@ -51,6 +41,4 @@ class Solution(object):
                     dp[i][j]=dp[i-1][j-1]
                 elif s[i-1]!=p[j-1]:
                     dp[i][j]=False
-                # else:
-                #     dp[i][j]=True
         return dp[n][m]
