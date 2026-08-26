ad3ab5527c4f69426695bf91c71b369efc5569d7
Kushul Aleti <kushulaleti@gmail.com>
2026-08-26 11:49:31 +0530

    refactor: update Wildcard Matching (hard)

--- solutions/hard/0044-wildcard-matching.py
+++ solutions/hard/0044-wildcard-matching.py
@@ -1,15 +1,18 @@
 # ─────────────────────────────────────────────────
 #  Problem : 0044. Wildcard Matching
 #  Difficulty : Hard
-#  Runtime  : 398 ms
-#  Memory   : 149 MB
+#  Runtime  : 394 ms
+#  Memory   : 53.3 MB
 #  Solved   : 2026-08-26
 # ─────────────────────────────────────────────────
 
-from functools import lru_cache
-class Solution:
-    def isMatch(self, s: str, p: str) -> bool:
-        
+class Solution(object):
+    def isMatch(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: bool
+        """
         n,m=len(s),len(p)
         if n==0 and m!=0:
             if p.count('*')==m:
@@ -19,10 +22,9 @@ class Solution:
             return True
         if n!=0 and m==0:
             return False
-        
-        @lru_cache(None)
+        dp=[[-1]*m for _ in range(n)]
         def recur(i,j):
-            if i<0 or  j<0:
+            if i<0 or j<0:
                 if i<0 and j<0:
                     return True
                 elif i<0 and j>=0:
@@ -31,13 +33,20 @@ class Solution:
                     if j<0:
                         return True
                 return False 
+            if dp[i][j]!=-1:
+                return dp[i][j]
             if s[i]==p[j]:
-                return recur(i-1,j-1)
+                dp[i][j]=recur(i-1,j-1)
+                return dp[i][j]
             elif p[j]=='*':
-                return recur(i-1,j-1) or recur(i-1,j) or recur(i,j-1)
+                dp[i][j]=recur(i-1,j-1) or recur(i-1,j) or recur(i,j-1)
+                return dp[i][j]
             elif p[j]=='?':
-                return recur(i-1,j-1)
+                dp[i][j]=recur(i-1,j-1)
+                return dp[i][j] 
             elif s[i]!=p[j]:
-                return False
-            return True
+                dp[i][j]=False
+                return dp[i][j]
+            dp[i][j]=True
+            return dp[i][j]
         return recur(n-1,m-1)
