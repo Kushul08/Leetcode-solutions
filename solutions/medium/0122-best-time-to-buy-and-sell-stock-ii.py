d0bc27b4160469fa27f3123ac290aa4a61f8dba1
Kushul Aleti <kushulaleti@gmail.com>
2026-08-26 18:25:18 +0530

    refactor: update Best Time to Buy and Sell Stock II (medium)

--- solutions/medium/0122-best-time-to-buy-and-sell-stock-ii.py
+++ solutions/medium/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,27 +1,26 @@
 # ─────────────────────────────────────────────────
 #  Problem : 0122. Best Time to Buy and Sell Stock II
 #  Difficulty : Medium
-#  Runtime  : 11 ms
-#  Memory   : 20.3 MB
+#  Runtime  : 7 ms
+#  Memory   : 20.4 MB
 #  Solved   : 2026-08-26
 # ─────────────────────────────────────────────────
 
-from functools import lru_cache
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         
         n=len(prices)
-        dp=[0,prices[-1]]
+        x,y=0,prices[-1]
         for i in range(n-2,-1,-1):
             temp=[0,0]
             for buy in range(2):
                 if buy==1:
-                    skip=dp[1]
-                    sell=dp[0]+prices[i]
-                    temp[1]=max(skip,sell)
+                    skip=y
+                    sell=x+prices[i]
+                    ny=max(skip,sell)
                 else:
-                    skip=dp[0]
-                    buy=dp[1]-prices[i]
-                    temp[0]=max(skip,buy)
-            dp=temp
-        return dp[0]
+                    skip=x
+                    buy=y-prices[i]
+                    nx=max(skip,buy)
+            x,y=nx,ny
+        return x
