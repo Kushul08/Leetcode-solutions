7505399fdf3ec8a3435a054d2e1417cd5353c8dd
Kushul Aleti <kushulaleti@gmail.com>
2026-08-26 17:34:44 +0530

    refactor: update Best Time to Buy and Sell Stock II (medium)

--- solutions/medium/0122-best-time-to-buy-and-sell-stock-ii.py
+++ solutions/medium/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,8 +1,8 @@
 # ─────────────────────────────────────────────────
 #  Problem : 0122. Best Time to Buy and Sell Stock II
 #  Difficulty : Medium
-#  Runtime  : 63 ms
-#  Memory   : 73.4 MB
+#  Runtime  : 43 ms
+#  Memory   : 31.8 MB
 #  Solved   : 2026-08-26
 # ─────────────────────────────────────────────────
 
@@ -11,16 +11,21 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         
         n=len(prices)
-        @lru_cache(None)
+        dp=[[-1]*2 for _ in range(n)]
         def recur(i,status):
             if i==n:
                 return 0
+            if dp[i][status]!=-1:
+                return dp[i][status]
             if status==1:
                 skip=recur(i+1,status)
                 sell=recur(i+1,0)+prices[i]
-                return max(skip,sell)
+                dp[i][status]=max(skip,sell)
+                return dp[i][status] 
             else:
                 skip=recur(i+1,status)
                 buy=recur(i+1,1)-prices[i]
-                return max(skip,buy)
+                dp[i][status]=max(skip,buy)
+                return dp[i][status] 
+
         return recur(0,0)
