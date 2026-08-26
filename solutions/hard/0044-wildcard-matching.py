44d7d8c5ee3902c4377970eb1918e1a9d9a78c58
Kushul Aleti <kushulaleti@gmail.com>
2026-08-26 12:20:11 +0530

    refactor: update Wildcard Matching (hard)

--- solutions/hard/0044-wildcard-matching.py
+++ solutions/hard/0044-wildcard-matching.py
@@ -1,8 +1,8 @@
 # ─────────────────────────────────────────────────
 #  Problem : 0044. Wildcard Matching
 #  Difficulty : Hard
-#  Runtime  : 394 ms
-#  Memory   : 53.3 MB
+#  Runtime  : 1215 ms
+#  Memory   : 43.5 MB
 #  Solved   : 2026-08-26
 # ─────────────────────────────────────────────────
 
@@ -22,31 +22,35 @@ class Solution(object):
             return True
         if n!=0 and m==0:
             return False
-        dp=[[-1]*m for _ in range(n)]
-        def recur(i,j):
-            if i<0 or j<0:
-                if i<0 and j<0:
-                    return True
-                elif i<0 and j>=0:
-                    while j>=0 and p[j]=='*':
-                        j-=1
-                    if j<0:
-                        return True
-                return False 
-            if dp[i][j]!=-1:
-                return dp[i][j]
-            if s[i]==p[j]:
-                dp[i][j]=recur(i-1,j-1)
-                return dp[i][j]
-            elif p[j]=='*':
-                dp[i][j]=recur(i-1,j-1) or recur(i-1,j) or recur(i,j-1)
-                return dp[i][j]
-            elif p[j]=='?':
-                dp[i][j]=recur(i-1,j-1)
-                return dp[i][j] 
-            elif s[i]!=p[j]:
-                dp[i][j]=False
-                return dp[i][j]
-            dp[i][j]=True
-            return dp[i][j]
-        return recur(n-1,m-1)
+        dp=[[False]*(m+1) for _ in range(n+1)]
+        # def recur(i,j):
+        #     if i<0 or j<0:
+        #         if i<0 and j<0:
+        #             return True
+        #         elif i<0 and j>=0:
+        #             while j>=0 and p[j]=='*':
+        #                 j-=1
+        #             if j<0:
+        #                 return True
+        #         return False
+        for i in range(n):
+            dp[i][0]=False
+        for j in range(1,m+1):
+            if p[j-1]=='*':
+                dp[0][j]=True
+            else:
+                break
+        dp[0][0]=True
+        for i in range(1,n+1):
+            for j in range(1,m+1):
+                if s[i-1]==p[j-1]:
+                    dp[i][j]=dp[i-1][j-1]
+                elif p[j-1]=='*':
+                    dp[i][j]=dp[i-1][j-1] or dp[i-1][j] or dp[i][j-1]
+                elif p[j-1]=='?':
+                    dp[i][j]=dp[i-1][j-1]
+                elif s[i-1]!=p[j-1]:
+                    dp[i][j]=False
+                # else:
+                #     dp[i][j]=True
+        return dp[n][m]
