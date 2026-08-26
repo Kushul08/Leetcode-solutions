913fdce3d12ac81f6167040f9107b5ab7b52cb59
Kushul Aleti <kushulaleti@gmail.com>
2026-08-26 18:21:51 +0530

    refactor: update Best Time to Buy and Sell Stock II (medium)

--- solutions/medium/0122-best-time-to-buy-and-sell-stock-ii.py
+++ solutions/medium/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,8 +1,8 @@
 # ─────────────────────────────────────────────────
 #  Problem : 0122. Best Time to Buy and Sell Stock II
 #  Difficulty : Medium
-#  Runtime  : 25 ms
-#  Memory   : 23.3 MB
+#  Runtime  : 11 ms
+#  Memory   : 20.3 MB
 #  Solved   : 2026-08-26
 # ─────────────────────────────────────────────────
 
@@ -11,18 +11,17 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         
         n=len(prices)
-        dp=[[0]*2 for _ in range(n)]
-        dp[-1][0]=0
-        dp[-1][1]=prices[-1]
-        flag=0
+        dp=[0,prices[-1]]
         for i in range(n-2,-1,-1):
+            temp=[0,0]
             for buy in range(2):
                 if buy==1:
-                    skip=dp[i+1][1]
-                    sell=dp[i+1][0]+prices[i]
-                    dp[i][1]=max(skip,sell)
+                    skip=dp[1]
+                    sell=dp[0]+prices[i]
+                    temp[1]=max(skip,sell)
                 else:
-                    skip=dp[i+1][0]
-                    buy=dp[i+1][1]-prices[i]
-                    dp[i][0]=max(skip,buy)
-        return dp[0][0]
+                    skip=dp[0]
+                    buy=dp[1]-prices[i]
+                    temp[0]=max(skip,buy)
+            dp=temp
+        return dp[0]
