42d20b45b75027d34a39c2ad6813273a8d5d3e92
Kushul Aleti <kushulaleti@gmail.com>
2026-08-26 07:39:19 +0530

    refactor: update Edit Distance (medium)

--- solutions/medium/0072-edit-distance.py
+++ solutions/medium/0072-edit-distance.py
@@ -1,8 +1,8 @@
 # ─────────────────────────────────────────────────
 #  Problem : 0072. Edit Distance
 #  Difficulty : Medium
-#  Runtime  : 20 ms
-#  Memory   : 22.1 MB
+#  Runtime  : 0 ms
+#  Memory   : 19.3 MB
 #  Solved   : 2026-08-26
 # ─────────────────────────────────────────────────
 
@@ -17,23 +17,21 @@ class Solution:
         elif n!=0 and m==0:
             return n
         
-        dp=[[-1]*m for _ in range(n)]
-        def recur(i,j):
-            if i<0 or j<0:
-                if i>=0:
-                    return i+1
-                if j>=0:
-                    return j+1
-                return 0
-            if dp[i][j]!=-1:
-                return dp[i][j]
-            if word1[i]==word2[j]:
-                dp[i][j]=recur(i-1,j-1)
-                return dp[i][j]
-            else:
-                dp[i][j]=min(1+recur(i-1,j-1),
-                            1+recur(i,j-1),
-                            1+recur(i-1,j))
-                return dp[i][j]
-        return recur(n-1,m-1)
+        dp=[[0]*(m+1) for _ in range(n+1)]
+        for i in range(n):
+            dp[i][0]=i+1
+
+        for j in range(m):
+            dp[0][j]=j+1
+        dp[0][0]=0
+        for i in range(1,n+1):
+            for j in range(1,m+1):
+                if word1[i-1]==word2[j-1]:
+                    dp[i][j]=dp[i-1][j-1]
+                else:
+                    dp[i][j]=min(1+dp[i-1][j-1],
+                                1+dp[i][j-1],
+                                1+dp[i-1][j])
+        print(dp)     
+        return dp[n][m]
             
