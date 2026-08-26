a794a12f74b9c3359a958261eec102ce300c4d0c
Kushul Aleti <kushulaleti@gmail.com>
2026-08-26 18:01:54 +0530

    refactor: update Best Time to Buy and Sell Stock II (medium)

--- solutions/medium/0122-best-time-to-buy-and-sell-stock-ii.py
+++ solutions/medium/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,8 +1,8 @@
 # ─────────────────────────────────────────────────
 #  Problem : 0122. Best Time to Buy and Sell Stock II
 #  Difficulty : Medium
-#  Runtime  : 43 ms
-#  Memory   : 31.8 MB
+#  Runtime  : 23 ms
+#  Memory   : 22.3 MB
 #  Solved   : 2026-08-26
 # ─────────────────────────────────────────────────
 
@@ -11,21 +11,16 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         
         n=len(prices)
-        dp=[[-1]*2 for _ in range(n)]
-        def recur(i,status):
-            if i==n:
-                return 0
-            if dp[i][status]!=-1:
-                return dp[i][status]
-            if status==1:
-                skip=recur(i+1,status)
-                sell=recur(i+1,0)+prices[i]
-                dp[i][status]=max(skip,sell)
-                return dp[i][status] 
-            else:
-                skip=recur(i+1,status)
-                buy=recur(i+1,1)-prices[i]
-                dp[i][status]=max(skip,buy)
-                return dp[i][status] 
-
-        return recur(0,0)
+        dp=[[0]*2 for _ in range(n)]
+        dp[-1][0]=0
+        dp[-1][1]=prices[-1]
+        flag=0
+        for i in range(n-2,-1,-1):
+            skip=dp[i+1][1]
+            sell=dp[i+1][0]+prices[i]
+            dp[i][1]=max(skip,sell)
+            
+            skip=dp[i+1][0]
+            buy=dp[i+1][1]-prices[i]
+            dp[i][0]=max(skip,buy)
+        return dp[0][0]
