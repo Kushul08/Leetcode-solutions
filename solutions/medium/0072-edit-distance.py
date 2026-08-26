2e8202afafc5cc03a60523c953f8c4c73f174156
Kushul Aleti <kushulaleti@gmail.com>
2026-08-26 07:11:32 +0530

    refactor: update Edit Distance (medium)

--- solutions/medium/0072-edit-distance.py
+++ solutions/medium/0072-edit-distance.py
@@ -1,18 +1,14 @@
 # ─────────────────────────────────────────────────
 #  Problem : 0072. Edit Distance
 #  Difficulty : Medium
-#  Runtime  : 0 ms
-#  Memory   : 12.5 MB
+#  Runtime  : 17 ms
+#  Memory   : 25.1 MB
 #  Solved   : 2026-08-26
 # ─────────────────────────────────────────────────
 
-class Solution(object):
-    def minDistance(self, word1, word2):
-        """
-        :type word1: str
-        :type word2: str
-        :rtype: int
-        """
+from functools import lru_cache
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
         n,m=len(word1),len(word2)
         if n==0 and m==0:
             return 0
@@ -20,9 +16,15 @@ class Solution(object):
             return m
         elif n!=0 and m==0:
             return n
+        
+        @lru_cache(None)
         def recur(i,j):
             if i<0 or j<0:
-                return 0 if i<0 else i+1
+                if i>=0:
+                    return i+1
+                if j>=0:
+                    return j+1
+                return 0
             if word1[i]==word2[j]:
                 return recur(i-1,j-1)
             else:
