e2a7cd36c7b66da6f3e9e930f7e7c691578968ba
Kushul Aleti <kushulaleti@gmail.com>
2026-08-26 14:33:31 +0530

    refactor: update Best Time to Buy and Sell Stock (easy)

--- solutions/easy/0121-best-time-to-buy-and-sell-stock.py
+++ solutions/easy/0121-best-time-to-buy-and-sell-stock.py
@@ -1,8 +1,8 @@
 # ─────────────────────────────────────────────────
 #  Problem : 0121. Best Time to Buy and Sell Stock
 #  Difficulty : Easy
-#  Runtime  : 105 ms
-#  Memory   : 29 MB
+#  Runtime  : 79 ms
+#  Memory   : 28.6 MB
 #  Solved   : 2026-08-26
 # ─────────────────────────────────────────────────
 
@@ -10,16 +10,12 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         n=len(prices)
         stack=[]
-        next_greater=[0]*(n)
+        ans=0
         for i in range(n-1,-1,-1):
             while stack and stack[-1]<=prices[i]:
                 stack.pop()
             if stack:
-                next_greater[i]=stack[-1] 
+                ans=max(ans,stack[-1]-prices[i])
             else:
                 stack.append(prices[i])
-                next_greater[i]=stack[-1] 
-        ans=0
-        for i,price in enumerate(prices):
-            ans=max(ans,next_greater[i]-prices[i])
         return ans
