d43496d83d35fde9b64a85069b46358f91b7979c
Kushul Aleti <kushulaleti@gmail.com>
2026-08-27 13:02:45 +0530

    refactor: update Best Time to Buy and Sell Stock IV (hard)

--- solutions/hard/0188-best-time-to-buy-and-sell-stock-iv.py
+++ solutions/hard/0188-best-time-to-buy-and-sell-stock-iv.py
@@ -1,25 +1,30 @@
 # ─────────────────────────────────────────────────
 #  Problem : 0188. Best Time to Buy and Sell Stock IV
 #  Difficulty : Hard
-#  Runtime  : 211 ms
-#  Memory   : 99.6 MB
+#  Runtime  : 169 ms
+#  Memory   : 25.7 MB
 #  Solved   : 2026-08-27
 # ─────────────────────────────────────────────────
 
-from functools import lru_cache
 class Solution:
     def maxProfit(self, k: int, prices: List[int]) -> int:
         n=len(prices)
-        @lru_cache(None)
+        dp=[[[-1 for _ in range(k+1)]
+                for _ in range(2)]
+                for _ in range(n)]
         def recur(i,status,k):
             if i==n or k==0:
                 return 0
+            if dp[i][status][k]!=-1:
+                return dp[i][status][k]
             if status==1:
                 skip=recur(i+1,status,k)
                 sell=recur(i+1,0,k-1)+prices[i]
-                return max(skip,sell)
+                dp[i][status][k]=max(skip,sell)
+                return dp[i][status][k]
             else:
                 skip=recur(i+1,status,k)
                 buy=recur(i+1,1,k)-prices[i]
-                return max(skip,buy)
+                dp[i][status][k]=max(skip,buy)
+                return dp[i][status][k]
         return recur(0,0,k)
