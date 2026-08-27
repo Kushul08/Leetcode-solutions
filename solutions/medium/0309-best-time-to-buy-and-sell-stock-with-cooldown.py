197621112cc5c1b0ec09a1a209780ef1f47004c9
Kushul Aleti <kushulaleti@gmail.com>
2026-08-27 14:59:39 +0530

    refactor: update Best Time to Buy and Sell Stock with Cooldown (medium)

--- solutions/medium/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ solutions/medium/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,8 +1,8 @@
 # ─────────────────────────────────────────────────
 #  Problem : 0309. Best Time to Buy and Sell Stock with Cooldown
 #  Difficulty : Medium
-#  Runtime  : 3 ms
-#  Memory   : 20.8 MB
+#  Runtime  : 2 ms
+#  Memory   : 20 MB
 #  Solved   : 2026-08-27
 # ─────────────────────────────────────────────────
 
@@ -11,20 +11,15 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         n=len(prices)
 
-        dp=[[-1]*2 for _ in range(n)]
-        def recur(i,status):
-            if i>=n:
-                return 0
-            if dp[i][status]!=-1:
-                return dp[i][status]
-            if status==1:
-                skip=recur(i+1,status)
-                sell=recur(i+2,0)+prices[i]
-                dp[i][status]=max(skip,sell)
-                return dp[i][status]
-            else:
-                skip=recur(i+1,status)
-                buy=recur(i+1,1)-prices[i]
-                dp[i][status]=max(skip,buy)
-                return dp[i][status]
-        return recur(0,0)
+        dp=[[0]*2 for _ in range(n+2)]
+        for i in range(n-1,-1,-1):
+            for status in range(2):
+                if status==1:
+                    skip=dp[i+1][status]
+                    sell=dp[i+2][0]+prices[i]
+                    dp[i][status]=max(skip,sell)
+                else:
+                    skip=dp[i+1][status]
+                    buy=dp[i+1][1]-prices[i]
+                    dp[i][status]=max(skip,buy)
+        return dp[0][0]
