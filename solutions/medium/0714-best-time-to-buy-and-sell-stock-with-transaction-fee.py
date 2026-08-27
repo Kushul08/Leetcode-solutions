c0a2f777255f1a3fb24b3d525300bb5b4de160a2
Kushul Aleti <kushulaleti@gmail.com>
2026-08-27 16:55:01 +0530

    refactor: update Best Time to Buy and Sell Stock with Transaction Fee (medium)

--- solutions/medium/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ solutions/medium/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -1,7 +1,7 @@
 # ─────────────────────────────────────────────────
 #  Problem : 0714. Best Time to Buy and Sell Stock with Transaction Fee
 #  Difficulty : Medium
-#  Runtime  : 129 ms
+#  Runtime  : 90 ms
 #  Memory   : 26.2 MB
 #  Solved   : 2026-08-27
 # ─────────────────────────────────────────────────
@@ -15,15 +15,13 @@ class Solution:
 
         for i in range(n-1,-1,-1):
             temp=[0,0]
-            for status in range(2):
-                if status==1:
-                    skip=dp[status]
-                    sell=dp[0]+prices[i]-fee
-                    temp[status]=max(skip,sell)
-                else:
-                    skip=dp[status]
-                    buy=dp[1]-prices[i]
-                    temp[status]=max(skip,buy)
+            skip=dp[1]
+            sell=dp[0]+prices[i]-fee
+            temp[1]=max(skip,sell)
+
+            skip=dp[0]
+            buy=dp[1]-prices[i]
+            temp[0]=max(skip,buy)
             dp=temp
         return dp[0]
         
