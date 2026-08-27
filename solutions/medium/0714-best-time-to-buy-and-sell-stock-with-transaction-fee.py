657e566417ce395e6d3582b163f144586fe1574d
Kushul Aleti <kushulaleti@gmail.com>
2026-08-27 16:51:48 +0530

    refactor: update Best Time to Buy and Sell Stock with Transaction Fee (medium)

--- solutions/medium/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ solutions/medium/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -1,8 +1,8 @@
 # ─────────────────────────────────────────────────
 #  Problem : 0714. Best Time to Buy and Sell Stock with Transaction Fee
 #  Difficulty : Medium
-#  Runtime  : 456 ms
-#  Memory   : 78.1 MB
+#  Runtime  : 232 ms
+#  Memory   : 30.1 MB
 #  Solved   : 2026-08-27
 # ─────────────────────────────────────────────────
 
@@ -11,22 +11,17 @@ class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
         n=len(prices)
 
-        dp=[[-1]*2 for _ in range(n)]
-        def recur(i,status):
-            if i==n:
-                return 0
-            if dp[i][status]!=-1:
-                return dp[i][status]
-            if status==1:
-                skip=recur(i+1,status)
-                sell=recur(i+1,0)+prices[i]-fee
-                dp[i][status]=max(skip,sell)
-                return dp[i][status]
+        dp=[[0]*2 for _ in range(n+1)]
 
-            else:
-                skip=recur(i+1,status)
-                buy=recur(i+1,1)-prices[i]
-                dp[i][status]=max(skip,buy)
-                return dp[i][status]
-
-        return recur(0,0)
+        for i in range(n-1,-1,-1):
+            for status in range(2):
+                if status==1:
+                    skip=dp[i+1][status]
+                    sell=dp[i+1][0]+prices[i]-fee
+                    dp[i][status]=max(skip,sell)
+                else:
+                    skip=dp[i+1][status]
+                    buy=dp[i+1][1]-prices[i]
+                    dp[i][status]=max(skip,buy)
+        return dp[0][0]
+        
