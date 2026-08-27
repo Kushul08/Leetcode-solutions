ff55e75f023ae0e2f0a27dbbff4c9d180b950729
Kushul Aleti <kushulaleti@gmail.com>
2026-08-27 22:18:50 +0530

    solve: 3720. Lexicographically Smallest Permutation Greater Than Target [medium] (py)

---
+++ solutions/medium/3720-lexicographically-smallest-permutation-greater-than-target.py
@@ -0,0 +1,41 @@
+# ─────────────────────────────────────────────────
+#  Problem : 3720. Lexicographically Smallest Permutation Greater Than Target
+#  Difficulty : Medium
+#  Runtime  : 0 ms
+#  Memory   : 12.5 MB
+#  Solved   : 2026-08-27
+# ─────────────────────────────────────────────────
+
+class Solution(object):
+    def lexGreaterPermutation(self, s, target):
+        """
+        :type s: str
+        :type target: str
+        :rtype: str
+        """
+        n=len(s)
+        nums=[0]*(27)
+        for ch in s:
+            nums[ord(ch)-97]+=1
+        j=0
+        ans=''
+        while j<n:
+            for i in range(27):
+                if nums[i]!=0 and 97+i==ord(target[j]):
+                    ans+=chr(i+97)
+                    j+=1
+                    nums[i]-=1
+                    break
+                elif nums[i]!=0 and 97+i>ord(target[j]):
+                    ans+=chr(i+97)
+                    j+=1
+                    nums[i]-=1
+                    chars=''
+                    for k in range(27):
+                        if nums[k]!=0:
+                            chars+=chr(k+97)*nums[k]            
+                    return ans+chars
+            if i==26: return ''
+        if ans>target:
+            return ans
+        return ''
