4a0f192834082a2180ea7e78123d6fe2ae8f8d2a
Kushul Aleti <kushulaleti@gmail.com>
2026-08-27 11:34:34 +0530

    refactor: update Best Time to Buy and Sell Stock III (hard)

--- solutions/hard/0123-best-time-to-buy-and-sell-stock-iii.py
+++ solutions/hard/0123-best-time-to-buy-and-sell-stock-iii.py
@@ -1,26 +1,28 @@
 # ─────────────────────────────────────────────────
 #  Problem : 0123. Best Time to Buy and Sell Stock III
 #  Difficulty : Hard
-#  Runtime  : 1820 ms
-#  Memory   : 797.6 MB
+#  Runtime  : 1676 ms
+#  Memory   : 114.1 MB
 #  Solved   : 2026-08-27
 # ─────────────────────────────────────────────────
 
-from functools import lru_cache
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         n=len(prices)
-
-        @lru_cache(None)
+        dp=[[[-1 for _ in range(4)] for _ in range(2) ] for _ in range(n)]
         def recur(i,status,buys):
             if i==n or buys==3:
                 return 0
+            if dp[i][status][buys]!=-1:
+                return dp[i][status][buys]
             if status==1:
                 skip=recur(i+1,status,buys)
                 sell=recur(i+1,0,buys)+prices[i]
-                return max(skip,sell)
+                dp[i][status][buys]=max(skip,sell)
+                return dp[i][status][buys]
             else:
                 skip=recur(i+1,status,buys)
                 buy=recur(i+1,1,buys+1)-prices[i]
-                return max(skip,buy)
+                dp[i][status][buys]=max(skip,buy)
+                return dp[i][status][buys]
         return recur(0,0,0)
