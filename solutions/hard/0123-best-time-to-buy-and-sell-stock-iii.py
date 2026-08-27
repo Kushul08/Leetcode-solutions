50827f0039b28caae56488e9980cb04732f1f07a
Kushul Aleti <kushulaleti@gmail.com>
2026-08-27 12:04:58 +0530

    refactor: update Best Time to Buy and Sell Stock III (hard)

--- solutions/hard/0123-best-time-to-buy-and-sell-stock-iii.py
+++ solutions/hard/0123-best-time-to-buy-and-sell-stock-iii.py
@@ -1,28 +1,27 @@
 # ─────────────────────────────────────────────────
 #  Problem : 0123. Best Time to Buy and Sell Stock III
 #  Difficulty : Hard
-#  Runtime  : 1676 ms
-#  Memory   : 114.1 MB
+#  Runtime  : 1046 ms
+#  Memory   : 62.6 MB
 #  Solved   : 2026-08-27
 # ─────────────────────────────────────────────────
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         n=len(prices)
-        dp=[[[-1 for _ in range(4)] for _ in range(2) ] for _ in range(n)]
-        def recur(i,status,buys):
-            if i==n or buys==3:
-                return 0
-            if dp[i][status][buys]!=-1:
-                return dp[i][status][buys]
-            if status==1:
-                skip=recur(i+1,status,buys)
-                sell=recur(i+1,0,buys)+prices[i]
-                dp[i][status][buys]=max(skip,sell)
-                return dp[i][status][buys]
-            else:
-                skip=recur(i+1,status,buys)
-                buy=recur(i+1,1,buys+1)-prices[i]
-                dp[i][status][buys]=max(skip,buy)
-                return dp[i][status][buys]
-        return recur(0,0,0)
+        dp=[[[0 for _ in range(4)] 
+            for _ in range(2)] 
+            for _ in range(n+1)]
+        
+        for i in range(n-1,-1,-1):
+            for status in range(2):
+                for buys in range(2,-1,-1):
+                    if status==1:
+                        skip=dp[i+1][status][buys]
+                        sell=dp[i+1][0][buys]+prices[i]
+                        dp[i][status][buys]=max(skip,sell)
+                    else:
+                        skip=dp[i+1][status][buys]
+                        buy=dp[i+1][1][buys+1]-prices[i]
+                        dp[i][status][buys]=max(skip,buy)
+        return dp[0][0][0]
