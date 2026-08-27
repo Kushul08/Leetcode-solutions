914f46975361cbdbce4345dbf8bdadd73bfd3be6
Kushul Aleti <kushulaleti@gmail.com>
2026-08-27 12:30:26 +0530

    refactor: update Best Time to Buy and Sell Stock III (hard)

--- solutions/hard/0123-best-time-to-buy-and-sell-stock-iii.py
+++ solutions/hard/0123-best-time-to-buy-and-sell-stock-iii.py
@@ -1,7 +1,7 @@
 # ─────────────────────────────────────────────────
 #  Problem : 0123. Best Time to Buy and Sell Stock III
 #  Difficulty : Hard
-#  Runtime  : 697 ms
+#  Runtime  : 482 ms
 #  Memory   : 30.9 MB
 #  Solved   : 2026-08-27
 # ─────────────────────────────────────────────────
@@ -10,9 +10,9 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         n=len(prices)
         dp=[[0 for _ in range(4)] for _ in range(2)]
+        temp=[[0 for _ in range(4)] for _ in range(2)]
         
         for i in range(n-1,-1,-1):
-            temp=[[0 for _ in range(4)] for _ in range(2)]
             for status in range(2):
                 for buys in range(2,-1,-1):
                     if status==1:
