69bf69b24667c47d35320c8bce6c433d0a94fd3d
Kushul Aleti <kushulaleti@gmail.com>
2026-08-27 15:27:16 +0530

    refactor: update Best Time to Buy and Sell Stock with Cooldown (medium)

--- solutions/medium/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ solutions/medium/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,8 +1,8 @@
 # ─────────────────────────────────────────────────
 #  Problem : 0309. Best Time to Buy and Sell Stock with Cooldown
 #  Difficulty : Medium
-#  Runtime  : 1 ms
-#  Memory   : 19.7 MB
+#  Runtime  : 3 ms
+#  Memory   : 19.3 MB
 #  Solved   : 2026-08-27
 # ─────────────────────────────────────────────────
 
@@ -10,15 +10,20 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         n=len(prices)
 
-        dp=[[0]*2 for _ in range(n+2)]
+        dp=[0,0]
+        next2=[0,0]
+        next1=[0,0]
         for i in range(n-1,-1,-1):
+            curr=[0,0]
             for status in range(2):
                 if status==1:
-                    skip=dp[i+1][status]
-                    sell=dp[i+2][0]+prices[i]
-                    dp[i][status]=max(skip,sell)
+                    skip=next1[status]
+                    sell=next2[0]+prices[i]
+                    curr[status]=max(skip,sell)
                 else:
-                    skip=dp[i+1][status]
-                    buy=dp[i+1][1]-prices[i]
-                    dp[i][status]=max(skip,buy)
-        return dp[0][0]
+                    skip=next1[status]
+                    buy=next1[1]-prices[i]
+                    curr[status]=max(skip,buy)
+            next2=next1
+            next1=curr
+        return curr[0]
