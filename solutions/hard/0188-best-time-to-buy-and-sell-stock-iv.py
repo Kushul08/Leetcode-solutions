158f0cdb4778065d63b4013cc81e7a9cb5b0d0b2
Kushul Aleti <kushulaleti@gmail.com>
2026-08-27 13:44:00 +0530

    refactor: update Best Time to Buy and Sell Stock IV (hard)

--- solutions/hard/0188-best-time-to-buy-and-sell-stock-iv.py
+++ solutions/hard/0188-best-time-to-buy-and-sell-stock-iv.py
@@ -1,26 +1,28 @@
 # ─────────────────────────────────────────────────
 #  Problem : 0188. Best Time to Buy and Sell Stock IV
 #  Difficulty : Hard
-#  Runtime  : 63 ms
-#  Memory   : 22.2 MB
+#  Runtime  : 53 ms
+#  Memory   : 19.4 MB
 #  Solved   : 2026-08-27
 # ─────────────────────────────────────────────────
 
 class Solution:
     def maxProfit(self, k: int, prices: List[int]) -> int:
         n=len(prices)
-        dp=[[[0 for _ in range(k+1)]
+        dp=[[0 for _ in range(k+1)]
                 for _ in range(2)]
-                for _ in range(n+1)]
         for i in range(n-1,-1,-1):
+            temp=[[0 for _ in range(k+1)]
+                    for _ in range(2)]
             for status in range(2):
                 for cap in range(k,0,-1):
                     if status==1:
-                        skip=dp[i+1][status][cap]
-                        sell=dp[i+1][0][cap-1]+prices[i]
-                        dp[i][status][cap]=max(skip,sell)
+                        skip=dp[status][cap]
+                        sell=dp[0][cap-1]+prices[i]
+                        temp[status][cap]=max(skip,sell)
                     else:
-                        skip=dp[i+1][status][cap]
-                        buy=dp[i+1][1][cap]-prices[i]
-                        dp[i][status][cap]=max(skip,buy)
-        return dp[0][0][k]
+                        skip=dp[status][cap]
+                        buy=dp[1][cap]-prices[i]
+                        temp[status][cap]=max(skip,buy)
+            dp=temp
+        return dp[0][k]
