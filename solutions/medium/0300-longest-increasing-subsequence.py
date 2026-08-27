c16fac2457e83197096a919b7a25035f97968c01
Kushul Aleti <kushulaleti@gmail.com>
2026-08-27 18:09:29 +0530

    refactor: update Longest Increasing Subsequence (medium)

--- solutions/medium/0300-longest-increasing-subsequence.py
+++ solutions/medium/0300-longest-increasing-subsequence.py
@@ -1,15 +1,17 @@
 # ─────────────────────────────────────────────────
 #  Problem : 0300. Longest Increasing Subsequence
 #  Difficulty : Medium
-#  Runtime  : 0 ms
-#  Memory   : 19.3 MB
+#  Runtime  : 2822 ms
+#  Memory   : 597.9 MB
 #  Solved   : 2026-08-27
 # ─────────────────────────────────────────────────
 
+from functools import lru_cache
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         n=len(nums)
 
+        @lru_cache(None)
         def recur(i,prev):
             if i==n:
                 return 0
