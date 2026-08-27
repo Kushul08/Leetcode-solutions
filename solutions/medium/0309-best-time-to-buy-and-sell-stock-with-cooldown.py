ab29a6ce1c00751740d19ab3a1770210c77bca67
Kushul Aleti <kushulaleti@gmail.com>
2026-08-27 14:52:15 +0530

    solve: 0309. Best Time to Buy and Sell Stock with Cooldown [medium] (py)

---
+++ solutions/medium/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -0,0 +1,28 @@
+# ─────────────────────────────────────────────────
+#  Problem : 0309. Best Time to Buy and Sell Stock with Cooldown
+#  Difficulty : Medium
+#  Runtime  : 0 ms
+#  Memory   : 19.2 MB
+#  Solved   : 2026-08-27
+# ─────────────────────────────────────────────────
+
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        n=len(prices)
+
+        def recur(i,status):
+            if i>=n:
+                return 0
+            if status==1:
+                skip=recur(i+1,status)
+                sell=recur(i+2,0)+prices[i]
+                return max(skip,sell)
+            else:
+                skip=recur(i+1,status)
+                buy=recur(i+1,1)-prices[i]
+                return max(skip,buy)
+        return recur(0,0)
