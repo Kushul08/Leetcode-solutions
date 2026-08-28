de7067f8c612ccaf07477eb88b7829c09b184736
Kushul Aleti <kushulaleti@gmail.com>
2026-08-28 08:26:29 +0530

    refactor: update Longest Increasing Subsequence (medium)

--- solutions/medium/0300-longest-increasing-subsequence.py
+++ solutions/medium/0300-longest-increasing-subsequence.py
@@ -1,9 +1,9 @@
 # ─────────────────────────────────────────────────
 #  Problem : 0300. Longest Increasing Subsequence
 #  Difficulty : Medium
-#  Runtime  : 8761 ms
-#  Memory   : 243.6 MB
-#  Solved   : 2026-08-27
+#  Runtime  : 371 ms
+#  Memory   : 104.9 MB
+#  Solved   : 2026-08-28
 # ─────────────────────────────────────────────────
 
 from functools import lru_cache
@@ -11,16 +11,18 @@ class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         n=len(nums)
 
-        dp=[[-1]*(n) for _ in range(n)]
+        # dp=[[-1]*(n) for _ in range(n)]
+        @lru_cache(None)
         def recur(i,prev):
             if i==n:
                 return 0
-            if dp[i][prev]!=-1:
-                return dp[i][prev]
+            # if dp[i][prev]!=-1:
+                # return dp[i][prev]
             pick=0
             if prev==-1 or nums[prev]<nums[i]:
                 pick=recur(i+1,i)+1
             unpick=recur(i+1,prev)
-            dp[i][prev]=max(pick,unpick)
-            return dp[i][prev]
+            # dp[i][prev]=max(pick,unpick)
+            # return dp[i][prev]
+            return max(pick,unpick)
         return recur(0,-1)
