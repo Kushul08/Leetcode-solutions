01fe3ad82d3ba094308f0bdf5dade26be8e96dfd
Kushul Aleti <kushulaleti@gmail.com>
2026-08-28 08:38:02 +0530

    refactor: update Longest Increasing Subsequence (medium)

--- solutions/medium/0300-longest-increasing-subsequence.py
+++ solutions/medium/0300-longest-increasing-subsequence.py
@@ -1,8 +1,8 @@
 # ─────────────────────────────────────────────────
 #  Problem : 0300. Longest Increasing Subsequence
 #  Difficulty : Medium
-#  Runtime  : 371 ms
-#  Memory   : 104.9 MB
+#  Runtime  : 3860 ms
+#  Memory   : 147.4 MB
 #  Solved   : 2026-08-28
 # ─────────────────────────────────────────────────
 
@@ -11,18 +11,12 @@ class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         n=len(nums)
 
-        # dp=[[-1]*(n) for _ in range(n)]
-        @lru_cache(None)
-        def recur(i,prev):
-            if i==n:
-                return 0
-            # if dp[i][prev]!=-1:
-                # return dp[i][prev]
-            pick=0
-            if prev==-1 or nums[prev]<nums[i]:
-                pick=recur(i+1,i)+1
-            unpick=recur(i+1,prev)
-            # dp[i][prev]=max(pick,unpick)
-            # return dp[i][prev]
-            return max(pick,unpick)
-        return recur(0,-1)
+        dp=[[0]*(n+1) for _ in range(n+1)]
+        for i in range(n-1,-1,-1):
+            for prev in range(-1,i):
+                pick=0
+                if prev==-1 or nums[prev]<nums[i]:
+                    pick=dp[i+1][i+1]+1
+                unpick=dp[i+1][prev+1]
+                dp[i][prev+1]=max(pick,unpick)
+        return dp[0][0]
