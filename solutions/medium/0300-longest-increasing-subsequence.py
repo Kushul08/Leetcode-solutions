c4003910a86fdd37d7368526ab247ff0abe8efec
Kushul Aleti <kushulaleti@gmail.com>
2026-08-28 15:29:46 +0530

    refactor: update Longest Increasing Subsequence (medium)

--- solutions/medium/0300-longest-increasing-subsequence.py
+++ solutions/medium/0300-longest-increasing-subsequence.py
@@ -1,7 +1,7 @@
 # ─────────────────────────────────────────────────
 #  Problem : 0300. Longest Increasing Subsequence
 #  Difficulty : Medium
-#  Runtime  : 2825 ms
+#  Runtime  : 1211 ms
 #  Memory   : 19.5 MB
 #  Solved   : 2026-08-28
 # ─────────────────────────────────────────────────
@@ -10,14 +10,11 @@ class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         n=len(nums)
 
-        dp=[0]*(n+1)
-        for i in range(n-1,-1,-1):
-            temp=[0]*(n+1)
-            for prev in range(i-1,-2,-1):
-                pick=0
-                if prev==-1 or nums[prev]<nums[i]:
-                    pick=dp[i+1]+1
-                unpick=dp[prev+1]
-                temp[prev+1]=max(pick,unpick)
-            dp=temp
-        return dp[0]
+        dp=[1]*(n)
+        for i in range(1,n):
+            max_val=dp[i]
+            for j in range(0,i):
+                if nums[j]<nums[i]:
+                    max_val=max(dp[i]+dp[j],max_val)
+            dp[i]=max_val
+        return max(dp)
