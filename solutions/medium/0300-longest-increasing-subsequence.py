d1eca454d1f625a056e581c86a7cbfa9c1f5aa28
Kushul Aleti <kushulaleti@gmail.com>
2026-08-29 08:37:33 +0530

    refactor: update Longest Increasing Subsequence (medium)

--- solutions/medium/0300-longest-increasing-subsequence.py
+++ solutions/medium/0300-longest-increasing-subsequence.py
@@ -1,20 +1,23 @@
 # ─────────────────────────────────────────────────
 #  Problem : 0300. Longest Increasing Subsequence
 #  Difficulty : Medium
-#  Runtime  : 1211 ms
-#  Memory   : 19.5 MB
-#  Solved   : 2026-08-28
+#  Runtime  : 7 ms
+#  Memory   : 12.5 MB
+#  Solved   : 2026-08-29
 # ─────────────────────────────────────────────────
 
-class Solution:
-    def lengthOfLIS(self, nums: List[int]) -> int:
-        n=len(nums)
-
-        dp=[1]*(n)
-        for i in range(1,n):
-            max_val=dp[i]
-            for j in range(0,i):
-                if nums[j]<nums[i]:
-                    max_val=max(dp[i]+dp[j],max_val)
-            dp[i]=max_val
-        return max(dp)
+from bisect import bisect_left
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        temp=[]
+        for num in nums:
+            if not temp or temp[-1]<num:
+                temp.append(num)
+            else:
+                indx=bisect_left(temp,num)
+                temp[indx]=num
+        return len(temp)
