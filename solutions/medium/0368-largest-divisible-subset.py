b38837774c5311d31a5ddaa00d844b27e7ddb462
Kushul Aleti <kushulaleti@gmail.com>
2026-08-29 11:20:42 +0530

    refactor: update Largest Divisible Subset (medium)

--- solutions/medium/0368-largest-divisible-subset.py
+++ solutions/medium/0368-largest-divisible-subset.py
@@ -1,8 +1,8 @@
 # ─────────────────────────────────────────────────
 #  Problem : 0368. Largest Divisible Subset
 #  Difficulty : Medium
-#  Runtime  : 0 ms
-#  Memory   : 12.4 MB
+#  Runtime  : 472 ms
+#  Memory   : 12.6 MB
 #  Solved   : 2026-08-29
 # ─────────────────────────────────────────────────
 
@@ -13,13 +13,14 @@ class Solution(object):
         :rtype: List[int]
         """
         n=len(nums)
+        nums.sort()
         dp=[1]*(n)
         hash=[-1]*n
         for i in range(1,n):
             for j in range(i):
                 if nums[i]%nums[j]==0 or nums[j]%nums[i]==0:
                     if dp[i]<dp[j]+1:
-                        dp[i]=max(dp[i],dp[j]+1)
+                        dp[i]=dp[j]+1
                         hash[i]=j
         max_val=max(dp)
         indx=dp.index(max_val)
@@ -27,4 +28,5 @@ class Solution(object):
         while indx!=-1:
             ans.append(nums[indx])
             indx=hash[indx]
+        print(dp,hash)
         return ans
