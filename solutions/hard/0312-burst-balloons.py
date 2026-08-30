bae9d723a5257d7b46039a03798b42a5c09ede3f
Kushul Aleti <kushulaleti@gmail.com>
2026-08-30 18:01:09 +0530

    refactor: update Burst Balloons (hard)

--- solutions/hard/0312-burst-balloons.py
+++ solutions/hard/0312-burst-balloons.py
@@ -1,8 +1,8 @@
 # ─────────────────────────────────────────────────
 #  Problem : 0312. Burst Balloons
 #  Difficulty : Hard
-#  Runtime  : 6419 ms
-#  Memory   : 16.5 MB
+#  Runtime  : 4039 ms
+#  Memory   : 14.4 MB
 #  Solved   : 2026-08-30
 # ─────────────────────────────────────────────────
 
@@ -15,16 +15,12 @@ class Solution(object):
         nums.insert(0,1)
         nums.append(1)
         n=len(nums)
-        dp=[[-1]*(n) for _ in range(n)]
-        def recur(i,j):
-            if i>j:
-                return 0
-            if dp[i][j]!=-1:
-                return dp[i][j]
-            maxi=float('-inf')
-            for indx in range(i,j+1):
-                steps=nums[i-1]*nums[indx]*nums[j+1]+recur(i,indx-1)+recur(indx+1,j)
-                maxi=max(maxi,steps)
-            dp[i][j]=maxi
-            return dp[i][j]
-        return recur(1,len(nums)-2)
+        dp=[[0]*(n) for _ in range(n)]
+        for i in range(len(nums)-2,0,-1):
+            for j in range(i,len(nums)-1):
+                maxi=float('-inf')
+                for indx in range(i,j+1):
+                    steps=nums[i-1]*nums[indx]*nums[j+1]+dp[i][indx-1]+dp[indx+1][j]
+                    maxi=max(maxi,steps)
+                dp[i][j]=maxi
+        return dp[1][n-2]
