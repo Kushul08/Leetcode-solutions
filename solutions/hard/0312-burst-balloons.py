2440c980367c22dfa7773c936a8589f161613f1b
Kushul Aleti <kushulaleti@gmail.com>
2026-08-30 17:55:52 +0530

    refactor: update Burst Balloons (hard)

--- solutions/hard/0312-burst-balloons.py
+++ solutions/hard/0312-burst-balloons.py
@@ -1,24 +1,30 @@
 # ─────────────────────────────────────────────────
 #  Problem : 0312. Burst Balloons
 #  Difficulty : Hard
-#  Runtime  : 4009 ms
-#  Memory   : 48.9 MB
+#  Runtime  : 6419 ms
+#  Memory   : 16.5 MB
 #  Solved   : 2026-08-30
 # ─────────────────────────────────────────────────
 
-from functools import lru_cache
-class Solution:
-    def maxCoins(self, nums: List[int]) -> int:
+class Solution(object):
+    def maxCoins(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
         nums.insert(0,1)
         nums.append(1)
-        
-        @lru_cache(None)
+        n=len(nums)
+        dp=[[-1]*(n) for _ in range(n)]
         def recur(i,j):
             if i>j:
                 return 0
+            if dp[i][j]!=-1:
+                return dp[i][j]
             maxi=float('-inf')
             for indx in range(i,j+1):
                 steps=nums[i-1]*nums[indx]*nums[j+1]+recur(i,indx-1)+recur(indx+1,j)
                 maxi=max(maxi,steps)
-            return maxi
+            dp[i][j]=maxi
+            return dp[i][j]
         return recur(1,len(nums)-2)
