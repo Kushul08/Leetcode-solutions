d3fc33cbdf1f5d0ec1b2c13736e3dc7702308644
Kushul Aleti <kushulaleti@gmail.com>
2026-08-30 16:23:46 +0530

    refactor: update Minimum Cost to Cut a Stick (hard)

--- solutions/hard/1547-minimum-cost-to-cut-a-stick.py
+++ solutions/hard/1547-minimum-cost-to-cut-a-stick.py
@@ -1,8 +1,8 @@
 # ─────────────────────────────────────────────────
 #  Problem : 1547. Minimum Cost to Cut a Stick
 #  Difficulty : Hard
-#  Runtime  : 1160 ms
-#  Memory   : 13.8 MB
+#  Runtime  : 583 ms
+#  Memory   : 12.5 MB
 #  Solved   : 2026-08-30
 # ─────────────────────────────────────────────────
 
@@ -16,16 +16,13 @@ class Solution(object):
         cuts.append(0)
         cuts.append(n)
         cuts.sort()
-        dp=[[-1]*(len(cuts)) for _ in range(len(cuts))]
-        def mcm(i,j):
-            if i>j:
-                return 0
-            if dp[i][j]!=-1:
-                return dp[i][j]
-            mini=1e9
-            for k in range(i,j+1):
-                steps=(cuts[j+1]-cuts[i-1])+mcm(i,k-1)+mcm(k+1,j)
-                mini=min(mini,steps)
-            dp[i][j]=mini
-            return dp[i][j]
-        return mcm(1,len(cuts)-2)
+        dp=[[0]*(len(cuts)) for _ in range(len(cuts))]
+
+        for i in range(len(cuts)-2,-1,-1):
+            for j in range(i,len(cuts)-1):
+                mini=1e9
+                for k in range(i,j+1):
+                    steps=(cuts[j+1]-cuts[i-1])+dp[i][k-1]+dp[k+1][j]
+                    mini=min(mini,steps)
+                dp[i][j]=mini
+        return dp[1][len(cuts)-2]
