aa2855b668bb66e623cab1319a015205c1f98b04
Kushul Aleti <kushulaleti@gmail.com>
2026-08-31 12:39:06 +0530

    refactor: update Palindrome Partitioning II (hard)

--- solutions/hard/0132-palindrome-partitioning-ii.py
+++ solutions/hard/0132-palindrome-partitioning-ii.py
@@ -1,7 +1,7 @@
 # ─────────────────────────────────────────────────
 #  Problem : 0132. Palindrome Partitioning II
 #  Difficulty : Hard
-#  Runtime  : 3 ms
+#  Runtime  : 0 ms
 #  Memory   : 19.4 MB
 #  Solved   : 2026-08-31
 # ─────────────────────────────────────────────────
@@ -11,15 +11,16 @@ class Solution:
         
         if s==s[::-1]:
             return 0
-        s=' '+s+' '
-        def recur(i,j):
-            if i==j:
-                return 0
-            if (i==0 and s[i:j+1]==s[j::-1]) or s[i:j+1]==s[j:i-1:-1] :
-                return 0
+        n=len(s)
+        def recur(i):
+            if i==n:
+                return -1
             mini=1e9
-            for k in range(i,j+1):
-                steps=1+recur(i,k-1)+recur(k+1,j)
-                mini=min(mini,steps)
+            for k in range(i,n):
+                print(i,k,s[:k+1],s[k::-1],s[:k+1]==s[k::-1])
+                if (i==0 and s[:k+1]==s[k::-1]) or s[i:k+1]==s[k:i-1:-1] :
+                    print('hello')
+                    steps=1+recur(k+1)
+                    mini=min(mini,steps)
             return mini
-        return recur(1,len(s)-1)
+        return recur(0)
