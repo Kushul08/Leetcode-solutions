31944d96815f3c6f8a4efccabd5cea2e3b3bab0b
Kushul Aleti <kushulaleti@gmail.com>
2026-08-31 12:39:19 +0530

    refactor: update Palindrome Partitioning II (hard)

--- solutions/hard/0132-palindrome-partitioning-ii.py
+++ solutions/hard/0132-palindrome-partitioning-ii.py
@@ -1,25 +1,25 @@
 # ─────────────────────────────────────────────────
 #  Problem : 0132. Palindrome Partitioning II
 #  Difficulty : Hard
-#  Runtime  : 0 ms
-#  Memory   : 19.4 MB
+#  Runtime  : 3054 ms
+#  Memory   : 22.4 MB
 #  Solved   : 2026-08-31
 # ─────────────────────────────────────────────────
 
+from functools import lru_cache
 class Solution:
     def minCut(self, s: str) -> int:
         
         if s==s[::-1]:
             return 0
         n=len(s)
+        @lru_cache(None)
         def recur(i):
             if i==n:
                 return -1
             mini=1e9
             for k in range(i,n):
-                print(i,k,s[:k+1],s[k::-1],s[:k+1]==s[k::-1])
                 if (i==0 and s[:k+1]==s[k::-1]) or s[i:k+1]==s[k:i-1:-1] :
-                    print('hello')
                     steps=1+recur(k+1)
                     mini=min(mini,steps)
             return mini
