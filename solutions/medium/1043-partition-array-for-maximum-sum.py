851c3976cc254f819417c76b7872c893e947cb47
Kushul Aleti <kushulaleti@gmail.com>
2026-08-31 14:22:08 +0530

    refactor: update Partition Array for Maximum Sum (medium)

--- solutions/medium/1043-partition-array-for-maximum-sum.py
+++ solutions/medium/1043-partition-array-for-maximum-sum.py
@@ -1,24 +1,31 @@
 # ─────────────────────────────────────────────────
 #  Problem : 1043. Partition Array for Maximum Sum
 #  Difficulty : Medium
-#  Runtime  : 183 ms
-#  Memory   : 21.1 MB
+#  Runtime  : 2275 ms
+#  Memory   : 16.4 MB
 #  Solved   : 2026-08-31
 # ─────────────────────────────────────────────────
 
-from functools import lru_cache
-class Solution:
-    def maxSumAfterPartitioning(self, arr: List[int], k: int) -> int:
+class Solution(object):
+    def maxSumAfterPartitioning(self, arr, k):
+        """
+        :type arr: List[int]
+        :type k: int
+        :rtype: int
+        """
         n=len(arr)
-        @lru_cache(None)
+        dp=[-1]*(n)
         def recur(i):
             if i==n:
                 return 0
+            if dp[i]!=-1:
+                return dp[i]
             maxi=-1e9
             max_val=-1e9
             for j in range(i,min(i+k,n)):
                 max_val=max(max_val,arr[j])
-                steps=(max_val*(j-i+1))+recur(j+1)
+                steps=(max(arr[i:j+1])*(j-i+1))+recur(j+1)
                 maxi=max(maxi,steps)
-            return maxi
+            dp[i]=maxi
+            return dp[i]
         return recur(0)
