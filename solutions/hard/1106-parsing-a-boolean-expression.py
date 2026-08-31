b2e0f64f6fe2de8c306e14d2e930ce99e205ba6e
Kushul Aleti <kushulaleti@gmail.com>
2026-08-31 10:45:17 +0530

    solve: 1106. Parsing A Boolean Expression [hard] (py)

---
+++ solutions/hard/1106-parsing-a-boolean-expression.py
@@ -0,0 +1,41 @@
+# ─────────────────────────────────────────────────
+#  Problem : 1106. Parsing A Boolean Expression
+#  Difficulty : Hard
+#  Runtime  : 43 ms
+#  Memory   : 12.5 MB
+#  Solved   : 2026-08-31
+# ─────────────────────────────────────────────────
+
+class Solution(object):
+    def parseBoolExpr(self, expression):
+        """
+        :type expression: str
+        :rtype: bool
+        """
+        stack=[]
+        curr=None
+        for ch in expression:
+            if ch in '&|!':
+                stack.append(ch)
+                if ch!='(':
+                    curr=ch
+            elif ch in 'tf':
+                stack.append(True if ch=='t' else False)
+            elif ch==')':
+                seen_t=False
+                seen_f=False
+                while stack and (stack[-1]==True or stack[-1]==False):
+                    val=stack.pop()
+                    if val==False:
+                        seen_f=True
+                    else:
+                        seen_t=True
+                
+                opr=stack.pop()
+                if opr=='&':
+                    stack.append(seen_f==False)
+                elif opr=='|':
+                    stack.append(seen_t==True)
+                else:
+                    stack.append(False if seen_t==True else True)
+        return stack[0]
