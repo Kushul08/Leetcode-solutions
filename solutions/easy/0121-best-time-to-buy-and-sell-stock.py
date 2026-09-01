d9799fcacd5514ee7be89c47d3a6fe3eabfd5a02
Kushul Aleti <kushulaleti@gmail.com>
2026-09-01 18:56:16 +0530

    refactor: update Best Time to Buy and Sell Stock (easy)

--- solutions/easy/0121-best-time-to-buy-and-sell-stock.py
+++ solutions/easy/0121-best-time-to-buy-and-sell-stock.py
@@ -1,17 +1,16 @@
 # ─────────────────────────────────────────────────
 #  Problem : 0121. Best Time to Buy and Sell Stock
 #  Difficulty : Easy
-#  Runtime  : 64 ms
-#  Memory   : 28.5 MB
-#  Solved   : 2026-08-26
+#  Runtime  : 60 ms
+#  Memory   : 28.7 MB
+#  Solved   : 2026-09-01
 # ─────────────────────────────────────────────────
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        n=len(prices)
-        max_val=prices[-1]
-        ans=0
-        for i in range(n-1,-1,-1):
-            ans=max(ans,max_val-prices[i])
-            max_val=max(max_val,prices[i])
-        return ans
+        profit=0
+        min_val=prices[0]
+        for price in  prices:
+            min_val=min(min_val,price)
+            profit=max(profit,price-min_val)
+        return profit
