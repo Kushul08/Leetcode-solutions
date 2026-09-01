a6fe473a70b7a07ebfd197ed2bf8d782e5fcb22a
Kushul Aleti <kushulaleti@gmail.com>
2026-09-01 13:39:33 +0530

    refactor: update Count Number of Nice Subarrays (medium)

--- solutions/medium/1248-count-number-of-nice-subarrays.py
+++ solutions/medium/1248-count-number-of-nice-subarrays.py
@@ -1,8 +1,8 @@
 # ─────────────────────────────────────────────────
 #  Problem : 1248. Count Number of Nice Subarrays
 #  Difficulty : Medium
-#  Runtime  : 179 ms
-#  Memory   : 24.6 MB
+#  Runtime  : 165 ms
+#  Memory   : 24.8 MB
 #  Solved   : 2026-09-01
 # ─────────────────────────────────────────────────
 
@@ -11,11 +11,9 @@ class Solution:
         n=len(nums)
         def recur(k):
             l=0
-            r=0
             odd=0
             count=0
-            ans=0
-            while r<n:
+            for r in range(n):
                 if nums[r]%2==1:
                     odd+=1
                 while odd>k:
@@ -24,6 +22,5 @@ class Solution:
                     l+=1
                 if odd<=k:
                     count+=(r-l+1)
-                r+=1
             return count
         return recur(k)-recur(k-1)
