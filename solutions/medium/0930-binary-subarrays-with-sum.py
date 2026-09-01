ff68a1c8d3bab28d5fdb5f1b39ea12a58b27109d
Kushul Aleti <kushulaleti@gmail.com>
2026-09-01 14:10:45 +0530

    refactor: update Binary Subarrays With Sum (medium)

--- solutions/medium/0930-binary-subarrays-with-sum.py
+++ solutions/medium/0930-binary-subarrays-with-sum.py
@@ -1,26 +1,19 @@
 # ─────────────────────────────────────────────────
 #  Problem : 0930. Binary Subarrays With Sum
 #  Difficulty : Medium
-#  Runtime  : 43 ms
-#  Memory   : 20.1 MB
+#  Runtime  : 24 ms
+#  Memory   : 23.2 MB
 #  Solved   : 2026-09-01
 # ─────────────────────────────────────────────────
 
 class Solution:
     def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
-        n=len(nums)
-        def recur(k):
-            l=0
-            count=0
-            ans=0
-            for r in range(n):
-                ans+=nums[r]
-                while ans>k:
-                    ans-=nums[l]
-                    l+=1
-                if ans<=k:
-                    count+=(r-l+1)
-            return count
-        if goal==0:
-            return recur(goal)
-        return recur(goal)-recur(goal-1)
+        hashmap={0:1}
+        ans=0
+        count=0
+        for num in nums:
+            ans+=num
+            if ans-goal in hashmap:
+                count+=hashmap[ans-goal]
+            hashmap[ans]=hashmap.get(ans,0)+1
+        return count
