ed685674cf809bfcb1057ac68946d23398cc64b4
Kushul Aleti <kushulaleti@gmail.com>
2026-09-01 17:31:41 +0530

    refactor: update Maximum Subarray (medium)

--- solutions/medium/0053-maximum-subarray.py
+++ solutions/medium/0053-maximum-subarray.py
@@ -1,7 +1,7 @@
 # ─────────────────────────────────────────────────
 #  Problem : 0053. Maximum Subarray
 #  Difficulty : Medium
-#  Runtime  : 110 ms
+#  Runtime  : 94 ms
 #  Memory   : 21.1 MB
 #  Solved   : 2026-09-01
 # ─────────────────────────────────────────────────
@@ -12,15 +12,6 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        # max_sum=float('-inf')
-        # sums=0
-        # for i in range(len(nums)):
-        #     sums+=nums[i]
-        #     max_sum=max(max_sum,sums)
-        #     # res=max(res,max_sum)
-        #     if sums<0:
-        #         sums=0
-        # return max_sum
         sums=nums[0]
         max_sum=nums[0]
         for i in range(1,len(nums)):
