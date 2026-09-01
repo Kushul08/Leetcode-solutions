ff7bee9877567d7fee8b8658ed665a64462435f3
Kushul Aleti <kushulaleti@gmail.com>
2026-09-01 14:36:46 +0530

    refactor: update Subarray Product Less Than K (medium)

--- solutions/medium/0713-subarray-product-less-than-k.py
+++ solutions/medium/0713-subarray-product-less-than-k.py
@@ -1,8 +1,8 @@
 # ─────────────────────────────────────────────────
 #  Problem : 0713. Subarray Product Less Than K
 #  Difficulty : Medium
-#  Runtime  : 30 ms
-#  Memory   : 21.3 MB
+#  Runtime  : 16 ms
+#  Memory   : 21.4 MB
 #  Solved   : 2026-09-01
 # ─────────────────────────────────────────────────
 
@@ -10,14 +10,13 @@ class Solution:
     def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
         if k<=1: return 0
         n=len(nums)
-        l=r=0
+        l=0
         prod=1
         count=0
-        while r<n:
+        for r in range(n):
             prod*=nums[r]
             while prod>=k:
                 prod=prod//nums[l]
                 l+=1
             count+=(r-l+1)
-            r+=1 
         return count
