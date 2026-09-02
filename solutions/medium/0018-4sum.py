845c117bbf589d0dd408d47265736f15980ab912
Kushul Aleti <kushulaleti@gmail.com>
2026-09-02 11:08:44 +0530

    refactor: update 4Sum (medium)

--- solutions/medium/0018-4sum.py
+++ solutions/medium/0018-4sum.py
@@ -1,8 +1,8 @@
 # ─────────────────────────────────────────────────
 #  Problem : 0018. 4Sum
 #  Difficulty : Medium
-#  Runtime  : 1596 ms
-#  Memory   : 12.5 MB
+#  Runtime  : 0 ms
+#  Memory   : 12.4 MB
 #  Solved   : 2026-09-02
 # ─────────────────────────────────────────────────
 
@@ -13,43 +13,30 @@ class Solution(object):
         :type target: int
         :rtype: List[List[int]]
         """
+        nums.sort()
+        n=len(nums)
         result=set()
-        for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                hashset=set()
-                for k in range(j+1,len(nums)):
-                    bias=target-(nums[i]+nums[j]+nums[k])
-                    if bias in hashset:
-                        temp=[nums[i],nums[j],nums[k],bias]
-                        temp.sort()
-                        result.add(tuple(temp))
-                    hashset.add(nums[k])
+        for i in range(n-4):
+            for j in range(i+1,n-3):
+                k=j+1
+                l=n-1
+                while k<l:
+                    if nums[i]+nums[j]+nums[k]+nums[l]<target:
+                        k+=1
+                    elif nums[i]+nums[j]+nums[k]+nums[l]>target:
+                        l-=1
+                    else:
+                        result.add(tuple([nums[i],nums[j],nums[k],nums[l]]))
+                        while k<l and nums[k]==nums[k+1]:
+                            k+=1
+                        k+=1
+                        while k<l and nums[l]==nums[l-1]:
+                            l-=1
+                        l-=1
+                while j+1<n and nums[j]==nums[j+1]:
+                    j+=1
+                j+=1
+            while i+1<n and nums[i]==nums[i+1]:
+                i+=1
+            i+=1
         return list(result)
-        
-        # nums.sort()
-        # n=len(nums)
-        # result=set()
-        # for i in range(n-4):
-        #     for j in range(i+1,n-3):
-        #         k=j+1
-        #         l=n-1
-        #         while k<l:
-        #             if nums[i]+nums[j]+nums[k]+nums[l]<target:
-        #                 k+=1
-        #             elif nums[i]+nums[j]+nums[k]+nums[l]>target:
-        #                 l-=1
-        #             else:
-        #                 result.add(tuple([nums[i],nums[j],nums[k],nums[l]]))
-        #                 while k<l and nums[k]==nums[k+1]:
-        #                     k+=1
-        #                 k+=1
-        #                 while k<l and nums[l]==nums[l-1]:
-        #                     l-=1
-        #                 l-=1
-        #         while j+1<n and nums[j]==nums[j+1]:
-        #             j+=1
-        #         j+=1
-        #     while i+1<n and nums[i]==nums[i+1]:
-        #         i+=1
-        #     i+=1
-        # return list(result)
