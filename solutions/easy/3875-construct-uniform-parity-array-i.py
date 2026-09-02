52e43d786b9bf43c0079b53011d5ab1dca33bc0c
Kushul Aleti <kushulaleti@gmail.com>
2026-09-02 09:45:21 +0530

    solve: 3875. Construct Uniform Parity Array I [easy] (py)

---
+++ solutions/easy/3875-construct-uniform-parity-array-i.py
@@ -0,0 +1,15 @@
+# ─────────────────────────────────────────────────
+#  Problem : 3875. Construct Uniform Parity Array I
+#  Difficulty : Easy
+#  Runtime  : 0 ms
+#  Memory   : 12.5 MB
+#  Solved   : 2026-09-02
+# ─────────────────────────────────────────────────
+
+class Solution(object):
+    def uniformArray(self, nums1):
+        """
+        :type nums1: List[int]
+        :rtype: bool
+        """
+        return True
