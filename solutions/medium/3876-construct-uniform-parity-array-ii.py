42cc5761f11bf44aa61b466596c7b49436f3a086
Kushul Aleti <kushulaleti@gmail.com>
2026-09-03 10:09:36 +0530

    refactor: update Construct Uniform Parity Array II (medium)

--- solutions/medium/3876-construct-uniform-parity-array-ii.py
+++ solutions/medium/3876-construct-uniform-parity-array-ii.py
@@ -1,8 +1,8 @@
 # ─────────────────────────────────────────────────
 #  Problem : 3876. Construct Uniform Parity Array II
 #  Difficulty : Medium
-#  Runtime  : 20 ms
-#  Memory   : 35 MB
+#  Runtime  : 19 ms
+#  Memory   : 35.3 MB
 #  Solved   : 2026-09-03
 # ─────────────────────────────────────────────────
 
@@ -12,11 +12,7 @@ class Solution:
             return True
         even=0
         odd=0
-        for num in nums1:
-            if num%2==0:
-                even+=1
-            else:
-                odd+=1
-        if odd==len(nums1) or even==len(nums1):
-            return True
-        return False
+        for num in nums1:#we do this to check only if there exits any odd if so we return False
+            if num%2==1:
+                return False
+        return True
