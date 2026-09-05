f65fea416190322ab3329a910684549520020e8e
Kushul Aleti <kushulaleti@gmail.com>
2026-09-05 18:50:59 +0530

    refactor: update Count Square Submatrices with All Ones (medium)

--- solutions/medium/1277-count-square-submatrices-with-all-ones.py
+++ solutions/medium/1277-count-square-submatrices-with-all-ones.py
@@ -1,9 +1,9 @@
 # ─────────────────────────────────────────────────
 #  Problem : 1277. Count Square Submatrices with All Ones
 #  Difficulty : Medium
-#  Runtime  : 71 ms
-#  Memory   : 13.5 MB
-#  Solved   : 2026-08-31
+#  Runtime  : 70 ms
+#  Memory   : 13.6 MB
+#  Solved   : 2026-09-05
 # ─────────────────────────────────────────────────
 
 class Solution(object):
@@ -14,15 +14,19 @@ class Solution(object):
         """
         n,m=len(matrix),len(matrix[0])
         dp=[[0]*m for _ in range(n)]
-        for i in range(m):
-            dp[0][i]=matrix[0][i]
-        for j in range(n):
-            dp[j][0]=matrix[j][0]
+        ans=0
+        for i in range(n):
+            dp[i][0]=matrix[i][0]
+            ans+=dp[i][0]
+        for j in range(m):
+            dp[0][j]=matrix[0][j]
+            ans+=dp[0][j]
+        ans-=matrix[0][0]#here the origin is counted twice so i deleted it for once
         for i in range(1,n):
             for j in range(1,m):
-                if matrix[i][j]==0: continue
+                if matrix[i][j]==0: 
+                    continue
                 dp[i][j]=min(dp[i-1][j],dp[i][j-1],dp[i-1][j-1])+1
-        ans=0
-        for row in dp:
-            ans+=sum(row)
+                ans+=dp[i][j]
+        
         return ans
