69c9df7101d8ec96b187c6436e3599a3eb289c76
Kushul Aleti <kushulaleti@gmail.com>
2026-09-08 16:16:10 +0530

    refactor: update Count Commas in Range (easy)

--- solutions/easy/3870-count-commas-in-range.py
+++ solutions/easy/3870-count-commas-in-range.py
@@ -2,16 +2,12 @@
 #  Problem : 3870. Count Commas in Range
 #  Difficulty : Easy
 #  Runtime  : 0 ms
-#  Memory   : 12.2 MB
+#  Memory   : 19.1 MB
 #  Solved   : 2026-09-08
 # ─────────────────────────────────────────────────
 
-class Solution(object):
-    def countCommas(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
+class Solution:
+    def countCommas(self, n: int) -> int:
         if n<1000:
             return 0
         return n-999
