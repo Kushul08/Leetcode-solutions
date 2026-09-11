4dfe8323faf81ed571910ed9a71fbedd9426ce04
Kushul Aleti <kushulaleti@gmail.com>
2026-09-11 21:50:52 +0530

    refactor: update Unique 3-Digit Even Numbers (easy)

--- solutions/easy/3483-unique-3-digit-even-numbers.py
+++ solutions/easy/3483-unique-3-digit-even-numbers.py
@@ -1,7 +1,7 @@
 # ─────────────────────────────────────────────────
 #  Problem : 3483. Unique 3-Digit Even Numbers
 #  Difficulty : Easy
-#  Runtime  : 23 ms
+#  Runtime  : 16 ms
 #  Memory   : 12.5 MB
 #  Solved   : 2026-09-11
 # ─────────────────────────────────────────────────
@@ -13,10 +13,16 @@ class Solution(object):
         :rtype: int
         """
         n=len(digits)
-        seen=set()
+        seen=[False]*1000
+        ans=0
         for i in range(n):
+            if digits[i]==0: continue
             for j in range(n):
+                if i==j: continue
                 for k in range(n):
-                    if i!=j and j!=k and i!=k and digits[k]%2==0 and digits[i]!=0:
-                        seen.add((digits[i],digits[j],digits[k]))
-        return len(seen)
+                    if k==j or k==i or digits[k]%2==1: continue
+                    val=digits[i]*100+digits[j]*10+digits[k]
+                    if seen[val]==False:
+                        seen[val]=True
+                        ans+=1
+        return ans
