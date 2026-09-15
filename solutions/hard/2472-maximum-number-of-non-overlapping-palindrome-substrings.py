c36af0879b2103d2cb4e5b8b0d234e5d7e49a9bf
Kushul Aleti <kushulaleti@gmail.com>
2026-09-15 22:13:05 +0530

    refactor: update Maximum Number of Non-overlapping Palindrome Substrings (hard)

--- solutions/hard/2472-maximum-number-of-non-overlapping-palindrome-substrings.py
+++ solutions/hard/2472-maximum-number-of-non-overlapping-palindrome-substrings.py
@@ -1,28 +1,31 @@
 # ─────────────────────────────────────────────────
 #  Problem : 2472. Maximum Number of Non-overlapping Palindrome Substrings
 #  Difficulty : Hard
-#  Runtime  : 570 ms
-#  Memory   : 12.3 MB
+#  Runtime  : 0 ms
+#  Memory   : 19.4 MB
 #  Solved   : 2026-09-15
 # ─────────────────────────────────────────────────
 
-class Solution(object):
-    def maxPalindromes(self, s, k):
-        """
-        :type s: str
-        :type k: int
-        :rtype: int
-        """
+from functools import lru_cache
+class Solution:
+    def maxPalindromes(self, s: str, k: int) -> int:
+        
         n=len(s)
-        def recur(i,string):
-            if i==-1:
+
+        @lru_cache(None)
+        def recur(i,j):
+            if i==0:
+                string=s[i:j+1]
                 if len(string)>=k and string==string[::-1]:
                     return 1
                 return 0
-            if len(string)>=k and string==string[::-1]:
-                return 1+recur(i-1,'')
-            unpick=recur(i-1,'')
-            pick=recur(i-1,string+s[i])
+
+            if (j-i+1)>=k:
+                string=s[i:j+1]
+                if string==string[::-1]:
+                    return 1+recur(i-1,i-1)
+            unpick=recur(i-1,j-1)
+            pick=recur(i-1,j)
             return max(unpick,pick)
             
-        return recur(n-1,'')
+        return recur(n-1,n-1)
