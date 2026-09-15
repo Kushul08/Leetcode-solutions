eea8d87e88c9d8285edf56e2b500ad7b78227966
Kushul Aleti <kushulaleti@gmail.com>
2026-09-15 21:52:14 +0530

    refactor: update Maximum Number of Non-overlapping Palindrome Substrings (hard)

--- solutions/hard/2472-maximum-number-of-non-overlapping-palindrome-substrings.py
+++ solutions/hard/2472-maximum-number-of-non-overlapping-palindrome-substrings.py
@@ -1,33 +1,28 @@
 # ─────────────────────────────────────────────────
 #  Problem : 2472. Maximum Number of Non-overlapping Palindrome Substrings
 #  Difficulty : Hard
-#  Runtime  : 348 ms
-#  Memory   : 12.5 MB
+#  Runtime  : 570 ms
+#  Memory   : 12.3 MB
 #  Solved   : 2026-09-15
 # ─────────────────────────────────────────────────
 
-class Solution:
-    def maxPalindromes(self, s: str, k: int) -> int:
-
-        def check(i,j):
-            length=(j-i+1)
-            if length<k:
-                return False
-            string=s[i:j+1]
-            return string==string[::-1]
-
-        ans=0
-        i=0
-        j=1
+class Solution(object):
+    def maxPalindromes(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """
         n=len(s)
-
-        while i<n:
-            j=i+1
-            while j<n:
-                if check(i,j):
-                    ans+=1
-                    i=j
-                    break
-                j+=1
-            i+=1
-        return ans
+        def recur(i,string):
+            if i==-1:
+                if len(string)>=k and string==string[::-1]:
+                    return 1
+                return 0
+            if len(string)>=k and string==string[::-1]:
+                return 1+recur(i-1,'')
+            unpick=recur(i-1,'')
+            pick=recur(i-1,string+s[i])
+            return max(unpick,pick)
+            
+        return recur(n-1,'')
