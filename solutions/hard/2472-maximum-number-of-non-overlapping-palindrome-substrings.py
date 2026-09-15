f5c88f05fd603a8f919a89e3f04b089c6ecb9258
Kushul Aleti <kushulaleti@gmail.com>
2026-09-15 22:23:04 +0530

    refactor: update Maximum Number of Non-overlapping Palindrome Substrings (hard)

--- solutions/hard/2472-maximum-number-of-non-overlapping-palindrome-substrings.py
+++ solutions/hard/2472-maximum-number-of-non-overlapping-palindrome-substrings.py
@@ -1,31 +1,36 @@
 # ─────────────────────────────────────────────────
 #  Problem : 2472. Maximum Number of Non-overlapping Palindrome Substrings
 #  Difficulty : Hard
-#  Runtime  : 0 ms
-#  Memory   : 19.4 MB
+#  Runtime  : 3130 ms
+#  Memory   : 230.2 MB
 #  Solved   : 2026-09-15
 # ─────────────────────────────────────────────────
 
 from functools import lru_cache
 class Solution:
     def maxPalindromes(self, s: str, k: int) -> int:
-        
         n=len(s)
 
-        @lru_cache(None)
-        def recur(i,j):
-            if i==0:
-                string=s[i:j+1]
-                if len(string)>=k and string==string[::-1]:
-                    return 1
-                return 0
+        pal=[[True]*n for _ in range(n)]
+        for i in range(n):
+            pal[i][i] = True
 
-            if (j-i+1)>=k:
-                string=s[i:j+1]
-                if string==string[::-1]:
-                    return 1+recur(i-1,i-1)
-            unpick=recur(i-1,j-1)
-            pick=recur(i-1,j)
-            return max(unpick,pick)
+        for length in range(2, n + 1):
+            for i in range(n - length + 1):
+                j = i + length - 1
+                pal[i][j] = s[i] == s[j] and (length == 2 or pal[i + 1][j - 1])
+
+        
+        @lru_cache(None)
+        def recur(i):
+            if i>=n:
+                return 0
             
-        return recur(n-1,n-1)
+            ans=recur(i+1)
+
+            for j in range(i+k-1,n):
+                if pal[i][j]:
+                    ans=max(ans,1+recur(j+1))
+            return ans
+
+        return recur(0)
