1ea1d7828037c3c3cd2a627a76f092e54ac2ed33
Kushul Aleti <kushulaleti@gmail.com>
2026-09-16 21:56:58 +0530

    refactor: update Number of Sets of K Non-Overlapping Line Segments (medium)

--- solutions/medium/1621-number-of-sets-of-k-non-overlapping-line-segments.py
+++ solutions/medium/1621-number-of-sets-of-k-non-overlapping-line-segments.py
@@ -2,22 +2,13 @@
 #  Problem : 1621. Number of Sets of K Non-Overlapping Line Segments
 #  Difficulty : Medium
 #  Runtime  : 0 ms
-#  Memory   : 19.3 MB
+#  Memory   : 19.5 MB
 #  Solved   : 2026-09-16
 # ─────────────────────────────────────────────────
 
+from math import comb
 class Solution:
     def numberOfSets(self, n: int, k: int) -> int:
-        
+        MOD=int(1e9+7)
 
-        def recur(num,k):
-            if num==1:
-                if k==0:
-                    return 1
-                return 0
-            skip=recur(num-1,k)
-            take=0
-            if k>0:
-                take=recur(num-1,k-1)
-            return (skip+take)
-        return recur(n+k-1,k*2)
+        return comb(n+k-1,k*2)%MOD
