55cd27be62ecd7687e6ac1072251c46083a6c6b9
Kushul Aleti <kushulaleti@gmail.com>
2026-09-17 21:55:32 +0530

    solve: 1477. Find Two Non-overlapping Sub-arrays Each With Target Sum [medium] (py)

---
+++ solutions/medium/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.py
@@ -0,0 +1,36 @@
+# ─────────────────────────────────────────────────
+#  Problem : 1477. Find Two Non-overlapping Sub-arrays Each With Target Sum
+#  Difficulty : Medium
+#  Runtime  : 0 ms
+#  Memory   : 12.4 MB
+#  Solved   : 2026-09-17
+# ─────────────────────────────────────────────────
+
+class Solution(object):
+    def minSumOfLengths(self, arr, target):
+        """
+        :type arr: List[int]
+        :type target: int
+        :rtype: int
+        """
+        n=len(arr)
+        min1=-1
+        min2=-1
+        for i in range(n):
+            sums=0
+            for j in range(i,n):
+                sums+=arr[j] 
+                if sums==target:
+                    length=(j-i+1)
+                    if min1==-1:
+                        min1=length
+                    elif min2==-1:
+                        min2=length
+                    else:
+                        if min1>length:
+                            min1=length
+                        elif min2>length:
+                            min2=length
+        if min1!=-1 and min2!=-1:
+            return min1+min2
+        return -1
