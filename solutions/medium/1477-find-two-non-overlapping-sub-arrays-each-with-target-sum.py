9d8b1152b65ba4004b37c182d479c9eb9518baba
Kushul Aleti <kushulaleti@gmail.com>
2026-09-17 22:58:03 +0530

    refactor: update Find Two Non-overlapping Sub-arrays Each With Target Sum (medium)

--- solutions/medium/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.py
+++ solutions/medium/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.py
@@ -1,8 +1,8 @@
 # ─────────────────────────────────────────────────
 #  Problem : 1477. Find Two Non-overlapping Sub-arrays Each With Target Sum
 #  Difficulty : Medium
-#  Runtime  : 0 ms
-#  Memory   : 12.4 MB
+#  Runtime  : 308 ms
+#  Memory   : 32.9 MB
 #  Solved   : 2026-09-17
 # ─────────────────────────────────────────────────
 
@@ -14,29 +14,24 @@ class Solution(object):
         :rtype: int
         """
         n=len(arr)
-        min1=-1
-        min2=-1
-        i=0
-        while i<n:
-            sums=0
-            for j in range(i,n):
-                sums+=arr[j] 
-                if sums==target:
-                    length=(j-i+1)
-                    if min1==-1:
-                        min1=length
-                    elif min2==-1:
-                        min2=length
-                    else:
-                        if min1>length:
-                            min1=length
-                        elif min2>length:
-                            min2=length
-                    i=j
-                    break
-                elif sums>target:
-                    break
-            i+=1
-        if min1!=-1 and min2!=-1:
-            return min1+min2
-        return -1
+        hashmap={0:-1}
+        sums=0
+        dp=[(1e9)]*n
+        min_so_far=1e9
+        min_total_len=1e9
+        for i,num in enumerate(arr):
+            sums+=num
+            if i>0:
+                min_so_far=min(min_so_far,dp[i-1])
+            if sums-target in hashmap:
+                start=hashmap[sums-target]
+                curr_len=i-start
+
+                dp[i]=min(min_so_far,curr_len)
+
+                if start>=0 and dp[start]!=1e9:
+                    min_total_len=min(min_total_len,curr_len+dp[start])
+            else:
+                dp[i]=min_so_far
+            hashmap[sums]=i
+        return min_total_len if min_total_len!=1e9 else -1
